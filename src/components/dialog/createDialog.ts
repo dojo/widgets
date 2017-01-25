f726d1da1c031115d537181bf06e33fa578c656a
bitpshr <paul@paulbouchon.net>
2017-01-25 09:18:30 -0500

    WIP example

--- src/components/dialog/createDialog.ts
+++ src/components/dialog/createDialog.ts
@@ -3,6 +3,9 @@ import { DNode, Widget, WidgetProperties, WidgetFactory } from '@dojo/widget-cor
 import createWidgetBase from '@dojo/widget-core/createWidgetBase';
 import { v } from '@dojo/widget-core/d';
 
+import * as baseTheme from './styles/dialog';
+import themeableMixin, { Themeable } from '@dojo/widget-core/mixins/themeable';
+
 export interface DialogProperties extends WidgetProperties {
 	closeable?: boolean;
 	enterAnimation?: string;
@@ -15,18 +18,20 @@ export interface DialogProperties extends WidgetProperties {
 	onRequestClose?(): void;
 };
 
-export type Dialog = Widget<DialogProperties> & {
+export type Dialog = Widget<DialogProperties> & Themeable<typeof baseTheme> & {
 	onCloseClick?(): void;
 	onUnderlayClick?(): void;
 };
 
 export interface DialogFactory extends WidgetFactory<Dialog, DialogProperties> { };
 
-const createDialogWidget: DialogFactory = createWidgetBase.mixin({
+const createDialog: DialogFactory = createWidgetBase.mixin(themeableMixin).mixin({
 	mixin: {
+		baseTheme,
+
 		onCloseClick: function (this: Dialog) {
 			const closeable = this.properties.closeable || typeof this.properties.closeable === 'undefined';
-			closeable && this.properties.onRequestClose && this.properties.onRequestClose.call(this);
+			closeable && this.properties.onRequestClose && this.properties.onRequestClose();
 		},
 
 		onUnderlayClick: function (this: Dialog) {
@@ -37,45 +42,50 @@ const createDialogWidget: DialogFactory = createWidgetBase.mixin({
 			const {
 				closeable = true,
 				enterAnimation = 'show',
-				exitAnimation = 'hide'
+				exitAnimation = 'hide',
+				title = '',
+				open = false
 			} = this.properties;
 
 			const children: DNode[] = [
-				v('div.underlay', {
+				v('div', {
+					classes: this.theme.underlay,
 					enterAnimation: 'show',
 					exitAnimation: 'hide',
 					onclick: this.onUnderlayClick
 				}),
-				v('div.main', {
+				v('div', {
+					classes: this.theme.main,
 					enterAnimation: enterAnimation,
 					exitAnimation: exitAnimation
 				}, [
-					v('div.title', [
-						this.properties.title ? this.properties.title : null,
-						closeable ? v('div.close', {
+					v('div', {
+						classes: this.theme.title
+					}, [
+						title,
+						closeable ? v('div', {
+							classes: this.theme.close,
 							innerHTML: '✕',
 							onclick: this.onCloseClick
 						}) : null
 					]),
-					v('div.content', this.children)
+					v('div', { classes: this.theme.content }, this.children)
 				])
 			];
 
-			return this.properties.open ? children : [];
+			return open ? children : [];
 		},
 
 		nodeAttributes: [
 			function(this: Dialog): VNodeProperties {
-				this.properties.open && this.properties.onOpen && this.properties.onOpen.call(this);
+				this.properties.open && this.properties.onOpen && this.properties.onOpen();
 				return {
 					'data-underlay': this.properties.underlay ? 'true' : 'false',
 					'data-open': this.properties.open ? 'true' : 'false'
 				};
 			}
-		],
-
-		classes: [ 'dialog' ]
+		]
 	}
 });
 
-export default createDialogWidget;
+export default createDialog;
---
+++ src/examples/dialog/index.ts
@@ -0,0 +1,126 @@
+import { DNode, Widget, WidgetProperties } from '@dojo/widget-core/interfaces';
+import { w, v } from '@dojo/widget-core/d';
+import createWidgetBase from '@dojo/widget-core/createWidgetBase';
+import createProjectorMixin from '@dojo/widget-core/mixins/createProjectorMixin';
+import createDialog from '../../components/dialog/createDialog';
+import internalState, { InternalState } from '@dojo/widget-core/mixins/internalState';
+
+type Root = Widget<WidgetProperties> & InternalState;
+
+function toggleModal(this: Root, event: Event) {
+	this.setState({ modal: (<HTMLInputElement> event.target).checked });
+}
+
+function toggleUnderlay(this: Root, event: Event) {
+	this.setState({ underlay: (<HTMLInputElement> event.target).checked });
+}
+
+function toggleCloseable(this: Root, event: Event) {
+	this.setState({ closeable: (<HTMLInputElement> event.target).checked });
+}
+
+function toggleEnterAnimation(this: Root, event: Event) {
+	this.setState({ enterAnimation: (<HTMLInputElement> event.target).checked ? 'slideIn' : undefined });
+}
+
+function toggleExitAnimation(this: Root, event: Event) {
+	this.setState({ exitAnimation: (<HTMLInputElement> event.target).checked ? 'slideOut' : undefined });
+}
+
+const createApp = createWidgetBase.mixin(internalState).mixin({
+	mixin: {
+		cssTransitions: true,
+		getChildrenNodes: function(this: Root): DNode[] {
+			return [
+				w(createDialog, {
+					id: 'dialog',
+					title: 'Dialog',
+					open: <boolean> this.state['open'],
+					modal: <boolean> this.state['modal'],
+					underlay: <boolean> this.state['underlay'],
+					closeable: <boolean> this.state['closeable'],
+					enterAnimation: <string> this.state['enterAnimation'],
+					exitAnimation: <string> this.state['exitAnimation'],
+					onRequestClose: () => {
+						this.setState({ open: false });
+					}
+				}, [
+					`Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+					Quisque id purus ipsum. Aenean ac purus purus.
+					Nam sollicitudin varius augue, sed lacinia felis tempor in.`
+				]),
+				v('button', {
+					id: 'button',
+					label: 'open dialog',
+					onclick: () => {
+						this.setState({ open: true });
+					}
+				}),
+				v('div', { classes: { option: true }}, [
+					v('input', {
+						type: 'checkbox',
+						id: 'modal',
+						onchange: toggleModal
+					}),
+					v('label', {
+						for: 'modal',
+						innerHTML: 'modal'
+					})
+				]),
+				v('div', { classes: { option: true }}, [
+					v('input', {
+						type: 'checkbox',
+						id: 'underlay',
+						onchange: toggleUnderlay
+					}),
+					v('label', {
+						for: 'underlay',
+						innerHTML: 'underlay'
+					})
+				]),
+				v('div', { classes: { option: true }}, [
+					v('input', {
+						type: 'checkbox',
+						id: 'closeable',
+						onchange: toggleCloseable,
+						checked: true
+					}),
+					v('label', {
+						for: 'closeable',
+						innerHTML: 'closeable'
+					})
+				]),
+				v('div', { classes: { option: true }}, [
+					v('input', {
+						type: 'checkbox',
+						id: 'enterAnimation',
+						onchange: toggleEnterAnimation
+					}),
+					v('label', {
+						for: 'enterAnimation',
+						innerHTML: 'enterAnimation'
+					})
+				]),
+				v('div', { classes: { option: true }}, [
+					v('input', {
+						type: 'checkbox',
+						id: 'exitAnimation',
+						onchange: toggleExitAnimation
+					}),
+					v('label', {
+						for: 'exitAnimation',
+						innerHTML: 'exitAnimation'
+					})
+				])
+			];
+		},
+		classes: [ 'main-app' ],
+		tagName: 'main'
+	}
+});
+
+createApp.mixin(createProjectorMixin)({
+	cssTransitions: true
+}).append().then(() => {
+	console.log('projector is attached');
+});
