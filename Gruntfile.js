41b61918391e00345a2b21293a6117e435b77f5f
bitpshr <paul@paulbouchon.net>
2017-01-25 11:52:50 -0500

    Update Gruntfile for correct dist and dev support

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,25 @@
 module.exports = function (grunt) {
+	var staticExampleFiles = [ 'src/examples/**', '!src/examples/**/*.js' ];
+
 	require('grunt-dojo2').initConfig(grunt, {
-		/* any custom configuration goes here */
+		copy: {
+			staticExampleFiles: {
+				expand: true,
+				cwd: '.',
+				src: staticExampleFiles,
+				dest: '<%= devDirectory %>'
+			}
+		}
 	});
 
+	grunt.registerTask('dev', grunt.config.get('devTasks').concat([
+		'copy:staticExampleFiles',
+		'postcss:modules-dev',
+		'postcss:variables-dev'
+	]));
+
 	grunt.registerTask('dist', grunt.config.get('distTasks').concat([
-		'postcss:modules',
-		'postcss:variables'
+		'postcss:modules-dist',
+		'postcss:variables-dist'
 	]));
 };
