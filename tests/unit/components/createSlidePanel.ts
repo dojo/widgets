aff3b1a26de87bb1f37766cdd0208cde32d69d08
Paul Bouchon <paul@paulbouchon.net>
2017-02-04 09:19:48 -0500

    Increase coverage

--- tests/unit/components/createSlidePanel.ts
+++ tests/unit/components/createSlidePanel.ts
@@ -3,6 +3,23 @@ import * as assert from 'intern/chai!assert';
 import { VNode } from '@dojo/interfaces/vdom';
 import createSlidePanel from '../../../src/components/slidePanel/createSlidePanel';
 
+function createEvent(type: string, x: number): any {
+	return {
+		type,
+		stopPropagation() { },
+		preventDefault() { },
+		pageX: x,
+		changedTouches: [{
+			screenX: x
+		}],
+		target: {
+			classList: {
+				contains() { return true; }
+			}
+		}
+	};
+}
+
 registerSuite({
 	name: 'createSlidePanel',
 
@@ -59,5 +76,71 @@ registerSuite({
 		});
 		<VNode> slidePanel.__render__();
 		assert.isTrue(called, 'onOpen should be called');
+	},
+
+	'click underlay to close'() {
+		let called = false;
+
+		const slidePanel = createSlidePanel({
+			properties: {
+				onRequestClose() { called = true; }
+			}
+		});
+
+		slidePanel.onSwipeStart!(createEvent('mousedown', 300));
+		slidePanel.onSwipeEnd!(createEvent('mouseup', 300));
+
+		assert.isTrue(called, 'onRequestClose should be called on underlay click');
+	},
+
+	'drag to close'() {
+		let called = false;
+
+		const slidePanel = createSlidePanel({
+			properties: {
+				onRequestClose() { called = true; },
+				align: 'right'
+			}
+		});
+
+		slidePanel.onSwipeStart!(createEvent('mousedown', 300));
+		slidePanel.onSwipeMove!(createEvent('mousemove', 400));
+		slidePanel.onSwipeEnd!(createEvent('mouseup', 500));
+
+		assert.isTrue(called, 'onRequestClose should be called if dragged far enough to close');
+	},
+
+	'swipe to close'() {
+		let called = false;
+
+		const slidePanel = createSlidePanel({
+			properties: {
+				onRequestClose() { called = true; },
+				width: 256
+			}
+		});
+
+		slidePanel.onSwipeStart!(createEvent('touchstart', 300));
+		slidePanel.onSwipeMove!(createEvent('touchmove', 400));
+		slidePanel.onSwipeEnd!(createEvent('touchend', 50));
+
+		assert.isTrue(called, 'onRequestClose should be called if swiped far enough to close');
+	},
+
+	'not drag far enough to close'() {
+		let called = false;
+
+		const slidePanel = createSlidePanel({
+			properties: {
+				onRequestClose() { called = true; }
+			}
+		});
+
+		slidePanel.onSwipeMove!(createEvent('mousemove', 250));
+		slidePanel.onSwipeStart!(createEvent('mousedown', 300));
+		slidePanel.onSwipeMove!(createEvent('mousemove', 250));
+		slidePanel.onSwipeEnd!(createEvent('mouseup', 250));
+
+		assert.isFalse(called, 'onRequestClose should not be called if not dragged far enough to close');
 	}
 });
