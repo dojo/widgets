60e7a1b8ad17ad76127fb0dd30ec7f018b02b76c
Mangala SSS Khalsa <mkhalsa@sitepen.com>
2017-02-08 14:03:35 -0700

    TitlePane: follow style guide for indentation

--- src/components/titlePane/createTitlePane.ts
+++ src/components/titlePane/createTitlePane.ts
@@ -25,64 +25,64 @@ export type TitlePane = Widget<TitlePaneProperties> & ThemeableMixin & {
 export interface TitlePaneFactory extends WidgetFactory<TitlePane, TitlePaneProperties> { };
 
 const createTitlePane: TitlePaneFactory = createWidgetBase
-.mixin(themeable)
-.mixin({
-	mixin: {
-		baseClasses: css,
+	.mixin(themeable)
+	.mixin({
+		mixin: {
+			baseClasses: css,
 
-		onClickTitle: function (this: TitlePane) {
-			const {
-				properties: {
-					collapsed = false
+			onClickTitle: function (this: TitlePane) {
+				const {
+					properties: {
+						collapsed = false
+					}
+				} = this;
+
+				if (collapsed) {
+					this.properties.onRequestExpand && this.properties.onRequestExpand();
 				}
-			} = this;
+				else {
+					this.properties.onRequestCollapse && this.properties.onRequestCollapse();
+				}
+			},
 
-			if (collapsed) {
-				this.properties.onRequestExpand && this.properties.onRequestExpand();
+			getChildrenNodes: function (this: TitlePane): DNode[] {
+				const {
+					collapsed = false,
+					collapsible = true,
+					enterAnimation = animations.slideInDown,
+					exitAnimation = animations.slideOutUp,
+					title = ''
+				} = this.properties;
+
+				let titleProperties: VNodeProperties;
+
+				if (collapsible) {
+					titleProperties = {
+						classes: this.classes(css.title, css.collapsible).get(),
+						onclick: this.onClickTitle
+					};
+				}
+				else {
+					titleProperties = {
+						classes: this.classes(css.title).get()
+					};
+				}
+
+				const children: DNode[] = [
+					v('div', titleProperties, [ title ])
+				];
+
+				if (!collapsed) {
+					children.push(v('div', {
+						classes: this.classes(css.content).get(),
+						enterAnimation,
+						exitAnimation
+					}, this.children));
+				}
+
+				return children;
 			}
-			else {
-				this.properties.onRequestCollapse && this.properties.onRequestCollapse();
-			}
-		},
-
-		getChildrenNodes: function (this: TitlePane): DNode[] {
-			const {
-				collapsed = false,
-				collapsible = true,
-				enterAnimation = animations.slideInDown,
-				exitAnimation = animations.slideOutUp,
-				title = ''
-			} = this.properties;
-
-			let titleProperties: VNodeProperties;
-
-			if (collapsible) {
-				titleProperties = {
-					classes: this.classes(css.title, css.collapsible).get(),
-					onclick: this.onClickTitle
-				};
-			}
-			else {
-				titleProperties = {
-					classes: this.classes(css.title).get()
-				};
-			}
-
-			const children: DNode[] = [
-				v('div', titleProperties, [ title ])
-			];
-
-			if (!collapsed) {
-				children.push(v('div', {
-					classes: this.classes(css.content).get(),
-					enterAnimation,
-					exitAnimation
-				}, this.children));
-			}
-
-			return children;
 		}
-	}
-});
+	});
 
 export default createTitlePane;
--- src/examples/titlePane/index.ts
+++ src/examples/titlePane/index.ts
@@ -8,71 +8,71 @@ import createTitlePane from '../../components/titlePane/createTitlePane';
 type Root = Widget<WidgetProperties> & InternalState;
 
 const createApp = createWidgetBase
-.mixin(internalState)
-.mixin({
-	mixin: {
-		getChildrenNodes: function(this: Root): DNode[] {
-			return [
-				w(createTitlePane, {
-					id: 'titlePane1',
-					title: 'Title Pane Widget With collapsible=false',
-					collapsible: false,
-					onRequestCollapse: () => {
-						alert('onRequestCollapse should never get called');
-					},
-					onRequestExpand: () => {
-						alert('onRequestExpand should never get called');
-					}
-				}, [
-					v('div', {
-						innerHTML: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-							Quisque id purus ipsum. Aenean ac purus purus.
-							Nam sollicitudin varius augue, sed lacinia felis tempor in.`
-					})
-				]),
-				w(createTitlePane, {
-					id: 'titlePane2',
-					title: 'Title Pane Widget (collapsible)',
-					collapsed: <boolean> this.state['t2collapsed'],
-					onRequestCollapse: () => {
-						this.setState({ t2collapsed: true });
-					},
-					onRequestExpand: () => {
-						this.setState({ t2collapsed: false });
-					}
-				}, [
-					v('div', {
-						innerHTML: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-							Quisque id purus ipsum. Aenean ac purus purus.
-							Nam sollicitudin varius augue, sed lacinia felis tempor in.`
-					})
-				]),
-				w(createTitlePane, {
-					id: 'titlePane3',
-					title: 'Title Pane Widget with collapsed=true',
-					collapsed: <boolean> (this.state['t3collapsed'] === undefined ? true : this.state['t3collapsed']),
-					onRequestCollapse: () => {
-						this.setState({ t3collapsed: true });
-					},
-					onRequestExpand: () => {
-						this.setState({ t3collapsed: false });
-					}
-				}, [
-					v('div', {
-						innerHTML: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-							Quisque id purus ipsum. Aenean ac purus purus.
-							Nam sollicitudin varius augue, sed lacinia felis tempor in.`
-					})
-				])
-			];
-		},
-		// TODO: why doesn't this work? What is the recommended approach?
-		classes: [ 'main-app' ]
-	}
-});
+	.mixin(internalState)
+	.mixin({
+		mixin: {
+			getChildrenNodes: function(this: Root): DNode[] {
+				return [
+					w(createTitlePane, {
+						id: 'titlePane1',
+						title: 'Title Pane Widget With collapsible=false',
+						collapsible: false,
+						onRequestCollapse: () => {
+							alert('onRequestCollapse should never get called');
+						},
+						onRequestExpand: () => {
+							alert('onRequestExpand should never get called');
+						}
+					}, [
+						v('div', {
+							innerHTML: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+								Quisque id purus ipsum. Aenean ac purus purus.
+								Nam sollicitudin varius augue, sed lacinia felis tempor in.`
+						})
+					]),
+					w(createTitlePane, {
+						id: 'titlePane2',
+						title: 'Title Pane Widget (collapsible)',
+						collapsed: <boolean> this.state['t2collapsed'],
+						onRequestCollapse: () => {
+							this.setState({ t2collapsed: true });
+						},
+						onRequestExpand: () => {
+							this.setState({ t2collapsed: false });
+						}
+					}, [
+						v('div', {
+							innerHTML: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+								Quisque id purus ipsum. Aenean ac purus purus.
+								Nam sollicitudin varius augue, sed lacinia felis tempor in.`
+						})
+					]),
+					w(createTitlePane, {
+						id: 'titlePane3',
+						title: 'Title Pane Widget with collapsed=true',
+						collapsed: <boolean> (this.state['t3collapsed'] === undefined ? true : this.state['t3collapsed']),
+						onRequestCollapse: () => {
+							this.setState({ t3collapsed: true });
+						},
+						onRequestExpand: () => {
+							this.setState({ t3collapsed: false });
+						}
+					}, [
+						v('div', {
+							innerHTML: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+								Quisque id purus ipsum. Aenean ac purus purus.
+								Nam sollicitudin varius augue, sed lacinia felis tempor in.`
+						})
+					])
+				];
+			},
+			// TODO: why doesn't this work? What is the recommended approach?
+			classes: [ 'main-app' ]
+		}
+	});
 
 createApp.mixin(createProjectorMixin)({
-	cssTransitions: true
-}).append().then(() => {
-	console.log('projector is attached');
-});
+		cssTransitions: true
+	}).append().then(() => {
+		console.log('projector is attached');
+	});
