bcbcdac7bcc6c048b9a77028728296a4927f4dca
smhigley <shigley@sitepen.com>
2017-02-24 09:08:25 -0800

    updated textinput tests

--- src/textinput/tests/unit/TextInput.ts
+++ src/textinput/tests/unit/TextInput.ts
@@ -2,6 +2,7 @@ import * as registerSuite from 'intern!object';
 import * as assert from 'intern/chai!assert';
 import { VNode } from '@dojo/interfaces/vdom';
 import TextInput from '../../TextInput';
+import * as css from '../../styles/textinput.css';
 
 registerSuite({
 	name: 'TextInput',
@@ -31,6 +32,26 @@ registerSuite({
 		vnode = <VNode> textinput.__render__();
 
 		assert.strictEqual(vnode.properties!.type, 'email');
+	},
+
+	'invalid state'() {
+		const textinput = new TextInput({});
+		let vnode = <VNode> textinput.__render__();
+
+		assert.isFalse(vnode.properties!.classes![css.valid]);
+		assert.isFalse(vnode.properties!.classes![css.invalid]);
+
+		textinput.setProperties({
+			invalid: true
+		});
+		vnode = <VNode> textinput.__render__();
+		assert.isTrue(vnode.properties!.classes![css.invalid]);
+
+		textinput.setProperties({
+			invalid: false
+		});
+		vnode = <VNode> textinput.__render__();
+		assert.isTrue(vnode.properties!.classes![css.valid]);
 	},
 
 	events: {
