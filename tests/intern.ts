4aa42533123dc667cbddb19c8c208ff7974e5283
Anthony Gubler <agubler@sitepen.com>
2017-02-24 15:48:02 +0000

    remove the reporter and dojo-dev typings

--- tests/intern.ts
+++ tests/intern.ts
@@ -55,6 +55,7 @@ export const loaderOptions = {
 		{ name: 'tests', location: '_build/tests' },
 		{ name: 'dojo', location: 'node_modules/intern/node_modules/dojo' },
 		{ name: '@dojo', location: 'node_modules/@dojo' },
+		{ name: 'grunt-dojo2', location: 'node_modules/grunt-dojo2'},
 		{ name: 'maquette', location: 'node_modules/maquette/dist', main: 'maquette' }
 	]
 };
