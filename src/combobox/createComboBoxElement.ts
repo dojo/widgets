822340223e690c5f09951d02f2766c685c3b98a0
Paul Bouchon <paul@paulbouchon.net>
2017-03-06 09:56:01 -0500

    Update custom element for ComboBox

--- src/combobox/createComboBoxElement.ts
+++ src/combobox/createComboBoxElement.ts
@@ -11,10 +11,62 @@ export default function createComboBoxElement(): CustomElementDescriptor {
 		tagName: 'dojo-combo-box',
 		widgetFactory: ComboBox,
 		attributes: [
-
+			{
+				attributeName: 'autoblur',
+				propertyName: 'autoBlur',
+				value: value => value === 'false' || value === '0' ? false : true
+			},
+			{
+				attributeName: 'clearable',
+				value: value => value === 'false' || value === '0' ? false : true
+			},
+			{
+				attributeName: 'openonfocus',
+				propertyName: 'openOnFocus',
+				value: value => value === 'false' || value === '0' ? false : true
+			},
+			{
+				attributeName: 'value'
+			}
+		],
+		properties: [
+			{
+				propertyName: 'results'
+			},
+			{
+				propertyName: 'inputProperties'
+			},
+			{
+				propertyName: 'customResultItem'
+			},
+			{
+				propertyName: 'customResultMenu'
+			},
+			{
+				propertyName: 'getResultLabel'
+			}
 		],
 		events: [
-
+			{
+				propertyName: 'onBlur',
+				eventName: 'blur'
+			},
+			{
+				propertyName: 'onFocus',
+				eventName: 'focus'
+			},
+			{
+				propertyName: 'onChange',
+				eventName: 'change'
+			},
+			{
+				propertyName: 'onRequestResults',
+				eventName: 'requestresults'
+			},
+			{
+				propertyName: 'onMenuChange',
+				eventName: 'menuchange'
+			}
 		]
 	};
 };
