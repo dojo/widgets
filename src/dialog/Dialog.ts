31f854507736b8df9c6eba36e72f1f02984f30ef
Paul Bouchon <paul@paulbouchon.net>
2017-03-07 09:07:24 -0500

    Bump version of grunt-dojo2

--- src/dialog/Dialog.ts
+++ src/dialog/Dialog.ts
@@ -76,7 +76,7 @@ export default class Dialog extends DialogBase<DialogProperties> {
 		return v('div', {}, open ? [
 			v('div', {
 				key: 'underlay',
-				classes: this.classes(css.underlayVisible).fixed(underlay ? css.underlay : null).get(),
+				classes: this.classes(css.underlay).fixed(underlay ? css.underlayVisible : null).get(),
 				enterAnimation: animations.fadeIn,
 				exitAnimation: animations.fadeOut,
 				onclick: this.onUnderlayClick
