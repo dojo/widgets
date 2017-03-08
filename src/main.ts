770bc47eeddb7d251b934f74df589d3265dc40af
smhigley <shigley@sitepen.com>
2017-03-08 14:17:06 -0700

    changed to consistent root class

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
 import Dialog from './dialog/Dialog';
 import SlidePane from './slidepane/SlidePane';
 import Label from './label/Label';
-<<<<<<< HEAD
 import TextInput from './textinput/TextInput';
 import Textarea from './textarea/Textarea';
 import Checkbox from './checkbox/Checkbox';
 import Radio from './radio/Radio';
-=======
 import Select from './select/Select';
->>>>>>> select unit tests
 
 export {
 	Dialog,
