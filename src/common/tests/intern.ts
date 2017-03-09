f01b0f8c115cc19ddf64edca2270f3bc8c4ba847
Anthony Gubler <agubler@sitepen.com>
2017-03-09 09:49:29 +0000

    add polyfill config for intern and examples

--- src/common/tests/intern.ts
+++ src/common/tests/intern.ts
@@ -56,6 +56,7 @@ export const loaderOptions = {
 		{ name: 'dojo', location: 'node_modules/intern/node_modules/dojo' },
 		{ name: '@dojo', location: 'node_modules/@dojo' },
 		{ name: 'grunt-dojo2', location: 'node_modules/grunt-dojo2'},
+		{ name: 'pepjs', location: 'node_modules/pepjs/dist', main: 'pep' },
 		{ name: 'maquette', location: 'node_modules/maquette/dist', main: 'maquette' }
 	]
 };
