2767986a18cc686c5c15df8264d9b13ac9b00820
Kitson Kelly <me@kitsonkelly.com>
2017-04-10 15:08:04 +0100

    Uncomment test platforms for SauceLabs

--- src/common/tests/intern-saucelabs.ts
+++ src/common/tests/intern-saucelabs.ts
@@ -1,11 +1,11 @@
 export * from './intern';
 
 export const environments = [
-	// { browserName: 'internet explorer', version: [ '11.0' ], platform: 'Windows 7' },
-	// { browserName: 'MicrosoftEdge', platform: 'Windows 10' },
-	// { browserName: 'firefox', platform: 'Windows 10' },
-	// { browserName: 'chrome', platform: 'Windows 10' },
-	// { browserName: 'safari', version: '9.0', platform: 'OS X 10.11' },
+	{ browserName: 'internet explorer', version: [ '11.0' ], platform: 'Windows 7' },
+	{ browserName: 'MicrosoftEdge', platform: 'Windows 10' },
+	{ browserName: 'firefox', platform: 'Windows 10' },
+	{ browserName: 'chrome', platform: 'Windows 10' },
+	{ browserName: 'safari', version: '9.0', platform: 'OS X 10.11' },
 	// Android platforms currently failing travis as saucelabs is not returning within a reasonable time
 	// { browserName: 'android', version: '5.1' }
 	{ browserName: 'iphone', version: '9.3' }
