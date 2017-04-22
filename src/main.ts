02d2fae1301f8cb8c29d6b7f9174229ca6962bfe
Paul Bouchon <paul@paulbouchon.net>
2017-04-22 09:19:44 -0400

    Fix directory name

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import Radio from './radio/Radio';
 import Select from './select/Select';
 import SlidePane from './slidepane/SlidePane';
 import Slider from './slider/Slider';
-import SplitPane from './splitPane/SplitPane';
+import SplitPane from './splitpane/SplitPane';
 import TabPane from './tabpane/TabPane';
 import Textarea from './textarea/Textarea';
 import TextInput from './textinput/TextInput';
