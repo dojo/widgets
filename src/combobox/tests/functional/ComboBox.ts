d62ae0058dbbf604119dddab7d2acce6069f06c7
Matt Wistrand <matt@troglodytidae.com>
2017-05-05 11:34:42 -0500

    Update ComboBox functional tests.
    - Use SeleniumTunnel for tests.
    - Use CSS module for ComboBox functional tests.
    - Skip keyboard tests that do not work in FF.
    - Fix tests to execute more accurately across supported browsers.

--- src/combobox/tests/functional/ComboBox.ts
+++ src/combobox/tests/functional/ComboBox.ts
@@ -1,6 +1,9 @@
 import * as registerSuite from 'intern!object';
 import * as assert from 'intern/chai!assert';
 import * as keys from 'leadfoot/keys';
+import * as css from '../../styles/comboBox.m.css';
+
+const DELAY = 300;
 
 function getPage(remote: any) {
 	return remote
@@ -11,65 +14,92 @@ function getPage(remote: any) {
 registerSuite({
 	name: 'ComboBox',
 
-	'the controls should be visible'(this: any) {
+	'the controls should be over the right end of the input'(this: any) {
+		let clearButtonSize: { height: number; width: number; };
+		let closeButtonSize: { height: number; width: number; };
+		let inputPosition: { x: number; y: number };
+		let inputSize: { height: number; width: number; };
+
 		return getPage(this.remote)
-			.findByCssSelector('[role=combobox]:nth-of-type(2) input')
-				.isDisplayed()
-				.then((displayed: boolean) => {
-					assert.isTrue(displayed, 'The input should be displayed.');
-				})
+			.findByCssSelector(`.${css.input}`)
 				.getSize()
-				.then(({ height, width }: { height: number; width: number; }) => {
-					assert.isAbove(height, 0, 'The input height should be greater than zero.');
-					assert.isAbove(width, 0, 'The input width should be greater than zero.');
-				})
+					.then(({ height, width }: { height: number; width: number; }) => {
+						inputSize = { height, width };
+					})
+				.getPosition()
+					.then((position: { x: number; y: number; }) => {
+						inputPosition = position;
+					})
 				.end()
-			.findByCssSelector('[role=combobox]:nth-of-type(2) button')
-				.isDisplayed()
-				.then((displayed: boolean) => {
-					assert.isTrue(displayed, 'The "clear" button should be displayed.');
-				})
+			.findByCssSelector(`.${css.arrow}`)
 				.getSize()
-				.then(({ height, width }: { height: number; width: number; }) => {
-					assert.isAbove(height, 0, 'The "clear" button height should be greater than zero.');
-					assert.isAbove(width, 0, 'The "clear" button width should be greater than zero.');
-				})
+					.then(({ height, width }: { height: number; width: number; }) => {
+						closeButtonSize = { height, width };
+					})
+				.getPosition()
+					.then(({ x, y }: { x: number; y: number; }) => {
+						const expectedX = inputPosition.x + inputSize.width - closeButtonSize.width;
+
+						assert.closeTo(x, expectedX, 10, 'The close button should be near the right edge of the input.');
+						assert.isAbove(y, inputPosition.y);
+						assert.isBelow(y + closeButtonSize.height, inputPosition.y + inputSize.height);
+					})
 				.end()
-			.findByCssSelector('[role=combobox]:nth-of-type(2) button:nth-of-type(2)')
-				.isDisplayed()
-				.then((displayed: boolean) => {
-					assert.isTrue(displayed, 'The "open" button should be displayed.');
-				})
+			.findByCssSelector(`.${css.clear}`)
 				.getSize()
-				.then(({ height, width }: { height: number; width: number; }) => {
-					assert.isAbove(height, 0, 'The "open" button height should be greater than zero.');
-					assert.isAbove(width, 0, 'The "open" button width should be greater than zero.');
-				});
+					.then(({ height, width }: { height: number; width: number; }) => {
+						clearButtonSize = { height, width };
+					})
+				.getPosition()
+					.then(({ x, y }: { x: number; y: number; }) => {
+						const expectedX = inputPosition.x + inputSize.width -
+							closeButtonSize.width - clearButtonSize.width;
+
+						assert.closeTo(x, expectedX, 20, 'The clear button should be near the left edge of the close button.');
+						assert.isAbove(y, inputPosition.y);
+						assert.isBelow(y + clearButtonSize.height, inputPosition.y + inputSize.height);
+					});
 	},
 
 	'the results menu should be visible'(this: any) {
+		let inputWidth: number;
+
 		return getPage(this.remote)
-			.findByCssSelector('[role=combobox] input')
+			.findByCssSelector(`.${css.input}`)
+				.getSize()
+					.then(({ width }: { width: number; }) => {
+						inputWidth = width;
+					})
+				.end()
+			.findByCssSelector(`.${css.arrow}`)
 				.click()
 				.end()
-			.findByCssSelector('[role=listbox]')
-				.isDisplayed()
-				.then((displayed: boolean) => {
-					assert.isTrue(displayed, 'The menu should be visible.');
+			.sleep(DELAY)
+			.findByCssSelector(`.${css.results}`)
+				.getSize()
+				.then(({ height, width }: { height: number; width: number; }) => {
+					assert.strictEqual(width, inputWidth);
+					assert.isAbove(height, 0);
 				});
 	},
 
 	'the selected result menu should be visible'(this: any) {
+		if (this.remote.session.capabilities.browserName === 'firefox') {
+			this.skip('FirefoxDriver sends actual charcodes to the input.');
+		}
+
 		let menuBottom: number;
 		let menuTop: number;
 		let itemHeight: number;
 
 		return getPage(this.remote)
-			.findByCssSelector('[role=combobox] input')
+			.findByCssSelector(`.${css.arrow}`)
 				.click()
+				.end()
+			.findByCssSelector(`.${css.input}`)
 				.pressKeys(keys.ARROW_UP)
 				.end()
-			.findByCssSelector('[role=listbox]')
+			.findByCssSelector(`.${css.results}`)
 				.getPosition()
 				.then(({ y }: { y: number; }) => {
 					menuTop = y;
@@ -79,7 +109,7 @@ registerSuite({
 					menuBottom = menuTop + height;
 				})
 				.end()
-			.findByCssSelector('[role=listbox] [data-selected=true]')
+			.findByCssSelector(`.${css.selectedResult}`)
 				.getSize()
 				.then(({ height }: { height: number }) => {
 					itemHeight = height;
@@ -87,15 +117,27 @@ registerSuite({
 				.getPosition()
 				.then(({ y }: { y: number; }) => {
 					assert.isAtLeast(y, menuTop);
-					assert.isAtMost(y, menuBottom - itemHeight);
+					assert.isAtMost(Math.floor(y), Math.ceil(menuBottom - itemHeight));
 				});
 	},
 
 	'tab order'(this: any) {
+		const { browserName } = this.remote.session.capabilities;
+
+		if (browserName === 'safari') {
+			// TODO: https://github.com/seleniumhq/selenium-google-code-issue-archive/issues/5403
+			this.skip('SafariDriver does not move focus with tab key.');
+		}
+
+		if (browserName === 'firefox') {
+			this.skip('FirefoxDriver sends actual charcodes to the input.');
+		}
+
 		return getPage(this.remote)
-			.findByCssSelector('[role=combobox]:nth-of-type(2) input')
+			.findByCssSelector(`.${css.arrow}`)
 				.click()
-				.pressKeys([ keys.ARROW_DOWN, keys.TAB ])
+				.sleep(DELAY)
+				.pressKeys(keys.TAB)
 			.getActiveElement()
 				.getProperty('textContent')
 				.then((text: string) => {
@@ -116,11 +158,13 @@ registerSuite({
 	},
 
 	'the input should receive focus when the "clear" button is activated'(this: any) {
+		if (this.remote.session.capabilities.browserName === 'safari') {
+			// TODO: https://github.com/seleniumhq/selenium-google-code-issue-archive/issues/5403
+			this.skip('SafariDriver does not move focus with tab key.');
+		}
+
 		return getPage(this.remote)
-			.findByCssSelector('[role=combobox]:nth-of-type(2) input')
-				.click()
-				.pressKeys(keys.TAB)
-			.getActiveElement()
+			.findByCssSelector(`.${css.clear}`)
 				.click()
 				.sleep(30)
 			.getActiveElement()
@@ -140,11 +184,13 @@ registerSuite({
 	},
 
 	'the input should receive focus when the "open" button is activated'(this: any) {
+		if (this.remote.session.capabilities.browserName === 'safari') {
+			// TODO: https://github.com/seleniumhq/selenium-google-code-issue-archive/issues/5403
+			this.skip('SafariDriver does not move focus with tab key.');
+		}
+
 		return getPage(this.remote)
-			.findByCssSelector('[role=combobox] input')
-				.click()
-				.pressKeys(keys.TAB)
-			.getActiveElement()
+			.findByCssSelector(`.${css.arrow}`)
 				.click()
 				.sleep(30)
 			.getActiveElement()
