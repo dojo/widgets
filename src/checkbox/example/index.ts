bad19973625b15c9fa0b915bb75c568c49e207dc
smhigley <shigley@sitepen.com>
2017-06-06 16:28:13 -0700

    fixed event typings and toggle mode label

--- src/checkbox/example/index.ts
+++ src/checkbox/example/index.ts
@@ -1,5 +1,5 @@
 import { WidgetBase } from '@dojo/widget-core/WidgetBase';
-import { WidgetProperties } from '@dojo/widget-core/interfaces';
+import { WidgetProperties, TypedTargetEvent } from '@dojo/widget-core/interfaces';
 import { ProjectorMixin } from '@dojo/widget-core/mixins/Projector';
 import { StatefulMixin } from '@dojo/widget-core/mixins/Stateful';
 import { v, w } from '@dojo/widget-core/d';
@@ -10,15 +10,15 @@ export const AppBase = StatefulMixin(WidgetBase);
 export class App extends AppBase<WidgetProperties> {
 	private _theme: {};
 
-	themeChange(event: Event) {
-		const checked = (<HTMLInputElement> event.target).checked;
+	themeChange(event: TypedTargetEvent<HTMLInputElement>) {
+		const checked = event.target.checked;
 		this._theme = checked ? dojoTheme : {};
 		this.invalidate();
 	}
 
-	onChange(event: Event) {
-		const value = (<HTMLInputElement> event.target).value;
-		const checked = (<HTMLInputElement> event.target).checked;
+	onChange(event: TypedTargetEvent<HTMLInputElement>) {
+		const value = event.target.value;
+		const checked = event.target.checked;
 		this.setState({ [value]: checked });
 	}
 
