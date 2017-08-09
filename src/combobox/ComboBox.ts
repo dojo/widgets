81abf3037a76781e59794686e83f87c875c0bd02
Paul Bouchon <paul@paulbouchon.net>
2017-08-09 08:03:33 -0400

    Fix styling after rebase

--- src/combobox/ComboBox.ts
+++ src/combobox/ComboBox.ts
@@ -372,7 +372,7 @@ export default class ComboBox extends ComboBoxBase<ComboBoxProperties> {
 				classes: this.classes(clearable ? css.clearable : null),
 				controls: menuId,
 				disabled,
-				extraClasses: css as any,
+				// extraClasses: css as any,
 				invalid,
 				onBlur: this._onInputBlur,
 				onFocus: this._onInputFocus,
