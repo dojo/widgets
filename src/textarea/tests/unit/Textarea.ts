1a681a589a6a4e858255599e98c717f5cf0e6a94
smhigley <shigley@sitepen.com>
2017-08-18 16:51:30 -0700

    updated textarea tests to use test-extras

--- src/textarea/tests/unit/Textarea.ts
+++ src/textarea/tests/unit/Textarea.ts
@@ -1,131 +1,203 @@
 import * as registerSuite from 'intern!object';
 import * as assert from 'intern/chai!assert';
-import { VNode } from '@dojo/interfaces/vdom';
-import Textarea from '../../Textarea';
+
+import has from '@dojo/has/has';
+import { v, w } from '@dojo/widget-core/d';
+import { assignProperties, assignChildProperties } from '@dojo/test-extras/support/d';
+import harness, { Harness } from '@dojo/test-extras/harness';
+
+import Label from '../../../label/Label';
+import Textarea, { TextareaProperties } from '../../Textarea';
 import * as css from '../../styles/textarea.m.css';
+
+const hasTouch = (function (): boolean {
+	/* Since jsdom will fake it anyways, no problem pretending we can do touch in NodeJS */
+	return Boolean('ontouchstart' in window || has('host-node'));
+})();
+
+const expected = function(widget: any, label = false) {
+	const textareaVdom = v('div', { classes: widget.classes(css.inputWrapper) }, [
+		v('textarea', {
+			classes: widget.classes(css.input),
+			cols: null,
+			'aria-describedby': undefined,
+			disabled: undefined,
+			'aria-invalid': null,
+			maxlength: null,
+			minlength: null,
+			name: undefined,
+			placeholder: undefined,
+			readOnly: undefined,
+			'aria-readonly': null,
+			required: undefined,
+			rows: null,
+			value: undefined,
+			wrap: undefined,
+			onblur: widget.listener,
+			onchange: widget.listener,
+			onclick: widget.listener,
+			onfocus: widget.listener,
+			oninput: widget.listener,
+			onkeydown: widget.listener,
+			onkeypress: widget.listener,
+			onkeyup: widget.listener,
+			onmousedown: widget.listener,
+			onmouseup: widget.listener,
+			ontouchstart: widget.listener,
+			ontouchend: widget.listener,
+			ontouchcancel: widget.listener
+		})
+	]);
+
+	if (label) {
+		return w(Label, {
+			extraClasses: { root: css.root },
+			label: 'foo',
+			formId: undefined,
+			theme: undefined
+		}, [ textareaVdom ]);
+	}
+	else {
+		return v('div', {
+			classes: widget.classes(css.root)
+		}, [ textareaVdom ]);
+	}
+};
+
+let widget: Harness<TextareaProperties, typeof Textarea>;
 
 registerSuite({
 	name: 'Textarea',
 
-	construction() {
-		const textarea = new Textarea();
-		textarea.__setProperties__({
-			columns: 30,
-			rows: 10,
-			wrapText: 'hard',
-			placeholder: 'bar',
-			value: 'baz'
-		});
-
-		assert.strictEqual(textarea.properties.columns, 30);
-		assert.strictEqual(textarea.properties.rows, 10);
-		assert.strictEqual(textarea.properties.wrapText, 'hard');
-		assert.strictEqual(textarea.properties.placeholder, 'bar');
-		assert.strictEqual(textarea.properties.value, 'baz');
+	beforeEach() {
+		widget = harness(Textarea);
 	},
 
-	'correct node attributes'() {
-		const textarea = new Textarea();
-		let vnode = <VNode> textarea.__render__();
+	afterEach() {
+		widget.destroy();
+	},
 
-		assert.strictEqual(vnode.children![0].children![0].vnodeSelector, 'textarea');
-		assert.isUndefined(vnode.properties!.cols);
-		assert.isUndefined(vnode.properties!.rows);
-		assert.isUndefined(vnode.properties!.wrap);
+	'default properties'() {
+		widget.expectRender(expected(widget));
+	},
 
-		textarea.__setProperties__({
-			columns: 30,
-			rows: 10,
-			wrapText: 'hard',
-			describedBy: 'id1',
-			disabled: true,
-			formId: 'id2',
-			invalid: true,
-			label: 'foo',
+	'custom properties'() {
+		widget.setProperties({
+			columns: 15,
+			describedBy: 'foo',
 			maxLength: 50,
-			minLength: 5,
+			minLength: 10,
 			name: 'bar',
 			placeholder: 'baz',
-			readOnly: true,
-			required: true,
-			value: 'qux'
+			rows: 42,
+			value: 'qux',
+			wrapText: 'soft'
 		});
-		vnode = <VNode> textarea.__render__();
-		const labelNode = vnode.children![0];
-		const inputNode = vnode.children![1].children![0];
 
-		assert.strictEqual(inputNode.properties!.cols, '30');
-		assert.strictEqual(inputNode.properties!.rows, '10');
-		assert.strictEqual(inputNode.properties!.wrap, 'hard');
-		assert.strictEqual(inputNode.properties!['aria-describedby'], 'id1');
-		assert.isTrue(inputNode.properties!.disabled);
-		assert.strictEqual(inputNode.properties!['aria-invalid'], 'true');
-		assert.strictEqual(inputNode.properties!.maxlength, '50');
-		assert.strictEqual(inputNode.properties!.minlength, '5');
-		assert.strictEqual(inputNode.properties!.name, 'bar');
-		assert.strictEqual(inputNode.properties!.placeholder, 'baz');
-		assert.isTrue(inputNode.properties!.readOnly);
-		assert.strictEqual(inputNode.properties!['aria-readonly'], 'true');
-		assert.isTrue(inputNode.properties!.required);
-		assert.strictEqual(inputNode.properties!.value, 'qux');
+		const expectedVdom = expected(widget);
+		assignChildProperties(expectedVdom, '0,0', {
+			cols: '15',
+			'aria-describedby': 'foo',
+			maxlength: '50',
+			minlength: '10',
+			name: 'bar',
+			placeholder: 'baz',
+			rows: '42',
+			value: 'qux',
+			wrap: 'soft'
+		});
 
-		assert.strictEqual(vnode.properties!['form'], 'id2');
-		assert.strictEqual(labelNode.properties!.innerHTML, 'foo');
+		widget.expectRender(expectedVdom);
+	},
+
+	'label'() {
+		widget.setProperties({
+			label: 'foo'
+		});
+
+		widget.expectRender(expected(widget, true));
 	},
 
 	'state classes'() {
-		const textarea = new Textarea();
-		textarea.__setProperties__({
-			disabled: true,
+		widget.setProperties({
 			invalid: true,
+			disabled: true,
 			readOnly: true,
 			required: true
 		});
-		let vnode = <VNode> textarea.__render__();
 
-		assert.isTrue(vnode.properties!.classes![css.disabled]);
-		assert.isTrue(vnode.properties!.classes![css.invalid]);
-		assert.isTrue(vnode.properties!.classes![css.readonly]);
-		assert.isTrue(vnode.properties!.classes![css.required]);
+		let expectedVdom = expected(widget);
+		assignChildProperties(expectedVdom, '0,0', {
+			disabled: true,
+			'aria-invalid': 'true',
+			readOnly: true,
+			'aria-readonly': 'true',
+			required: true
+		});
+		assignProperties(expectedVdom, {
+			classes: widget.classes(css.root, css.invalid, css.disabled, css.readonly, css.required)
+		});
 
-		textarea.__setProperties__({
-			disabled: false,
+		widget.expectRender(expectedVdom, 'Widget should be invalid, disabled, read-only, and required');
+
+		widget.setProperties({
 			invalid: false,
+			disabled: false,
 			readOnly: false,
 			required: false
 		});
-		vnode = <VNode> textarea.__render__();
-		assert.isFalse(vnode.properties!.classes![css.disabled]);
-		assert.isTrue(vnode.properties!.classes![css.valid]);
-		assert.isFalse(vnode.properties!.classes![css.invalid]);
-		assert.isFalse(vnode.properties!.classes![css.readonly]);
-		assert.isFalse(vnode.properties!.classes![css.required]);
+		expectedVdom = expected(widget);
 
-		textarea.__setProperties__({
-			invalid: undefined
+		assignChildProperties(expectedVdom, '0,0', {
+			disabled: false,
+			readOnly: false,
+			required: false
 		});
-		vnode = <VNode> textarea.__render__();
-		assert.isFalse(vnode.properties!.classes![css.valid]);
-		assert.isFalse(vnode.properties!.classes![css.invalid]);
+		assignProperties(expectedVdom, {
+			classes: widget.classes(css.root, css.valid)
+		});
+
+		widget.expectRender(expectedVdom, 'State classes should be false, css.valid should be true');
+	},
+
+	'state classes on label'() {
+		widget.setProperties({
+			label: 'foo',
+			formId: 'bar',
+			invalid: true,
+			disabled: true,
+			readOnly: true,
+			required: true
+		});
+
+		const expectedVdom = expected(widget, true);
+		assignChildProperties(expectedVdom, '0,0', {
+			disabled: true,
+			'aria-invalid': 'true',
+			readOnly: true,
+			'aria-readonly': 'true',
+			required: true
+		});
+		assignProperties(expectedVdom, {
+			extraClasses: { root: `${css.root} ${css.disabled} ${css.invalid} ${css.readonly} ${css.required}` },
+			formId: 'bar'
+		});
+		widget.expectRender(expectedVdom);
 	},
 
 	events() {
-		let blurred = false,
-				changed = false,
-				clicked = false,
-				focused = false,
-				input = false,
-				keydown = false,
-				keypress = false,
-				keyup = false,
-				mousedown = false,
-				mouseup = false,
-				touchstart = false,
-				touchend = false,
-				touchcancel = false;
+		let blurred = false;
+		let changed = false;
+		let clicked = false;
+		let focused = false;
+		let input = false;
+		let keydown = false;
+		let keypress = false;
+		let keyup = false;
+		let mousedown = false;
+		let mouseup = false;
 
-		const textarea = new Textarea();
-		textarea.__setProperties__({
+		widget.setProperties({
 			onBlur: () => { blurred = true; },
 			onChange: () => { changed = true; },
 			onClick: () => { clicked = true; },
@@ -135,37 +207,51 @@ registerSuite({
 			onKeyPress: () => { keypress = true; },
 			onKeyUp: () => { keyup = true; },
 			onMouseDown: () => { mousedown = true; },
-			onMouseUp: () => { mouseup = true; },
+			onMouseUp: () => { mouseup = true; }
+		});
+
+		widget.sendEvent('blur', { selector: 'textarea' });
+		assert.isTrue(blurred, 'Didn\'t run onBlur');
+		widget.sendEvent('change', { selector: 'textarea' });
+		assert.isTrue(changed, 'Didn\'t run onChange');
+		widget.sendEvent('click', { selector: 'textarea' });
+		assert.isTrue(clicked, 'Didn\'t run onClick');
+		widget.sendEvent('focus', { selector: 'textarea' });
+		assert.isTrue(focused, 'Didn\'t run onFocus');
+		widget.sendEvent('input', { selector: 'textarea' });
+		assert.isTrue(input, 'Didn\'t run onInpput');
+		widget.sendEvent('keydown', { selector: 'textarea' });
+		assert.isTrue(keydown, 'Didn\'t run onKeyDown');
+		widget.sendEvent('keypress', { selector: 'textarea' });
+		assert.isTrue(keypress, 'Didn\'t run onKeyPress');
+		widget.sendEvent('keyup', { selector: 'textarea' });
+		assert.isTrue(keyup, 'Didn\'t run onKeyUp');
+		widget.sendEvent('mousedown', { selector: 'textarea' });
+		assert.isTrue(mousedown, 'Didn\'t run onMouseDown');
+		widget.sendEvent('mouseup', { selector: 'textarea' });
+		assert.isTrue(mouseup, 'Didn\'t run onMouseUp');
+	},
+
+	'touch events'(this: any) {
+		if (!hasTouch) {
+			this.skip('Environment not support touch events');
+		}
+
+		let touchstart = false;
+		let touchend = false;
+		let touchcancel = false;
+
+		widget.setProperties({
 			onTouchStart: () => { touchstart = true; },
 			onTouchEnd: () => { touchend = true; },
 			onTouchCancel: () => { touchcancel = true; }
 		});
 
-		(<any> textarea)._onBlur(<FocusEvent> {});
-		assert.isTrue(blurred);
-		(<any> textarea)._onChange(<Event> {});
-		assert.isTrue(changed);
-		(<any> textarea)._onClick(<MouseEvent> {});
-		assert.isTrue(clicked);
-		(<any> textarea)._onFocus(<FocusEvent> {});
-		assert.isTrue(focused);
-		(<any> textarea)._onInput(<Event> {});
-		assert.isTrue(input);
-		(<any> textarea)._onKeyDown(<KeyboardEvent> {});
-		assert.isTrue(keydown);
-		(<any> textarea)._onKeyPress(<KeyboardEvent> {});
-		assert.isTrue(keypress);
-		(<any> textarea)._onKeyUp(<KeyboardEvent> {});
-		assert.isTrue(keyup);
-		(<any> textarea)._onMouseDown(<MouseEvent> {});
-		assert.isTrue(mousedown);
-		(<any> textarea)._onMouseUp(<MouseEvent> {});
-		assert.isTrue(mouseup);
-		(<any> textarea)._onTouchStart(<TouchEvent> {});
-		assert.isTrue(touchstart);
-		(<any> textarea)._onTouchEnd(<TouchEvent> {});
-		assert.isTrue(touchend);
-		(<any> textarea)._onTouchCancel(<TouchEvent> {});
-		assert.isTrue(touchcancel);
+		widget.sendEvent('touchstart', { selector: 'textarea' });
+		assert.isTrue(touchstart, 'Didn\'t run onTouchStart');
+		widget.sendEvent('touchend', { selector: 'textarea' });
+		assert.isTrue(touchend, 'Didn\'t run onTouchEnd');
+		widget.sendEvent('touchcancel', { selector: 'textarea' });
+		assert.isTrue(touchcancel, 'Didn\'t run onTouchCancel');
 	}
 });
