4678025ba0b989139721c372aa289f51492e75bb
smhigley <shigley@sitepen.com>
2017-08-31 09:22:30 -0700

    update has tests

--- src/textinput/tests/unit/TextInput.ts
+++ src/textinput/tests/unit/TextInput.ts
@@ -11,11 +11,6 @@ import Label from '../../../label/Label';
 import TextInput, { TextInputProperties } from '../../TextInput';
 import * as css from '../../styles/textinput.m.css';
 
-const hasTouch = (function (): boolean {
-	/* Since jsdom will fake it anyways, no problem pretending we can do touch in NodeJS */
-	return Boolean('ontouchstart' in window || has('host-node'));
-})();
-
 const expected = function(widget: any, label = false) {
 	const inputVdom = v('div', { classes: widget.classes(css.inputWrapper) }, [
 		v('input', {
@@ -229,7 +224,7 @@ registerSuite({
 	},
 
 	'touch events'(this: any) {
-		if (!hasTouch) {
+		if (!has('touch')) {
 			this.skip('Environment not support touch events');
 		}
 
