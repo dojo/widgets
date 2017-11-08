42298486df964c463581d994c878536075ab22d1
Neil Roberts <neil@onebahamas.com>
2017-11-08 14:34:46 -0600

    Textarea functional tests (#324)
    * Add Textarea functional tests

--- src/textarea/example/index.ts
+++ src/textarea/example/index.ts
@@ -9,7 +9,7 @@ export class App extends WidgetBase<WidgetProperties> {
 	private _theme: {};
 	private _value1: string;
 	private _value2: string;
-	private _invalid = false;
+	private _invalid: boolean;
 
 	themeChange(event: TypedTargetEvent<HTMLInputElement>) {
 		const checked = event.target.checked;
@@ -27,46 +27,65 @@ export class App extends WidgetBase<WidgetProperties> {
 					onchange: this.themeChange
 				})
 			]),
-			w(Textarea, {
-				key: 't1',
-				columns: 40,
-				rows: 8,
-				placeholder: 'Hello, World',
-				label: 'Type Something',
-				value: this._value1,
-				onChange: (event: TypedTargetEvent<HTMLInputElement>) => {
-					this._value1 = event.target.value;
-					this.invalidate();
-				},
-				theme: this._theme
-			}),
+			v('div', { id: 'example-t1'}, [
+				w(Textarea, {
+					key: 't1',
+					columns: 40,
+					rows: 8,
+					placeholder: 'Hello, World',
+					label: 'Type Something',
+					value: this._value1,
+					onChange: (event: TypedTargetEvent<HTMLInputElement>) => {
+						this._value1 = event.target.value;
+						this.invalidate();
+					},
+					theme: this._theme
+				})
+			]),
 			v('h3', {}, ['Disabled Textarea']),
-			w(Textarea, {
-				key: 't2',
-				columns: 40,
-				rows: 3,
-				label: 'Can\'t type here',
-				value: 'Initial value',
-				disabled: true,
-				theme: this._theme
-			}),
+			v('div', { id: 'example-t2'}, [
+				w(Textarea, {
+					key: 't2',
+					columns: 40,
+					rows: 3,
+					label: 'Can\'t type here',
+					value: 'Initial value',
+					disabled: true,
+					theme: this._theme
+				})
+			]),
 			v('h3', {}, ['Validated, Required Textarea']),
-			w(Textarea, {
-				key: 't3',
-				columns: 40,
-				rows: 8,
-				label: 'Required',
-				required: true,
-				value: this._value2,
-				invalid: this._invalid,
-				onChange: (event: TypedTargetEvent<HTMLInputElement>) => {
-					const value = event.target.value;
-					this._value2 = value;
-					this._invalid = value.trim().length === 0;
-					this.invalidate();
-				},
-				theme: this._theme
-			})
+			v('div', { id: 'example-t3'}, [
+				w(Textarea, {
+					key: 't3',
+					columns: 40,
+					rows: 8,
+					label: 'Required',
+					required: true,
+					value: this._value2,
+					invalid: this._invalid,
+					onChange: (event: TypedTargetEvent<HTMLInputElement>) => {
+						const value = event.target.value;
+						this._value2 = value;
+						this._invalid = value.trim().length === 0;
+						this.invalidate();
+					},
+					theme: this._theme
+				})
+			]),
+			v('h3', {}, ['Hidden Label Textarea']),
+			v('div', { id: 'example-t4'}, [
+				w(Textarea, {
+					key: 't4',
+					columns: 40,
+					rows: 8,
+					label: {
+						content: 'Hidden label',
+						before: false,
+						hidden: true
+					}
+				})
+			])
 		]);
 	}
 }
