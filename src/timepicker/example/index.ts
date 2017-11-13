e2609c0653409efce976326fa8c5dc18f1feffdb
Neil Roberts <neil@onebahamas.com>
2017-11-13 10:34:30 -0600

    Add TimePicker functional tests (#368)
    * Add TimePicker functional tests

--- src/timepicker/example/index.ts
+++ src/timepicker/example/index.ts
@@ -54,207 +54,227 @@ export class App extends ThemedMixin(WidgetBase)<ThemedProperties> {
 			}, [ 'Accepts 24-hour time with a leading zero, rounded to the nearest half hour.' ]),
 
 			v('h3', [ 'Filter options on input' ]),
-			w(TimePicker, {
-				inputProperties: {
-					describedBy: 'description1',
-					placeholder: 'Enter a value'
-				},
-				key: '1',
-				onChange: (value: string) => {
-					this._setValue('value1', value);
-				},
-				onRequestOptions: (value: string, options: TimeUnits[]) => {
-					if (!value) {
-						this._options = options;
-					}
-					else {
+			v('div', { id: 'example-filter-on-input' }, [
+				w(TimePicker, {
+					inputProperties: {
+						describedBy: 'description1',
+						placeholder: 'Enter a value'
+					},
+					key: '1',
+					onChange: (value: string) => {
+						this._setValue('value1', value);
+					},
+					onRequestOptions: (value: string, options: TimeUnits[]) => {
+						if (!value) {
+							this._options = options;
+						}
+						else {
 
-						const matching = options.filter(option => {
-							const { hour, minute = 0 } = option;
-							const hours = hour >= 10 ? hour : `0${hour}`;
-							const minutes = minute >= 10 ? minute : `0${minute}`;
-							return `${hours}:${minutes}`.indexOf(value) === 0;
-						});
+							const matching = options.filter(option => {
+								const { hour, minute = 0 } = option;
+								const hours = hour >= 10 ? hour : `0${hour}`;
+								const minutes = minute >= 10 ? minute : `0${minute}`;
+								return `${hours}:${minutes}`.indexOf(value) === 0;
+							});
 
-						this._options = matching.length ? matching : options;
-					}
-					this.invalidate();
-				},
-				options: this._options,
-				step: 1800,
-				theme: this._theme,
-				value: this._values['value1']
-			}),
+							this._options = matching.length ? matching : options;
+						}
+						this.invalidate();
+					},
+					options: this._options,
+					step: 1800,
+					theme: this._theme,
+					value: this._values['value1']
+				})
+			]),
 
 			v('h3', [ 'Open on focus' ]),
-			w(TimePicker, {
-				inputProperties: {
-					describedBy: 'description1',
-					placeholder: 'Enter a value'
-				},
-				key: '2',
-				openOnFocus: true,
-				onChange: (value: string) => {
-					this._setValue('value2', value);
-				},
-				onRequestOptions: this.onRequestOptions,
-				options: this._options,
-				step: 1800,
-				theme: this._theme,
-				value: this._values['value2']
-			}),
+			v('div', { id: 'example-open-on-focus' }, [
+				w(TimePicker, {
+					inputProperties: {
+						describedBy: 'description1',
+						placeholder: 'Enter a value'
+					},
+					key: '2',
+					openOnFocus: true,
+					onChange: (value: string) => {
+						this._setValue('value2', value);
+					},
+					onRequestOptions: this.onRequestOptions,
+					options: this._options,
+					step: 1800,
+					theme: this._theme,
+					value: this._values['value2']
+				})
+			]),
 
 			v('h3', [ 'Disabled menu items' ]),
 			v('p', {
 				id: 'description2',
 				classes: baseCss.visuallyHidden
 			}, [ 'Accepts 24-hour time with a leading zero, rounded to the nearest hour.' ]),
-			w(TimePicker, {
-				inputProperties: {
-					describedBy: 'description2',
-					placeholder: 'Enter a value'
-				},
-				isOptionDisabled: (option: TimeUnits) => option.hour >= 12,
-				key: '3',
-				onChange: (value: string) => {
-					this._setValue('value3', value);
-				},
-				onRequestOptions: this.onRequestOptions,
-				options: this._options,
-				step: 3600,
-				theme: this._theme,
-				value: this._values['value3']
-			}),
+			v('div', { id: 'example-disabled-items' }, [
+				w(TimePicker, {
+					inputProperties: {
+						describedBy: 'description2',
+						placeholder: 'Enter a value'
+					},
+					isOptionDisabled: (option: TimeUnits) => option.hour >= 12,
+					key: '3',
+					onChange: (value: string) => {
+						this._setValue('value3', value);
+					},
+					onRequestOptions: this.onRequestOptions,
+					options: this._options,
+					step: 3600,
+					theme: this._theme,
+					value: this._values['value3']
+				})
+			]),
 
 			v('h3', [ 'Disabled' ]),
-			w(TimePicker, {
-				inputProperties: {
-					describedBy: 'description1',
-					placeholder: 'Enter a value'
-				},
-				key: '4',
-				disabled: true,
-				theme: this._theme
-			}),
+			v('div', { id: 'example-disabled' }, [
+				w(TimePicker, {
+					inputProperties: {
+						describedBy: 'description1',
+						placeholder: 'Enter a value'
+					},
+					key: '4',
+					disabled: true,
+					theme: this._theme
+				})
+			]),
 
 			v('h3', [ 'Read Only' ]),
-			w(TimePicker, {
-				inputProperties: {
-					describedBy: 'description1',
-					placeholder: 'Enter a value'
-				},
-				key: '5',
-				readOnly: true,
-				theme: this._theme
-			}),
+			v('div', { id: 'example-readonly' }, [
+				w(TimePicker, {
+					inputProperties: {
+						describedBy: 'description1',
+						placeholder: 'Enter a value'
+					},
+					key: '5',
+					readOnly: true,
+					theme: this._theme
+				})
+			]),
 
-			v('h3', [ 'Label' ]),
-			w(TimePicker, {
-				key: '6',
-				inputProperties: {
-					describedBy: 'description1'
-				},
-				label: 'Enter a value',
-				onChange: (value: string) => {
-					this._setValue('value6', value);
-				},
-				onRequestOptions: this.onRequestOptions,
-				options: this._options,
-				step: 1800,
-				theme: this._theme,
-				value: this._values['value6']
-			}),
+			v('h3', [ 'Labeled' ]),
+			v('div', { id: 'example-labeled' }, [
+				w(TimePicker, {
+					key: '6',
+					inputProperties: {
+						describedBy: 'description1'
+					},
+					label: 'Enter a value',
+					onChange: (value: string) => {
+						this._setValue('value6', value);
+					},
+					onRequestOptions: this.onRequestOptions,
+					options: this._options,
+					step: 1800,
+					theme: this._theme,
+					value: this._values['value6']
+				})
+			]),
 
 			v('h3', [ 'Required and validated' ]),
-			w(TimePicker, {
-				inputProperties: {
-					describedBy: 'description1',
-					placeholder: 'Enter a value'
-				},
-				invalid: this._invalid,
-				key: '7',
-				required: true,
-				onBlur: (value: string) => {
-					this._invalid = value.trim().length === 0;
-					this.invalidate();
-				},
-				onChange: (value: string) => {
-					this._invalid = value.trim().length === 0;
-					this._setValue('value7', value);
-				},
-				onRequestOptions: this.onRequestOptions,
-				options: this._options,
-				step: 1800,
-				theme: this._theme,
-				value: this._values['value7']
-			}),
+			v('div', { id: 'example-required-validated' }, [
+				w(TimePicker, {
+					inputProperties: {
+						describedBy: 'description1',
+						placeholder: 'Enter a value'
+					},
+					invalid: this._invalid,
+					key: '7',
+					required: true,
+					onBlur: (value: string) => {
+						this._invalid = value.trim().length === 0;
+						this.invalidate();
+					},
+					onChange: (value: string) => {
+						this._invalid = value.trim().length === 0;
+						this._setValue('value7', value);
+					},
+					onRequestOptions: this.onRequestOptions,
+					options: this._options,
+					step: 1800,
+					theme: this._theme,
+					value: this._values['value7']
+				})
+			]),
 
 			v('h3', [ 'One second increment' ]),
 			v('p', {
 				id: 'description8',
 				classes: baseCss.visuallyHidden
 			}, [ 'Accepts 24-hour time with a leading zero, rounded to the nearest second.' ]),
-			w(TimePicker, {
-				end: '12:00:59',
-				inputProperties: {
-					describedBy: 'description8',
-					placeholder: 'Enter a value'
-				},
-				key: '8',
-				options: this._options,
-				onChange: (value: string) => {
-					this._setValue('value8', value);
-				},
-				onRequestOptions: this.onRequestOptions,
-				start: '12:00:00',
-				step: 1,
-				theme: this._theme,
-				value: this._values['value8']
-			}),
+			v('div', { id: 'example-every-second' }, [
+				w(TimePicker, {
+					end: '12:00:59',
+					inputProperties: {
+						describedBy: 'description8',
+						placeholder: 'Enter a value'
+					},
+					key: '8',
+					options: this._options,
+					onChange: (value: string) => {
+						this._setValue('value8', value);
+					},
+					onRequestOptions: this.onRequestOptions,
+					start: '12:00:00',
+					step: 1,
+					theme: this._theme,
+					value: this._values['value8']
+				})
+			]),
 
 			v('h3', [ 'Use 12-hour time' ]),
 			v('p', {
 				id: 'description9',
 				classes: baseCss.visuallyHidden
 			}, [ 'Accepts 12-hour time without a leading zero, rounded to the nearest half hour.' ]),
-			w(TimePicker, {
-				getOptionLabel: (option: TimeUnits) => {
-					TODAY.setHours(option.hour);
-					TODAY.setMinutes(option.minute as number);
-					return getEnglishTime(TODAY);
-				},
-				inputProperties: {
-					describedBy: 'description9',
-					placeholder: 'Enter a value'
-				},
-				key: '9',
-				onChange: (value: string) => {
-					this._setValue('value9', value );
-				},
-				onRequestOptions: this.onRequestOptions,
-				options: this._options,
-				step: 1800,
-				theme: this._theme,
-				value: this._values['value9']
-			}),
+			v('div', { id: 'example-12-hour' }, [
+				w(TimePicker, {
+					getOptionLabel: (option: TimeUnits) => {
+						TODAY.setHours(option.hour);
+						TODAY.setMinutes(option.minute as number);
+						return getEnglishTime(TODAY);
+					},
+					inputProperties: {
+						describedBy: 'description9',
+						placeholder: 'Enter a value'
+					},
+					key: '9',
+					onChange: (value: string) => {
+						this._setValue('value9', value );
+					},
+					onRequestOptions: this.onRequestOptions,
+					options: this._options,
+					step: 1800,
+					theme: this._theme,
+					value: this._values['value9']
+				})
+			]),
 
 			v('h3', [ 'Native `<input type="time">`' ]),
-			w(TimePicker, {
-				key: '10',
-				inputProperties: {
-					describedBy: 'description',
-					placeholder: 'Enter a value'
-				},
-				onChange: (value: string) => {
-					this._setValue('value10', value);
-				},
-				step: 1800,
-				theme: this._theme,
-				useNativeElement: true,
-				invalid: true,
-				label: 'foo',
-				value: this._values['value10']
-			})
+			v('div', { id: 'example-native' }, [
+				w(TimePicker, {
+					key: '10',
+					inputProperties: {
+						describedBy: 'description',
+						placeholder: 'Enter a value'
+					},
+					onChange: (value: string) => {
+						this._setValue('value10', value);
+					},
+					step: 1800,
+					theme: this._theme,
+					useNativeElement: true,
+					invalid: true,
+					label: 'foo',
+					value: this._values['value10']
+				})
+			])
 		]);
 	}
 }
