5ce3db796866cc4b33c0a9a4f09051fd22faaaf8
Tom Dye <github@tomdye.co.uk>
2017-11-28 14:03:12 +0000

    remove width auto to fix toggle

--- src/label/styles/label.m.css.d.ts
+++ src/label/styles/label.m.css.d.ts
@@ -1,6 +1,6 @@
 export const root: string;
 export const readonly: string;
-export const required: string;
 export const invalid: string;
 export const valid: string;
+export const required: string;
 export const disabled: string;
