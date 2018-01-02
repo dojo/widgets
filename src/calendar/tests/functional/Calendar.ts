b6ba82e321e53b58fa199d475eeb907225c7b97e
Umar Hansa <umar.hansa@gmail.com>
2018-01-02 18:18:19 +0000

    Unify calendar cell structure with master and move fonts (#412)

--- src/calendar/tests/functional/Calendar.ts
+++ src/calendar/tests/functional/Calendar.ts
@@ -4,7 +4,6 @@ const { assert } = intern.getPlugin('chai');
 import { Remote } from 'intern/lib/executors/Node';
 import keys from '@theintern/leadfoot/keys';
 import * as css from '../../../theme/calendar/calendar.m.css';
-import * as cssCell from '../../../theme/calendar/calendarCell.m.css';
 
 const DELAY = 500;
 
@@ -127,7 +126,7 @@ registerSuite('Calendar', {
 			.findByCssSelector(disabledDateSelector)
 				.getAttribute('class')
 				.then((className: string) => {
-					assert.include(className, cssCell.inactiveDate, 'Disabled date has correct css class');
+					assert.include(className, css.inactiveDate, 'Disabled date has correct css class');
 				});
 	},
 
@@ -208,7 +207,7 @@ registerSuite('Calendar', {
 	'Clicking disabled date moves focus'() {
 		let clickedDate = '';
 		return clickDate(this.remote)
-			.findByCssSelector(`.${cssCell.inactiveDate}`)
+			.findByCssSelector(`.${css.inactiveDate}`)
 				.getVisibleText()
 				.then(text => {
 					clickedDate = text;
@@ -223,7 +222,7 @@ registerSuite('Calendar', {
 				})
 				.getAttribute('class')
 				.then((className: string) => {
-					assert.include(className, cssCell.selectedDate, 'Clicked date has selected class');
+					assert.include(className, css.selectedDate, 'Clicked date has selected class');
 				});
 	}
 });
--- src/theme/calendar/calendarCell.m.css.d.ts
+++
@@ -1,4 +0,0 @@
-export const inactiveDate: string;
-export const selectedDate: string;
-export const todayDate: string;
-export const date: string;
