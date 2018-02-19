225f386e9d7647a6ddc820a505f235ae6e8d8baf
smhigley <shigley@sitepen.com>
2018-02-19 11:03:58 -0800

    mocked animation meta

--- src/titlepane/tests/unit/TitlePane.ts
+++ src/titlepane/tests/unit/TitlePane.ts
@@ -1,5 +1,6 @@
 const { registerSuite } = intern.getInterface('object');
 const { assert } = intern.getPlugin('chai');
+import * as sinon from 'sinon';
 
 import { v, w } from '@dojo/widget-core/d';
 import TitlePane, { TitlePaneProperties } from '../../TitlePane';
@@ -22,109 +23,103 @@ interface TestEventInit extends EventInit {
 	keyCode: number;
 }
 
+const animateStub = sinon.spy();
+class StubMeta {
+	// dimensions .get()
+	public get(key: any) {
+		return {
+				offset: { height: 100 }
+		};
+	}
+
+	// scroll meta
+	public animate(key: string | number, options: any) {
+		const { effects, controls } = options;
+		animateStub({key, effects, playbackRate: controls.playbackRate});
+	}
+};
+
+class StubbedTitlePane extends TitlePane {
+	meta(MetaType: any): any {
+		return new StubMeta();
+	}
+}
+
+const expected = function(options: {open?: boolean, closeable?: boolean, heading?: string} = {}) {
+	const {
+		open = true,
+		closeable = true,
+		heading = null
+	} = options;
+	return v('div', {
+		classes: [ css.root, open ? css.open : null, fixedCss.rootFixed ]
+	}, [
+		w(GlobalEvent, { window: { resize: noop }, key: 'global' }),
+		v('div', {
+			'aria-level': heading,
+			classes: [ css.title, closeable ? css.closeable : null, fixedCss.titleFixed, closeable ? fixedCss.closeableFixed : null ],
+			role: 'heading'
+		}, [
+			v('button', {
+				'aria-controls': '',
+				'aria-expanded': `${open}`,
+				classes: css.titleButton,
+				disabled: !closeable,
+				id: '',
+				type: 'button',
+				onclick: noop
+			}, [
+				v('i', {
+					classes: [
+						css.arrow,
+						iconCss.icon,
+						open ? iconCss.downIcon : iconCss.rightIcon
+					],
+					role: 'presentation',
+					'aria-hidden': 'true'
+				}),
+				'test'
+			])
+		]),
+		v('div', {
+			'aria-hidden': open ? null : 'true',
+			'aria-labelledby': '',
+			classes: [ css.content, fixedCss.contentFixed ],
+			styles: {
+				marginTop: open ? '0px' : '-100px'
+			},
+			id: '',
+			key: 'content'
+		}, [ ])
+	]);
+};
+
 registerSuite('TitlePane', {
+	beforeEach() {
+		animateStub.reset();
+	},
 
 	tests: {
 		'default rendering'() {
-			const h = harness(() => w(TitlePane, { title: 'test' }));
+			const h = harness(() => w(StubbedTitlePane, { title: 'test' }));
 
-			h.expect(() => v('div', {
-				classes: [ css.root, css.open, fixedCss.rootFixed ]
-			}, [
-				w(GlobalEvent, { window: { resize: noop }, key: 'global' }),
-				v('div', {
-					'aria-level': null,
-					classes: [ css.title, css.closeable, fixedCss.titleFixed, fixedCss.closeableFixed ],
-					role: 'heading'
-				}, [
-					v('button', {
-						'aria-controls': '',
-						'aria-expanded': 'true',
-						classes: css.titleButton,
-						disabled: false,
-						id: '',
-						type: 'button',
-						onclick: noop
-					}, [
-						v('i', {
-							classes: [
-								css.arrow,
-								iconCss.icon,
-								iconCss.downIcon
-							],
-							role: 'presentation',
-							'aria-hidden': 'true'
-						}),
-						'test'
-					])
-				]),
-				v('div', {
-					'aria-hidden': null,
-					'aria-labelledby': '',
-					classes: [ css.content, fixedCss.contentFixed ],
-					styles: {
-						marginTop: '0px'
-					},
-					id: '',
-					key: 'content'
-				}, [ ])
-			]));
+			h.expect(expected);
 		},
 
 		'Should construct with the passed properties'() {
-			const h = harness(() => w(TitlePane, {
+			const h = harness(() => w(StubbedTitlePane, {
 				closeable: false,
 				headingLevel: 5,
 				open: false,
 				title: 'test'
 			}));
 
-			h.expect(() => v('div', {
-				classes: [ css.root, null, fixedCss.rootFixed ]
-			}, [
-				w(GlobalEvent, { window: { resize: noop }, key: 'global' }),
-				v('div', {
-					'aria-level': '5',
-					classes: [ css.title, null, fixedCss.titleFixed, null ],
-					role: 'heading'
-				}, [
-					v('button', {
-						'aria-controls': '',
-						'aria-expanded': 'false',
-						classes: css.titleButton,
-						disabled: true,
-						id: '',
-						type: 'button',
-						onclick: noop
-					}, [
-						v('i', {
-							classes: [
-								css.arrow,
-								iconCss.icon,
-								iconCss.rightIcon
-							],
-							role: 'presentation',
-							'aria-hidden': 'true'
-						}),
-						'test'
-					])
-				]),
-				v('div', {
-					'aria-hidden': 'true',
-					'aria-labelledby': '',
-					classes: [ css.content, fixedCss.contentFixed ],
-					id: '',
-					styles: {
-						marginTop: '-0px'
-					},
-					key: 'content'
-				}, [])
-			]));
+			h.expect(() => expected({open: false, closeable: false, heading: '5'}));
 		},
 
 		'click title to close'() {
 			let called = false;
-			const h = harness(() => w(TitlePane, {
+			const h = harness(() => w(StubbedTitlePane, {
 				closeable: true,
 				onRequestClose() {
 					called = true;
@@ -138,7 +133,7 @@ registerSuite('TitlePane', {
 
 		'click title to open'() {
 			let called = false;
-			const h = harness(() => w(TitlePane, {
+			const h = harness(() => w(StubbedTitlePane, {
 				closeable: true,
 				open: false,
 				onRequestOpen() {
@@ -160,7 +155,7 @@ registerSuite('TitlePane', {
 				},
 				title: 'test'
 			};
-			const h = harness(() => w(TitlePane, properties));
+			const h = harness(() => w(StubbedTitlePane, properties));
 			h.trigger(`.${css.titleButton}`, 'onclick', stubEvent);
 
 			properties = {
@@ -172,6 +167,34 @@ registerSuite('TitlePane', {
 			};
 			h.trigger(`.${css.titleButton}`, 'onclick', stubEvent);
 			assert.strictEqual(called, 1, 'onRequestClose should only becalled once');
+		},
+
+		'Calls animate with correct effects'() {
+			let open = true;
+			const h = harness(() => w(StubbedTitlePane, {
+				title: 'test',
+				open
+			}));
+
+			assert.isTrue(animateStub.getCall(0).calledWith({
+				key: 'content',
+				effects: [
+					{ marginTop: '0px' },
+					{ marginTop: '-100px' }
+				],
+				playbackRate: -1
+			}));
+
+			open = false;
+			h.expect(() => expected({ open: false }));
+			assert.isTrue(animateStub.getCall(1).calledWith({
+				key: 'content',
+				effects: [
+					{ marginTop: '0px' },
+					{ marginTop: '-100px' }
+				],
+				playbackRate: 1
+			}));
 		}
 	}
 });
