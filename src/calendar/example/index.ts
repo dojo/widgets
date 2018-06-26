804c168d510525fb93914147df472b7d071a60cc
David Schontzler <schontz@gmail.com>
2018-06-26 15:25:42 -0700

    Added example

--- src/calendar/example/index.ts
+++ src/calendar/example/index.ts
@@ -8,9 +8,14 @@ export default class App extends WidgetBase {
 	private _year: number | undefined;
 	private _selectedDate: Date | undefined;
 
+	private _month2: number | undefined;
+	private _year2: number | undefined;
+	private _selectedDate2: Date | undefined;
+
 	render() {
 		return v('div', {}, [
 			w(Calendar, {
+				key: 'calendar-start-sunday',
 				month: this._month,
 				selectedDate: this._selectedDate,
 				year: this._year,
@@ -27,7 +32,29 @@ export default class App extends WidgetBase {
 					this.invalidate();
 				}
 			}),
-			this._selectedDate ? v('p', [ `Selected Date: ${this._selectedDate.toDateString()}` ]) : null
+			this._selectedDate ? v('p', [ `Selected Date: ${this._selectedDate.toDateString()}` ]) : null,
+			v('hr'),
+			v('p', {}, ['Calendar starts on Monday']),
+			w(Calendar, {
+				key: 'calendar-start-monday',
+				month: this._month2,
+				selectedDate: this._selectedDate2,
+				firstDayOfTheWeek: 1,
+				year: this._year2,
+				onMonthChange: (month: number) => {
+					this._month2 = month;
+					this.invalidate();
+				},
+				onYearChange: (year: number) => {
+					this._year2 = year;
+					this.invalidate();
+				},
+				onDateSelect: (date: Date) => {
+					this._selectedDate2 = date;
+					this.invalidate();
+				}
+			}),
+			this._selectedDate ? v('p', [ `Selected Date: ${this._selectedDate2.toDateString()}` ]) : null
 		]);
 	}
 }
