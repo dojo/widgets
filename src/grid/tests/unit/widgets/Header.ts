ee001922f02db1d8fa610f6d711ad3d8a121cee9
smhigley <shigley@sitepen.com>
2018-09-19 23:20:09 -0700

    accessible header sort and filter

--- src/grid/tests/unit/widgets/Header.ts
+++ src/grid/tests/unit/widgets/Header.ts
@@ -1,8 +1,11 @@
 const { describe, it } = intern.getInterface('bdd');
+const { assert } = intern.getPlugin('chai');
 
 import { stub } from 'sinon';
 import harness from '@dojo/framework/testing/harness';
 import { v, w } from '@dojo/framework/widget-core/d';
+import TextInput from '../../../../text-input/index';
+import Icon from '../../../../icon/index';
 
 import * as css from '../../../widgets/styles/Header.m.css';
 import Header from '../../../widgets/Header';
@@ -48,7 +51,7 @@ describe('Header', () => {
 		);
 		h.expect(() =>
 			v('div', { scrollLeft: 0, classes: [css.root, null], row: 'rowgroup' }, [
-				v('div', { classes: css.row, role: 'role' }, [
+				v('div', { classes: css.row, role: 'row' }, [
 					v('div', { classes: css.cell, role: 'columnheader' }, [v('div', {}, ['Title'])]),
 					v('div', { classes: css.cell, role: 'columnheader' }, [v('div', {}, ['First Name'])])
 				])
@@ -70,21 +73,32 @@ describe('Header', () => {
 
 		h.expect(() =>
 			v('div', { scrollLeft: 0, classes: [css.root, css.filterGroup], row: 'rowgroup' }, [
-				v('div', { classes: css.row, role: 'role' }, [
+				v('div', { classes: css.row, role: 'row' }, [
 					v('div', { classes: css.cell, role: 'columnheader' }, [v('div', {}, ['Title'])]),
 					v('div', { classes: css.cell, role: 'columnheader' }, [
-						v(
-							'div',
-							{
-								classes: [css.sortable, null, null, null],
+						v('div', {
+							classes: [css.sortable, null, null, null],
+							onclick: noop
+						}, [
+							'Custom Title',
+							v('button', {
+								classes: css.sort,
 								onclick: noop
-							},
-							['Custom Title']
-						),
-						v('input', {
-							classes: css.filter,
+							}, [
+								w(Icon, {
+									type: 'downIcon',
+									altText: 'Sort by Custom Title'
+								})
+							])
+						]),
+						w(TextInput, {
+							key: 'filter',
+							extraClasses: { root: css.filter },
+							label: 'Filter by Custom Title',
+							labelHidden: true,
+							type: 'search',
 							value: '',
-							oninput: noop
+							onInput: noop
 						})
 					])
 				])
@@ -110,21 +124,32 @@ describe('Header', () => {
 
 		h.expect(() =>
 			v('div', { scrollLeft: 0, classes: [css.root, css.filterGroup], row: 'rowgroup' }, [
-				v('div', { classes: css.row, role: 'role' }, [
+				v('div', { classes: css.row, role: 'row' }, [
 					v('div', { classes: css.cell, role: 'columnheader' }, [v('div', {}, ['Title'])]),
 					v('div', { classes: css.cell, role: 'columnheader' }, [
-						v(
-							'div',
-							{
-								classes: [css.sortable, css.sorted, null, css.asc],
+						v('div', {
+							classes: [css.sortable, css.sorted, null, css.asc],
+							onclick: noop
+						}, [
+							'Custom Title',
+							v('button', {
+								classes: css.sort,
 								onclick: noop
-							},
-							['Custom Title']
-						),
-						v('input', {
-							classes: css.filter,
+							}, [
+								w(Icon, {
+									type: 'upIcon',
+									altText: 'Sort by Custom Title'
+								})
+							])
+						]),
+						w(TextInput, {
+							key: 'filter',
+							extraClasses: { root: css.filter },
+							label: 'Filter by Custom Title',
+							labelHidden: true,
+							type: 'search',
 							value: '',
-							oninput: noop
+							onInput: noop
 						})
 					])
 				])
@@ -150,21 +175,32 @@ describe('Header', () => {
 
 		h.expect(() =>
 			v('div', { scrollLeft: 0, classes: [css.root, css.filterGroup], row: 'rowgroup' }, [
-				v('div', { classes: css.row, role: 'role' }, [
+				v('div', { classes: css.row, role: 'row' }, [
 					v('div', { classes: css.cell, role: 'columnheader' }, [v('div', {}, ['Title'])]),
 					v('div', { classes: css.cell, role: 'columnheader' }, [
-						v(
-							'div',
-							{
-								classes: [css.sortable, css.sorted, css.desc, null],
+					v('div', {
+							classes: [css.sortable, css.sorted, css.desc, null],
+							onclick: noop
+						}, [
+							'Custom Title',
+							v('button', {
+								classes: css.sort,
 								onclick: noop
-							},
-							['Custom Title']
-						),
-						v('input', {
-							classes: css.filter,
+							}, [
+								w(Icon, {
+									type: 'downIcon',
+									altText: 'Sort by Custom Title'
+								})
+							])
+						]),
+						w(TextInput, {
+							key: 'filter',
+							extraClasses: { root: css.filter },
+							label: 'Filter by Custom Title',
+							labelHidden: true,
+							type: 'search',
 							value: '',
-							oninput: noop
+							onInput: noop
 						})
 					])
 				])
@@ -190,25 +226,122 @@ describe('Header', () => {
 
 		h.expect(() =>
 			v('div', { scrollLeft: 0, classes: [css.root, css.filterGroup], row: 'rowgroup' }, [
-				v('div', { classes: css.row, role: 'role' }, [
+				v('div', { classes: css.row, role: 'row' }, [
 					v('div', { classes: css.cell, role: 'columnheader' }, [v('div', {}, ['Title'])]),
 					v('div', { classes: css.cell, role: 'columnheader' }, [
-						v(
-							'div',
-							{
-								classes: [css.sortable, null, null, null],
+						v('div', {
+							classes: [css.sortable, null, null, null],
+							onclick: noop
+						}, [
+							'Custom Title',
+							v('button', {
+								classes: css.sort,
 								onclick: noop
-							},
-							['Custom Title']
-						),
-						v('input', {
-							classes: css.filter,
+							}, [
+								w(Icon, {
+									type: 'downIcon',
+									altText: 'Sort by Custom Title'
+								})
+							])
+						]),
+						w(TextInput, {
+							key: 'filter',
+							extraClasses: { root: css.filter },
+							label: 'Filter by Custom Title',
+							labelHidden: true,
+							type: 'search',
 							value: 'my filter',
-							oninput: noop
+							onInput: noop
 						})
 					])
 				])
 			])
 		);
+	});
+
+	it('should call filterer on input', () => {
+		const sorterStub = stub();
+		const filtererStub = stub();
+		const h = harness(() =>
+			w(Header, {
+				columnConfig: advancedColumnConfig,
+				sorter: sorterStub,
+				filterer: filtererStub,
+				scrollLeft: 0
+			})
+		);
+
+		h.trigger('@filter', 'onInput', 'trillian');
+		assert.isTrue(filtererStub.calledWith('firstName', 'trillian'));
+	});
+
+	describe('sort interaction', () => {
+		it('should sort desc by default', () => {
+			const sorterStub = stub();
+			const filtererStub = stub();
+			const h = harness(() =>
+				w(Header, {
+					columnConfig: advancedColumnConfig,
+					sorter: sorterStub,
+					filterer: filtererStub,
+					scrollLeft: 0
+				})
+			);
+
+			h.trigger(`.${css.sort}`, 'onclick', {});
+			assert.isTrue(sorterStub.calledWith('firstName', 'desc'));
+			sorterStub.reset();
+
+			h.trigger(`.${css.sortable}`, 'onclick', {});
+			assert.isTrue(sorterStub.calledWith('firstName', 'desc'));
+		});
+
+		it('should sort asc on if currently desc', () => {
+			const sorterStub = stub();
+			const filtererStub = stub();
+			const h = harness(() =>
+				w(Header, {
+					columnConfig: advancedColumnConfig,
+					sorter: sorterStub,
+					filterer: filtererStub,
+					scrollLeft: 0,
+					sort: {
+						columnId: 'firstName',
+						direction: 'desc'
+					}
+				})
+			);
+
+			h.trigger(`.${css.sort}`, 'onclick', {});
+			assert.isTrue(sorterStub.calledWith('firstName', 'asc'));
+			sorterStub.reset();
+
+			h.trigger(`.${css.sortable}`, 'onclick', {});
+			assert.isTrue(sorterStub.calledWith('firstName', 'asc'));
+		});
+
+		it('should sort desc on if currently asc', () => {
+			const sorterStub = stub();
+			const filtererStub = stub();
+			const h = harness(() =>
+				w(Header, {
+					columnConfig: advancedColumnConfig,
+					sorter: sorterStub,
+					filterer: filtererStub,
+					scrollLeft: 0,
+					sort: {
+						columnId: 'firstName',
+						direction: 'asc'
+					}
+				})
+			);
+
+			h.trigger(`.${css.sort}`, 'onclick', {});
+			assert.isTrue(sorterStub.calledWith('firstName', 'desc'));
+			sorterStub.reset();
+
+			h.trigger(`.${css.sortable}`, 'onclick', {});
+			assert.isTrue(sorterStub.calledWith('firstName', 'desc'));
+		});
 	});
 });
--- src/grid/widgets/styles/Header.m.css.d.ts
+++ src/grid/widgets/styles/Header.m.css.d.ts
@@ -6,4 +6,5 @@ export const row: string;
 export const sorted: string;
 export const desc: string;
 export const asc: string;
+export const sort: string;
 export const filter: string;
