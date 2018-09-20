8ba5e983c25ee85f8f6e4dfdaa5efd044a9aca91
Sarah Higley <smhigley@users.noreply.github.com>
2018-09-20 08:51:55 -0700

    accessible grid sorting and filtering (#595)

--- src/grid/widgets/Header.ts
+++ src/grid/widgets/Header.ts
@@ -1,8 +1,10 @@
 import WidgetBase from '@dojo/framework/widget-core/WidgetBase';
-import { v } from '@dojo/framework/widget-core/d';
+import { v, w } from '@dojo/framework/widget-core/d';
 import ThemedMixin, { theme } from '@dojo/framework/widget-core/mixins/Themed';
 import { ColumnConfig, FilterOptions, SortOptions } from './../interfaces';
 import { DNode } from '@dojo/framework/widget-core/interfaces';
+import TextInput from '../../text-input/index';
+import Icon from '../../icon/index';
 
 import * as css from './styles/Header.m.css';
 
@@ -17,13 +19,19 @@ export interface HeaderProperties {
 
 @theme(css)
 export default class Header extends ThemedMixin(WidgetBase)<HeaderProperties> {
+	private _sortColumn(id: string) {
+		const { sort, sorter } = this.properties;
+		const direction = sort
+			? sort.columnId !== id ? 'desc' : sort.direction === 'desc' ? 'asc' : 'desc'
+			: 'desc';
+		sorter(id, direction);
+	}
+
 	protected render(): DNode {
 		const { columnConfig, sorter, sort, filterer, scrollLeft, filter } = this.properties;
 		const hasFilters = columnConfig.some((config) => !!config.filterable);
 		return v('div', { scrollLeft, classes: [css.root, hasFilters ? css.filterGroup : null], row: 'rowgroup' }, [
-			v(
-				'div',
-				{ classes: css.row, role: 'role' },
+			v('div', { classes: css.row, role: 'row' },
 				columnConfig.map((column) => {
 					let title: string | DNode;
 					if (typeof column.title === 'function') {
@@ -41,25 +49,39 @@ export default class Header extends ThemedMixin(WidgetBase)<HeaderProperties> {
 								sort && sort.columnId === column.id && sort.direction === 'asc' ? css.asc : null
 							],
 							onclick: () => {
-								const direction = sort
-									? sort.columnId !== column.id ? 'desc' : sort.direction === 'desc' ? 'asc' : 'desc'
-									: 'desc';
-								sorter(column.id, direction);
+								this._sortColumn(column.id);
 							}
 						};
 					}
 
 					return v('div', { classes: css.cell, role: 'columnheader' }, [
-						v('div', headerProperties, [title]),
-						column.filterable
-							? v('input', {
-									classes: css.filter,
-									value: filter && filter.columnId === column.id ? filter.value : '',
-									oninput: (event: KeyboardEvent) => {
-										const target = event.target as HTMLInputElement;
-										filterer(column.id, target.value);
-									}
+						v('div', headerProperties, [
+							title,
+							column.sortable ? v('button', {
+								classes: css.sort,
+								onclick: () => {
+									this._sortColumn(column.id);
+								}
+							}, [
+								w(Icon, {
+									type: sort && sort.columnId === column.id && sort.direction === 'asc' ? 'upIcon' : 'downIcon',
+									altText: `Sort by ${title}`
 								})
+							])
+							: null
+						]),
+						column.filterable
+							? w(TextInput, {
+								key: 'filter',
+								extraClasses: { root: css.filter },
+								label: `Filter by ${title}`,
+								labelHidden: true,
+								type: 'search',
+								value: filter && filter.columnId === column.id ? filter.value : '',
+								onInput: (value: string) => {
+									filterer(column.id, value);
+								}
+							})
 							: null
 					]);
 				})
