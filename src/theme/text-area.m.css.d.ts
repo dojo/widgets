cbbffb71561d22e3c50f977d5b8fee7c35130118
Tom Dye <github@tomdye.co.uk>
2018-11-12 16:35:26 +0000

    undo text-area changes

--- src/theme/text-area.m.css.d.ts
+++ src/theme/text-area.m.css.d.ts
@@ -7,5 +7,3 @@ export const readonly: string;
 export const required: string;
 export const invalid: string;
 export const valid: string;
-export const hasValue: string;
-export const label: string;
