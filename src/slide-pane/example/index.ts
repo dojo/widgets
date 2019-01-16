e4b185e6b1fc0e035483930d84361305a7d7f6ea
Simon Scholz <simon.scholz@vogella.com>
2019-01-16 08:06:27 +0100

    Added overflow-y: auto; to slide-pane

--- src/slide-pane/example/index.ts
+++ src/slide-pane/example/index.ts
@@ -38,6 +38,27 @@ export default class App extends WidgetBase {
 			}, [
 				`Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 				Quisque id purus ipsum. Aenean ac purus purus.
+				Nam sollicitudin varius augue, sed lacinia felis tempor in.
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				Quisque id purus ipsum. Aenean ac purus purus.
+				Nam sollicitudin varius augue, sed lacinia felis tempor in.
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				Quisque id purus ipsum. Aenean ac purus purus.
+				Nam sollicitudin varius augue, sed lacinia felis tempor in.
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				Quisque id purus ipsum. Aenean ac purus purus.
+				Nam sollicitudin varius augue, sed lacinia felis tempor in.
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				Quisque id purus ipsum. Aenean ac purus purus.
+				Nam sollicitudin varius augue, sed lacinia felis tempor in.
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				Quisque id purus ipsum. Aenean ac purus purus.
+				Nam sollicitudin varius augue, sed lacinia felis tempor in.
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				Quisque id purus ipsum. Aenean ac purus purus.
+				Nam sollicitudin varius augue, sed lacinia felis tempor in.
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				Quisque id purus ipsum. Aenean ac purus purus.
 				Nam sollicitudin varius augue, sed lacinia felis tempor in.`
 			]),
 			v('button', {
