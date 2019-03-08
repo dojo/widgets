6624e49306631caa33ceca7d4d6be2ec6d555ff1
Mangala SSS Khalsa <mssskhalsa@gmail.com>
2019-03-08 16:19:38 -0700

    Add test for menu on left

--- src/toolbar/tests/unit/Toolbar.ts
+++ src/toolbar/tests/unit/Toolbar.ts
@@ -181,6 +181,96 @@ registerSuite('Toolbar', {
 				slidePaneVDom,
 				buttonVDom
 			]));
+		},
+
+		'menu on left'() {
+			const mockMeta = stub();
+			const mockDimensionsGet = stub();
+			mockDimensionsGet.returns({
+				offset: { height: 100, left: 100, top: 100, width: 100 },
+				position: { bottom: 200, left: 100, right: 200, top: 100 },
+				scroll: { height: 100, left: 100, top: 100, width: 100 },
+				size: { width: 100, height: 100 }
+			});
+			mockMeta.withArgs(Dimensions).returns({
+				get: mockDimensionsGet
+			});
+			let properties: ToolbarProperties = {
+				align: Align.left,
+				collapseWidth: 1000,
+				onCollapse: () => {}
+			};
+			const h = harness(() => w(MockMetaMixin(Toolbar, mockMeta), properties, [ 'test' ]));
+			const slidePaneVDom = w(SlidePane, {
+				align: Align.left,
+				closeText: 'close',
+				key: 'slide-pane-menu',
+				onRequestClose: noop,
+				open: false,
+				theme: undefined,
+				classes: undefined,
+				title: 'foo'
+			}, [
+				'test'
+			]);
+
+			const buttonVDom = v('button', {
+				classes: css.menuButton,
+				type: 'button',
+				onclick: noop
+			}, [
+				'open',
+				w(Icon, { type: 'barsIcon', theme: undefined, classes: undefined })
+			]);
+
+			h.expect(() => v('div', {
+				key: 'root',
+				classes: [ fixedCss.rootFixed, css.root, null ]
+			}, [
+				w(GlobalEvent, { window: { resize: noop }, key: 'global' }),
+				null,
+				v('div', {
+					classes: css.actions,
+					key: 'menu'
+				}, [
+					'test'
+				]),
+				null
+			]));
+
+			properties = { heading: 'foo' };
+			h.trigger('@global', (node: any) => {
+				if (isWNode<GlobalEvent>(node) && node.properties.window !== undefined) {
+					return node.properties.window ? node.properties.window.resize : undefined;
+				}
+			});
+
+			h.expect(() => v('div', {
+				key: 'root',
+				classes: [ fixedCss.rootFixed, css.root, css.collapsed ]
+			}, [
+				w(GlobalEvent, { window: { resize: noop }, key: 'global' }),
+				v('div', {
+					classes: css.title
+				}, [ 'foo' ]),
+				slidePaneVDom,
+				buttonVDom
+			]));
+
+			h.trigger(`.${css.menuButton}`, 'onclick', stubEvent);
+			h.trigger('@slide-pane-menu', 'onRequestClose');
+
+			h.expect(() => v('div', {
+				key: 'root',
+				classes: [ fixedCss.rootFixed, css.root, css.collapsed ]
+			}, [
+				w(GlobalEvent, { window: { resize: noop }, key: 'global' }),
+				v('div', {
+					classes: css.title
+				}, [ 'foo' ]),
+				slidePaneVDom,
+				buttonVDom
+			]));
 		}
 	}
 });
