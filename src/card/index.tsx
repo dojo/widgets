129ab9bc771d6c7aabc86b537d7b82588eba8f28
Nick Nisi <nick@nisi.org>
2019-05-03 14:12:36 -0500

    initial commit of card widget

---
+++ src/card/index.tsx
@@ -0,0 +1,22 @@
+import { WidgetBase } from '@dojo/framework/widget-core/WidgetBase';
+import { tsx } from '@dojo/framework/widget-core/tsx';
+import { RenderResult } from '@dojo/framework/widget-core/interfaces';
+import { alwaysRender } from '@dojo/framework/widget-core/decorators/alwaysRender';
+import * as css from '../theme/card.m.css';
+
+export interface CardProperties {
+	actionsRenderer?(): RenderResult;
+}
+@alwaysRender()
+export default class Card extends WidgetBase<CardProperties> {
+	protected render() {
+		const { actionsRenderer } = this.properties;
+		const actionsResult = actionsRenderer && actionsRenderer();
+		return (
+			<div classes={[css.root]}>
+				{this.children}
+				{actionsResult && <div classes={[css.actions]}>{actionsResult}</div>}
+			</div>
+		);
+	}
+}
