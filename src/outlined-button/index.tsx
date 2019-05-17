e4c6c5de556ba40f664fa9c812a98677b07a10a0
Bradley Maier <bmaier@sitepen.com>
2019-05-17 07:15:57 -0400

    Add outlined button

---
+++ src/outlined-button/index.tsx
@@ -0,0 +1,46 @@
+import { WidgetBase } from '@dojo/framework/widget-core/WidgetBase';
+import { ThemedMixin, theme } from '@dojo/framework/widget-core/mixins/Themed';
+import { tsx } from '@dojo/framework/widget-core/tsx';
+import { customElement } from '@dojo/framework/widget-core/decorators/customElement';
+import Button, { ButtonProperties } from '../button';
+import * as css from '../theme/outlined-button.m.css';
+import { CustomElementChildType } from '@dojo/framework/widget-core/registerCustomElement';
+
+export interface OutlinedButtonProperties extends ButtonProperties {}
+
+@theme(css)
+@customElement<OutlinedButtonProperties>({
+	tag: 'dojo-outlined-button',
+	childType: CustomElementChildType.TEXT,
+	properties: ['disabled', 'pressed', 'popup', 'theme', 'aria', 'extraClasses', 'classes'],
+	attributes: ['widgetId', 'name', 'type', 'value'],
+	events: [
+		'onBlur',
+		'onChange',
+		'onClick',
+		'onFocus',
+		'onInput',
+		'onKeyDown',
+		'onKeyPress',
+		'onKeyUp',
+		'onMouseDown',
+		'onMouseUp',
+		'onTouchCancel',
+		'onTouchEnd',
+		'onTouchStart'
+	]
+})
+export class OutlinedButton extends ThemedMixin(WidgetBase)<OutlinedButtonProperties> {
+	protected render() {
+		return (
+			<Button
+				classes={{ '@dojo/widgets/button': { root: this.theme([css.root]) } }}
+				{...this.properties}
+			>
+				{this.children}
+			</Button>
+		);
+	}
+}
+
+export default OutlinedButton;
