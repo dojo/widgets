c2fa92f328cc27419728062a2c31e1c1af28ca75
Nick Nisi <nick@nisi.org>
2019-05-18 09:19:07 -0500

    add ThemedMixin to card widget

--- src/card/index.tsx
+++ src/card/index.tsx
@@ -2,14 +2,21 @@ import { WidgetBase } from '@dojo/framework/widget-core/WidgetBase';
 import { tsx } from '@dojo/framework/widget-core/tsx';
 import { RenderResult } from '@dojo/framework/widget-core/interfaces';
 import { alwaysRender } from '@dojo/framework/widget-core/decorators/alwaysRender';
+import { ThemedMixin, theme } from '@dojo/framework/widget-core/mixins/Themed';
 import * as css from '../theme/card.m.css';
+import { customElement } from '@dojo/framework/widget-core/decorators/customElement';
 
 export interface CardProperties {
 	actionsRenderer?(): RenderResult;
 }
 
+@customElement<CardProperties>({
+	tag: 'dojo-card',
+	properties: ['actionsRenderer']
+})
+@theme(css)
 @alwaysRender()
-export class Card extends WidgetBase<CardProperties> {
+export class Card extends ThemedMixin(WidgetBase)<CardProperties> {
 	protected render() {
 		const { actionsRenderer } = this.properties;
 		const actionsResult = actionsRenderer && actionsRenderer();
