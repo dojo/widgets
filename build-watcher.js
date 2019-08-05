d19f877bd2e945cb18a6e88a8cdd354ce19db32a
Sam Menza <smenza@sitepen.com>
2019-08-05 13:15:20 -0700

    Cleaned up build watcher script

--- build-watcher.js
+++ build-watcher.js
@@ -1,35 +1,32 @@
 var chokidar = require('chokidar');
 const { exec } = require('child_process');
-fs = require("fs");
+fs = require('fs');
 const http = require('http');
 const handler = require('serve-handler');
 
+var watchIgnore = /.*\.m\.css\.d\.ts$/;
+
 var watcher = chokidar.watch('src/*/*', {
-	ignored: /[\/\\]\./, persistent: true
+	persistent: true
   });
-
-var watchIgnore = 'm.css.d.ts';
-
-var log = console.log.bind(console);
 
 // Start development server
 var server = http.createServer((request, response) => {
 	return handler(request, response);
   });
 server.listen(5000, () => {
-	log('Server started, view examples at http://localhost:5000/dist/dev/src/common/example/')
+	console.log('Server started, view examples at http://localhost:5000/dist/dev/src/common/example/')
 })
-
 
 // Start watching files
 watcher.on('raw', function(event, path) {
-	if (!path.endsWith(watchIgnore)) {
-		log(`Changes detected at ${path}`);
-		exec("npm run clean && npm run build:test", (err, stdout) => {
+	if (!watchIgnore.test(path)) {
+		console.log(`Changes detected at ${path}`);
+		exec('npm run clean && npm run build:test', (err, stdout) => {
 			if (err) {
 			  console.error(err)
 			} else {
-			 console.log(`Rebuild complete`);
+			 console.log('Rebuild complete');
 			}
 		  });
 	}
