c512b8a928a8354a24c8db77819e99fc47cf4b78
Anthony Gubler <agubler@sitepen.com>
2019-10-09 17:01:06 +0100

    load lang

--- src/examples/src/code.block.ts
+++ src/examples/src/code.block.ts
@@ -3,9 +3,9 @@ import * as fs from 'fs';
 import * as path from 'path';
 const Prism = require('prismjs');
 const loadLanguages = require('prismjs/components/');
-loadLanguages(['tsx']);
 
 export default function() {
+	loadLanguages(['tsx']);
 	const examples = globExamples();
 	return examples.reduce((content, widget) => {
 		const code = fs.readFileSync(path.join(__dirname, 'widgets', widget), 'utf8');
