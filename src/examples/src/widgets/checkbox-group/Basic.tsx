12b7236d1445e8c349d14e7e387e2a0e9681aa86
Tom Dye <github@tomdye.co.uk>
2019-10-15 19:59:58 +0100

    fix example imports

--- src/examples/src/widgets/checkbox-group/Basic.tsx
+++ src/examples/src/widgets/checkbox-group/Basic.tsx
@@ -1,5 +1,5 @@
 import { create, tsx } from '@dojo/framework/core/vdom';
-import CheckboxGroup from '../../../../checkbox-group/index';
+import CheckboxGroup from '@dojo/widgets/checkbox-group/index';
 import { icache } from '@dojo/framework/core/middleware/icache';
 
 const factory = create({ icache });
