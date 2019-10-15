71102c03dba7290064a8bb4c682bd0612ac7218e
Daniel Lautzenheiser <lautzd@gmail.com>
2019-10-15 15:59:44 -0400

    Fix examples build on windows

--- src/examples/src/glob-examples.block.ts
+++ src/examples/src/glob-examples.block.ts
@@ -1,5 +1,5 @@
 import * as glob from 'glob';
-import * as path from 'path';
+import * as path from 'canonical-path';
 
 export default function() {
 	const files = glob.sync(path.join(__dirname, 'widgets/**/*'), { nodir: true });
