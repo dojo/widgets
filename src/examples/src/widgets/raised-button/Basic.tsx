132f08b9ac6cdca2ae12e0b91a6d6cae276c1b3d
Tom Dye <github@tomdye.co.uk>
2019-10-21 10:57:13 +0100

    wip

--- src/examples/src/widgets/raised-button/Basic.tsx
+++ src/examples/src/widgets/raised-button/Basic.tsx
@@ -1,8 +1,25 @@
 import { create, tsx } from '@dojo/framework/core/vdom';
 import RaisedButton from '@dojo/widgets/raised-button';
+import Button from '@dojo/widgets/button';
+import * as buttonCss from './ButtonTheme.m.css';
+import * as raisedButtonCss from './RaisedButtonTheme.m.css';
 
 const factory = create();
 
 export default factory(function Basic() {
-	return <RaisedButton>Raised Button</RaisedButton>;
+	const theme = {
+		'@dojo/widgets/button': buttonCss,
+		'@dojo/widgets/raised-button': raisedButtonCss
+	};
+
+	return (
+		<virtual>
+			<Button disabled theme={theme}>
+				Normal Buton
+			</Button>
+			<RaisedButton disabled theme={theme}>
+				Raised Button
+			</RaisedButton>
+		</virtual>
+	);
 });
--- src/raised-button/index.tsx
+++ src/raised-button/index.tsx
@@ -3,23 +3,40 @@ import { ThemedMixin, theme } from '@dojo/framework/core/mixins/Themed';
 import { tsx } from '@dojo/framework/core/vdom';
 import Button, { ButtonProperties } from '../button/index';
 import * as css from '../theme/raised-button.m.css';
-import { DNode } from '@dojo/framework/core/interfaces';
+import * as buttonCss from '../theme/button.m.css';
+import { DNode, SupportedClassName } from '@dojo/framework/core/interfaces';
 
 export interface RaisedButtonProperties extends ButtonProperties {}
 
+function composeThemeClasses(
+	baseCss: { [key: string]: SupportedClassName },
+	variantCss: { [key: string]: SupportedClassName },
+	getThemeClass: (classes: SupportedClassName) => SupportedClassName
+) {
+	const themedBaseClasses = Object.keys(baseCss).reduce((existing, key) => {
+		(existing as any)[key] = getThemeClass((baseCss as any)[key]);
+		return existing;
+	}, {});
+
+	const themedVariantClasses = Object.keys(variantCss).reduce((existing, key) => {
+		(existing as any)[key] = getThemeClass((variantCss as any)[key]);
+		return existing;
+	}, {});
+
+	return { ...themedBaseClasses, ...themedVariantClasses };
+}
+
 @theme(css)
+@theme(buttonCss)
 export class RaisedButton extends ThemedMixin(WidgetBase)<RaisedButtonProperties> {
 	protected render(): DNode {
+		const theme = {
+			...this.properties.theme,
+			'@dojo/widgets/button': composeThemeClasses(buttonCss, css, this.theme)
+		};
+
 		return (
-			<Button
-				classes={{
-					'@dojo/widgets/button': {
-						root: this.theme([css.root]),
-						disabled: this.theme([css.disabled])
-					}
-				}}
-				{...this.properties}
-			>
+			<Button {...this.properties} theme={theme}>
 				{this.children}
 			</Button>
 		);
