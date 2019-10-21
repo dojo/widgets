cae18bab6dde7761514614231c4f152e93a518ec
Bradley Maier <bmaier@sitepen.com>
2019-10-21 02:51:17 -0700

    Move icon examples (#850)

---
+++ src/examples/src/widgets/icon/Basic.tsx
@@ -0,0 +1,34 @@
+import { create, tsx } from '@dojo/framework/core/vdom';
+import Icon from '@dojo/widgets/icon';
+
+const factory = create();
+
+export default factory(function Basic() {
+	return (
+		<virtual>
+			<Icon type="downIcon" />
+			<Icon type="leftIcon" />
+			<Icon type="rightIcon" />
+			<Icon type="closeIcon" />
+			<Icon type="plusIcon" />
+			<Icon type="minusIcon" />
+			<Icon type="checkIcon" />
+			<Icon type="upIcon" />
+			<Icon type="upAltIcon" />
+			<Icon type="downAltIcon" />
+			<Icon type="searchIcon" />
+			<Icon type="barsIcon" />
+			<Icon type="settingsIcon" />
+			<Icon type="alertIcon" />
+			<Icon type="helpIcon" />
+			<Icon type="infoIcon" />
+			<Icon type="phoneIcon" />
+			<Icon type="editIcon" />
+			<Icon type="dateIcon" />
+			<Icon type="linkIcon" />
+			<Icon type="locationIcon" />
+			<Icon type="secureIcon" />
+			<Icon type="mailIcon" />
+		</virtual>
+	);
+});
