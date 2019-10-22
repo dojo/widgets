aafb9ad2b62469a5d61dc4be8e89fa4ad4202b3e
Michael Van Sickle <mvansickle1@gmail.com>
2019-10-22 11:07:34 -0400

    - Update NumberInput to use BaseInputProperties - Make BaseInputProperties generic on value - update tests

--- src/input/NumberInput/NumberInput.tsx
+++ src/input/NumberInput/NumberInput.tsx
@@ -1,76 +1,44 @@
 import { create, tsx } from '@dojo/framework/core/vdom';
-import { DNode } from '@dojo/framework/core/interfaces';
-import TextInput from '../../text-input';
-import { ThemedProperties } from '@dojo/framework/core/middleware/theme';
+import TextInput, { BaseInputProperties } from '../../text-input';
 
-export interface NumberInputProperties extends ThemedProperties {
-	/** Custom aria attributes */
-	aria?: { [key: string]: string | null };
-	/**  */
-	autocomplete?: boolean | string;
-	/** ID of an element that this input controls */
-	controls?: string;
-	/**  */
-	customValidator?: (value: string) => { valid?: boolean; message?: string } | void;
-	/**  */
-	disabled?: boolean;
-	/**  */
-	helperText?: string;
-	/**  */
-	label?: string;
-	/**  */
-	labelHidden?: boolean;
-	/** Renderer for leading icon content */
-	leading?: () => DNode;
+export interface NumberInputProperties extends BaseInputProperties<{ value: number }> {
 	/** The min value a number can be */
 	min?: number;
 	/** The max value a number can be */
 	max?: number;
 	/** The step to increment the number value by */
 	step?: number;
-	/**  */
-	name?: string;
-	/**  */
-	onBlur?(): void;
-	/**  */
-	onFocus?(): void;
-	/**  */
-	onKeyDown?(key: number, preventDefault: () => void): void;
-	/**  */
-	onKeyUp?(key: number, preventDefault: () => void): void;
-	/**  */
-	onValidate?: (valid: boolean | undefined, message: string) => void;
-	/**  */
-	onValue?(value?: string): void;
-	/**  */
-	onClick?(): void;
-	/**  */
-	onOver?(): void;
-	/**  */
-	onOut?(): void;
-	/**  */
-	pattern?: string | RegExp;
-	/** Placeholder text */
-	placeholder?: string;
-	/**  */
-	readOnly?: boolean;
-	/**  */
-	required?: boolean;
-	/** Renderer for trailing icon content */
-	trailing?: () => DNode;
-	/**  */
-	valid?: { valid?: boolean; message?: string } | boolean;
-	/** The current value */
-	value?: number;
-	/**  */
-	widgetId?: string;
 }
 
 const factory = create().properties<NumberInputProperties>();
 
 export default factory(function NumberInput({ properties }) {
-	const { value } = properties();
-	const formattedValue = value !== undefined && value !== null ? value.toString() : '';
+	const { value, onValue } = properties();
 
-	return <TextInput {...properties()} value={formattedValue} type="number" />;
+	const valueAsString = value !== undefined && value !== null ? value.toString() : value;
+
+	function onValueAdapter(valueAsString: string | undefined) {
+		console.log('1');
+		if (!onValue) {
+			return;
+		}
+		console.log('2');
+		if (valueAsString === undefined) {
+			console.log('3');
+			onValue();
+		} else {
+			console.log('4');
+			onValue(parseFloat(valueAsString));
+		}
+	}
+
+	return (
+		<TextInput
+			assertion-key="textInput"
+			{...properties()}
+			value={valueAsString}
+			onValue={onValueAdapter}
+			type="number"
+		/>
+	);
 });
--- src/input/NumberInput/tests/unit/NumberInput.tsx
+++ src/input/NumberInput/tests/unit/NumberInput.tsx
@@ -1,22 +1,86 @@
 import { tsx } from '@dojo/framework/core/vdom';
 const { registerSuite } = intern.getInterface('object');
-// const { assert } = intern.getPlugin('chai');
+const { assert } = intern.getPlugin('chai');
 
-// import * as sinon from 'sinon';
+import * as sinon from 'sinon';
 
 import { harness } from '@dojo/framework/testing/harness';
 import NumberInput from '../../NumberInput';
-import TextInput from '../../../../text-input';
+import TextInput, { BaseInputProperties } from '../../../../text-input';
 
-const expected = function() {
-	return <TextInput type="number" value="" />;
+const noop = () => {};
+
+const expected = function(
+	{
+		value,
+		baseProperties
+	}: {
+		value?: string;
+		baseProperties?: BaseInputProperties<{ value: number }>;
+	} = {
+		value: undefined,
+		baseProperties: undefined
+	}
+) {
+	return function() {
+		return (
+			<TextInput
+				{...baseProperties}
+				assertion-key="textInput"
+				type="number"
+				value={value}
+				onValue={() => {}}
+			/>
+		);
+	};
 };
 
 registerSuite('NumberInput', {
 	tests: {
 		'default properties'() {
 			const h = harness(() => <NumberInput />);
-			h.expect(expected);
+			h.expect(expected());
+		},
+		'passes expected properties to underlying TextInput'() {
+			const baseProperties: BaseInputProperties<{ value: number }> = {
+				aria: { foo: 'bar' },
+				autocomplete: true,
+				disabled: true,
+				helperText: 'helper text',
+				label: 'label',
+				labelHidden: true,
+				leading: () => <div />,
+				name: 'name',
+				onBlur: noop,
+				onFocus: noop,
+				onKeyDown: noop,
+				onKeyUp: noop,
+				onValidate: noop,
+				onClick: noop,
+				onOver: noop,
+				onOut: noop,
+				onValue: noop,
+				readOnly: true,
+				trailing: () => <div />,
+				value: 42,
+				widgetId: 'widgetId'
+			};
+
+			const h = harness(() => <NumberInput {...baseProperties} />);
+			h.expect(expected({ value: baseProperties.value!.toString(), baseProperties }));
+		},
+		'passes correct value to underlying TextInput'() {
+			const value = 42;
+			const h = harness(() => <NumberInput value={value} />);
+			h.expect(expected({ value: value.toString() }));
+		},
+		'calls onValue with correct value'() {
+			const value = 42;
+			const onValue = sinon.stub();
+
+			const h = harness(() => <NumberInput onValue={onValue} />);
+			h.trigger(':root', 'onValue', value.toString());
+			assert.isTrue(onValue.calledWith(value));
 		}
 	}
 });
