db4496c267ca6f828ef04cbc603c8acf485f23ba
Tom Dye <github@tomdye.co.uk>
2019-10-22 16:17:54 +0100

    move compose to middleware

--- src/raised-button/index.tsx
+++ src/raised-button/index.tsx
@@ -1,7 +1,7 @@
-import theme from '@dojo/framework/core/middleware/theme';
+import theme from '../middleware/theme';
 import { tsx, create } from '@dojo/framework/core/vdom';
 import Button, { ButtonProperties } from '../button/index';
-import * as variantCss from '../theme/raised-button.m.css';
+import * as raisedButtonCss from '../theme/raised-button.m.css';
 import * as buttonCss from '../theme/button.m.css';
 
 export interface RaisedButtonProperties extends ButtonProperties {}
@@ -16,10 +16,10 @@ export const RaisedButton = factory(function({ properties, children, middleware:
 			{...props}
 			theme={{
 				...props.theme,
-				'@dojo/widgets/button': {
-					...theme.classes(buttonCss),
-					...theme.classes(variantCss)
-				}
+				'@dojo/widgets/button': theme.compose(
+					buttonCss,
+					raisedButtonCss
+				)
 			}}
 		>
 			{children()}
