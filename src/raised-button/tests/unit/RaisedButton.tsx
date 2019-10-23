2c75fb2b099d65d81ecf0356fb4660e92cf243e1
Tom Dye <github@tomdye.co.uk>
2019-10-23 17:22:38 +0100

    use theme comparator

--- src/raised-button/tests/unit/RaisedButton.tsx
+++ src/raised-button/tests/unit/RaisedButton.tsx
@@ -7,23 +7,31 @@ import * as buttonCss from '../../../theme/button.m.css';
 import { tsx } from '@dojo/framework/core/vdom';
 import assertionTemplate from '@dojo/framework/testing/assertionTemplate';
 import Button from '../../../button/index';
+import { compareTheme } from '@dojo/widgets/common/tests/support/test-helpers';
 
-const { ' _key': key, ...buttonTheme } = buttonCss as any;
-const theme = { '@dojo/widgets/button': buttonTheme };
-
-const baseTemplate = assertionTemplate(() => <Button theme={theme} />);
+const baseTemplate = assertionTemplate(() => (
+	<Button theme={{ '@dojo/widgets/button': buttonCss }} />
+));
 
 registerSuite('RaisedButton', {
 	tests: {
 		'no content'() {
-			const h = harness(() => <RaisedButton />);
+			const h = harness(() => <RaisedButton />, [compareTheme(buttonCss)]);
 			h.expect(baseTemplate);
 		},
 
 		'properties and attributes'() {
-			const h = harness(() => <RaisedButton type="submit" name="bar" disabled={true} />);
-
-			h.expect(() => <Button theme={theme} type="submit" name="bar" disabled={true} />);
+			const h = harness(() => <RaisedButton type="submit" name="bar" disabled={true} />, [
+				compareTheme(buttonCss)
+			]);
+			h.expect(() => (
+				<Button
+					theme={{ '@dojo/widgets/button': buttonCss }}
+					type="submit"
+					name="bar"
+					disabled={true}
+				/>
+			));
 		}
 	}
 });
