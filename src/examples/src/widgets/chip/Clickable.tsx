d1c88cc216cbed72b4967fb58e7e86158044b6ab
Bradley Maier <brdlmaier49@gmail.com>
2019-11-01 09:56:29 -0700

    Update src/examples/src/widgets/chip/Clickable.tsx
    Co-Authored-By: Tom Dye <tomdye@users.noreply.github.com>

--- src/examples/src/widgets/chip/Clickable.tsx
+++ src/examples/src/widgets/chip/Clickable.tsx
@@ -4,7 +4,7 @@ import { icache } from '@dojo/framework/core/middleware/icache';
 
 const factory = create({ icache });
 
-const App = factory(function({ middleware: { icache } }) {
+const App = factory(function Clickable({ middleware: { icache } }) {
 	const clickable = icache.getOrSet<number>('clickable', 0);
 	return (
 		<virtual>
