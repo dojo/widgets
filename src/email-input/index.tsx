67937bb61e4505713a7394d12d9add3f98533f50
Sam Menza <smenza@sitepen.com>
2019-11-11 16:09:56 -0500

    Added themes middleware to email-input

--- src/email-input/index.tsx
+++ src/email-input/index.tsx
@@ -1,17 +1,20 @@
 import { TextInput, BaseInputProperties } from '../text-input/index';
 import { tsx, create } from '@dojo/framework/core/vdom';
 import icache from '@dojo/framework/core/middleware/icache';
+import theme from '../middleware/theme';
+import * as textInputCss from '../theme/text-input.m.css';
+import * as emailInputCss from '../theme/email-input.m.css';
 
 export interface EmailInputProperties extends BaseInputProperties {}
 
-const factory = create({ icache }).properties<EmailInputProperties>();
+const factory = create({ icache, theme }).properties<EmailInputProperties>();
 
-export const EmailInput = factory(function({ properties, middleware: { icache } }) {
+export const EmailInput = factory(function({ properties, middleware: { icache, theme } }) {
 	const { get, set } = icache;
 	const props = properties();
 	return (
 		<TextInput
-			{...properties()}
+			{...props}
 			type={'email'}
 			onValidate={(valid, message) => {
 				set('valid', valid);
@@ -19,6 +22,13 @@ export const EmailInput = factory(function({ properties, middleware: { icache } 
 				props.onValidate && props.onValidate(valid, message);
 			}}
 			valid={{ valid: get('valid'), message: get('message') }}
+			theme={{
+				...props.theme,
+				'@dojo/widgets/textInput': theme.compose(
+					emailInputCss,
+					textInputCss
+				)
+			}}
 		/>
 	);
 });
