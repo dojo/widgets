3117297bac6a27113634998cc8e5922a40a265dc
Matt Gadd <mgadd@sitepen.com>
2019-11-13 23:20:40 +0000

    support multiple theme files for widget variants

--- src/examples/src/theme.block.ts
+++ src/examples/src/theme.block.ts
@@ -1,28 +1,48 @@
 import * as postcss from 'postcss';
 import * as fs from 'fs';
+import * as path from 'canonical-path';
+import { Project } from 'ts-morph';
 
 interface ThemeInterface {
 	[index: string]: { [index: string]: string };
 }
 
 export default function(config: { [index: string]: string }): ThemeInterface {
-	return Object.keys(config).reduce((properties, widget) => {
-		const root = postcss.parse(fs.readFileSync(`./src/theme/${widget}.m.css`));
+	const project = new Project({
+		tsConfigFilePath: path.join(__dirname, '..', '..', '..', 'tsconfig.json')
+	});
+	return Object.keys(config).reduce((properties, widgetName) => {
 		const classHash = {} as any;
 		let comment = '';
-		root.walk((node) => {
-			if (node.type === 'comment') {
-				comment = node.text;
-			} else if (node.type === 'rule' && node.selector.match(/^\./)) {
-				const selector = /^\.[a-zA-Z0-9]*/.exec(node.selector);
-				if (selector && !classHash[selector[0]]) {
-					classHash[selector[0]] = comment;
+		const filename = config[widgetName] || 'index';
+		let sourceFile = project.getSourceFile(`./src/${widgetName}/${filename}.ts`);
+		if (!sourceFile) {
+			sourceFile = project.getSourceFile(`./src/${widgetName}/${filename}.tsx`);
+		}
+		if (sourceFile) {
+			const imports = sourceFile!.getImportDeclarations();
+			imports.forEach((importDeclaration) => {
+				const moduleSpecifierValue = importDeclaration.getModuleSpecifierValue();
+				const match = moduleSpecifierValue.match(/.*\/theme\/(.*\.m\.css)$/);
+				if (match) {
+					const cssFilename = match[1];
+					const root = postcss.parse(fs.readFileSync(`./src/theme/${cssFilename}`));
+					root.walk((node) => {
+						if (node.type === 'comment') {
+							comment = node.text;
+						} else if (node.type === 'rule' && node.selector.match(/^\./)) {
+							const selector = /^\.[a-zA-Z0-9]*/.exec(node.selector);
+							if (selector && !classHash[selector[0]]) {
+								classHash[selector[0]] = comment;
+							}
+							comment = '';
+						} else {
+							comment = '';
+						}
+					});
 				}
-				comment = '';
-			} else {
-				comment = '';
-			}
-		});
-		return { ...properties, [widget]: classHash };
+			});
+		}
+		return { ...properties, [widgetName]: classHash };
 	}, {});
 }
