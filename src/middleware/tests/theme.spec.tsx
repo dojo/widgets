f84ff4f0f1a0215c09833d2340a45051f5786544
Tom Dye <tomdye@users.noreply.github.com>
2019-11-20 16:02:52 +0000

    Add theme prefix and fix classes clash (#906)
    * add theme prefix and fix classes clash
    
    * change fix to use theme classes prop that doesnt return classes
    
    * add extra composes test
    
    * extra test
    
    * extra test
    
    * add extra test for composes and prefix with classes
    
    * add variant classes to theme result
    
    * simplify
    
    * simplify
    
    * simplify logic

--- src/middleware/tests/theme.spec.tsx
+++ src/middleware/tests/theme.spec.tsx
@@ -26,6 +26,21 @@ const TestWidget = factory(function TestWidget({ properties, middleware: { theme
 	);
 });
 
+const PrefixTestWidget = factory(function TestWidget({ properties, middleware: { theme } }) {
+	const { baseCss, variantCss } = properties();
+	return (
+		<div key="root">
+			{JSON.stringify(
+				theme.compose(
+					baseCss,
+					variantCss,
+					'prefix'
+				)
+			)}
+		</div>
+	);
+});
+
 const baseTemplate = assertionTemplate(() => <div key="root" />);
 
 registerSuite('theme-middleware', {
@@ -101,6 +116,149 @@ registerSuite('theme-middleware', {
 			h.expect(
 				baseTemplate.setChildren('@root', () => [
 					JSON.stringify({ a: 'base-theme-a', b: 'variant-theme-b' })
+				])
+			);
+		},
+
+		'Resolves themes correctly when using composes resulting in space-separated classnames'() {
+			const baseCss = { ' _key': 'base', a: 'base-a', b: 'base-b' };
+			const variantCss = {
+				' _key': 'variant',
+				a: 'variant-a composed-variant-a',
+				b: 'variant-b'
+			};
+			const theme = {
+				base: {
+					a: 'base-theme-a'
+				},
+				variant: {
+					// composed-variant-a in both themed / unthemed classnames
+					a: 'variant-theme-a composed-variant-a'
+				}
+			};
+			const h = harness(() => (
+				<TestWidget baseCss={baseCss} variantCss={variantCss} theme={theme} />
+			));
+
+			h.expect(
+				baseTemplate.setChildren('@root', () => [
+					JSON.stringify({ a: 'variant-theme-a composed-variant-a', b: 'base-b' })
+				])
+			);
+		},
+
+		'Resolves theme classes correctly when used with `classes` property'() {
+			const baseCss = { ' _key': 'base', a: 'base-a', b: 'base-b' };
+			const variantCss = { ' _key': 'variant', a: 'variant-a', b: 'variant-b' };
+			const theme = {
+				base: {
+					a: 'base-theme-a'
+				},
+				variant: {
+					b: 'variant-theme-b'
+				}
+			};
+			const h = harness(() => (
+				<TestWidget
+					baseCss={baseCss}
+					variantCss={variantCss}
+					theme={theme}
+					classes={{
+						variant: { a: ['variant-classes-a'] },
+						base: { a: ['base-classes-a'] }
+					}}
+				/>
+			));
+
+			h.expect(
+				baseTemplate.setChildren('@root', () => [
+					JSON.stringify({
+						a: 'base-theme-a base-classes-a variant-classes-a',
+						b: 'variant-theme-b'
+					})
+				])
+			);
+		},
+
+		'Can be used with a prefix to pick out theme classes for a child widget'() {
+			const baseCss = { ' _key': 'base', root: 'base-root', input: 'base-input' };
+			const variantCss = { ' _key': 'variant', prefixRoot: 'variant-root' };
+			const theme = {
+				base: {
+					root: 'base-theme-root'
+				},
+				variant: {
+					prefixRoot: 'variant-theme-root'
+				}
+			};
+			const h = harness(() => (
+				<PrefixTestWidget baseCss={baseCss} variantCss={variantCss} theme={theme} />
+			));
+
+			h.expect(
+				baseTemplate.setChildren('@root', () => [
+					JSON.stringify({ root: 'variant-theme-root', input: 'base-input' })
+				])
+			);
+		},
+
+		'Resolves prefixed theme classes correctly when used with `classes` property'() {
+			const baseCss = { ' _key': 'base', root: 'base-root', input: 'base-input' };
+			const variantCss = { ' _key': 'variant', prefixRoot: 'variant-root' };
+			const theme = {
+				variant: {
+					prefixInput: 'variant-theme-input'
+				}
+			};
+			const h = harness(() => (
+				<PrefixTestWidget
+					baseCss={baseCss}
+					variantCss={variantCss}
+					theme={theme}
+					classes={{ variant: { prefixRoot: ['variant-classes-prefix-root'] } }}
+				/>
+			));
+
+			h.expect(
+				baseTemplate.setChildren('@root', () => [
+					JSON.stringify({
+						root: 'base-root variant-classes-prefix-root',
+						input: 'variant-theme-input'
+					})
+				])
+			);
+		},
+
+		'Resolves prefixed theme classes correctly when used with `classes` property and composes classes'() {
+			const baseCss = {
+				' _key': 'base',
+				root: 'base-root',
+				input: 'base-input composed-input'
+			};
+			const variantCss = { ' _key': 'variant', prefixRoot: 'variant-root' };
+			const theme = {
+				variant: {
+					prefixInput: 'variant-theme-input composed-input'
+				}
+			};
+			const h = harness(() => (
+				<PrefixTestWidget
+					baseCss={baseCss}
+					variantCss={variantCss}
+					theme={theme}
+					classes={{
+						base: { root: ['base-classes-prefix-root'] },
+						variant: { prefixRoot: ['variant-classes-prefix-root'] }
+					}}
+				/>
+			));
+
+			h.expect(
+				baseTemplate.setChildren('@root', () => [
+					JSON.stringify({
+						root: 'base-root base-classes-prefix-root variant-classes-prefix-root',
+						input: 'variant-theme-input composed-input'
+					})
 				])
 			);
 		}
--- src/middleware/theme.ts
+++ src/middleware/theme.ts
@@ -3,23 +3,64 @@ import coreTheme from '@dojo/framework/core/middleware/theme';
 import { ClassNames } from '@dojo/framework/core/mixins/Themed';
 
 const factory = create({ coreTheme });
+export const THEME_KEY = ' _key';
 
-const theme = factory(function({ middleware: { coreTheme } }) {
+const theme = factory(function({ middleware: { coreTheme }, properties }) {
 	return {
-		compose(baseCss: ClassNames, variantCss: ClassNames) {
-			const allVariantThemeClasses: ClassNames = coreTheme.classes(variantCss);
-			const sanitizedVariantThemeClasses: ClassNames = {};
+		compose(baseCss: ClassNames, variantCss: ClassNames, prefix?: string) {
+			let allVariantThemeClasses: ClassNames = coreTheme.classes(variantCss);
+			const allBaseThemeClasses: ClassNames = coreTheme.classes(baseCss);
 
-			for (let className in allVariantThemeClasses) {
-				if (allVariantThemeClasses[className] !== variantCss[className]) {
-					sanitizedVariantThemeClasses[className] = allVariantThemeClasses[className];
-				}
+			const { classes } = properties();
+			const variantKey = variantCss[THEME_KEY];
+
+			const sanitizedThemeClasses: ClassNames = allBaseThemeClasses;
+
+			if (prefix) {
+				allVariantThemeClasses = Object.keys(allVariantThemeClasses).reduce(
+					(themeClasses, className) => {
+						if (className.indexOf(prefix) === 0) {
+							let newClassName = className.replace(prefix, '');
+							newClassName =
+								newClassName.charAt(0).toLowerCase() + newClassName.slice(1);
+
+							return {
+								...themeClasses,
+								[newClassName]: allVariantThemeClasses[className]
+							};
+						} else {
+							return themeClasses;
+						}
+					},
+					{}
+				);
 			}
 
-			return {
-				...coreTheme.classes(baseCss),
-				...sanitizedVariantThemeClasses
-			};
+			for (let className in allVariantThemeClasses) {
+				const calculatedClassName = prefix
+					? `${prefix}${className.charAt(0).toUpperCase() + className.slice(1)}`
+					: className;
+				let compare = variantCss[calculatedClassName];
+
+				let variantClasses = '';
+				if (classes && classes[variantKey] && classes[variantKey][calculatedClassName]) {
+					variantClasses = classes[variantKey][calculatedClassName].join(' ');
+				}
+
+				if (variantClasses) {
+					compare = `${compare} ${variantClasses}`;
+				}
+
+				if (allVariantThemeClasses[className] !== compare) {
+					sanitizedThemeClasses[className] = allVariantThemeClasses[className];
+				}
+
+				sanitizedThemeClasses[className] = `${
+					sanitizedThemeClasses[className]
+				} ${variantClasses}`.trim();
+			}
+
+			return sanitizedThemeClasses;
 		},
 		...coreTheme
 	};
