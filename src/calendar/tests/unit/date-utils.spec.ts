233684ec85a703fd4882687e9012ea1018cb4b52
samenz <smenza@sitepen.com>
2019-11-25 12:43:46 -0800

    Fixes calendar date util test error (#920)
    * Fixes calendar date util test error

--- src/calendar/tests/unit/date-utils.spec.ts
+++ src/calendar/tests/unit/date-utils.spec.ts
@@ -6,7 +6,7 @@ import { monthInMin } from '../../date-utils';
 registerSuite('Calendar date utils', {
 	tests: {
 		'monthInMin checks year/month against the first of the year'() {
-			const janFirst2019 = new Date('2019-01-01');
+			const janFirst2019 = new Date(2019, 0, 1);
 
 			assert.isFalse(monthInMin(2018, 10, janFirst2019));
 			assert.isFalse(monthInMin(2018, 11, janFirst2019));
@@ -14,7 +14,7 @@ registerSuite('Calendar date utils', {
 			assert.isTrue(monthInMin(2019, 1, janFirst2019));
 		},
 		'monthInMin checks year/month against the last day the year'() {
-			const decThirtyFirst2018 = new Date('2018-12-31');
+			const decThirtyFirst2018 = new Date(2018, 11, 31);
 
 			assert.isFalse(monthInMin(2018, 9, decThirtyFirst2018));
 			assert.isFalse(monthInMin(2018, 10, decThirtyFirst2018));
@@ -22,7 +22,7 @@ registerSuite('Calendar date utils', {
 			assert.isTrue(monthInMin(2019, 1, decThirtyFirst2018));
 		},
 		'monthInMin checks year/month against leap day'() {
-			const febTwentyNine2020 = new Date('2020-02-29');
+			const febTwentyNine2020 = new Date(2020, 1, 29);
 
 			assert.isFalse(monthInMin(2019, 11, febTwentyNine2020));
 			assert.isFalse(monthInMin(2020, 0, febTwentyNine2020));
@@ -30,7 +30,7 @@ registerSuite('Calendar date utils', {
 			assert.isTrue(monthInMin(2020, 2, febTwentyNine2020));
 		},
 		'monthInMin supports out of index months'() {
-			const janFirst2019 = new Date('2019-01-01');
+			const janFirst2019 = new Date(2019, 0, 1);
 
 			assert.isFalse(monthInMin(2017, 14, janFirst2019));
 			assert.isTrue(monthInMin(2018, 12, janFirst2019));
