01fd27b79fd8c96d7d3610e386c27a741ee1a0b0
Paul <paul@sitepen.com>
2019-12-03 16:00:00 -0700

    ignore time when checking min and max dates

--- src/calendar/tests/unit/date-utils.spec.tsx
+++ src/calendar/tests/unit/date-utils.spec.tsx
@@ -1,7 +1,10 @@
+import { isOutOfDateRange, monthInMin } from '../../date-utils';
+
 const { registerSuite } = intern.getInterface('object');
 const { assert } = intern.getPlugin('chai');
 
-import { monthInMin } from '../../date-utils';
+const fullDate = new Date(1979, 2, 20, 7, 33, 12);
+const shortDate = new Date(2019, 11, 3);
 
 registerSuite('Calendar date utils', {
 	tests: {
@@ -36,6 +39,72 @@ registerSuite('Calendar date utils', {
 			assert.isTrue(monthInMin(2018, 12, janFirst2019));
 			assert.isFalse(monthInMin(2019, -1, janFirst2019));
 			assert.isTrue(monthInMin(2020, -2, janFirst2019));
+		},
+		isOutOfDateRange: {
+			'no range is always inside the range'() {
+				assert.isFalse(isOutOfDateRange(fullDate));
+				assert.isFalse(isOutOfDateRange(shortDate));
+			},
+			'dates on the minimum day are inside the range'() {
+				const min = new Date(
+					fullDate.getFullYear(),
+					fullDate.getMonth(),
+					fullDate.getDate(),
+					23,
+					59,
+					59,
+					999
+				);
+				assert.isFalse(isOutOfDateRange(fullDate, min));
+			},
+			'dates on the maximum day are inside the range'() {
+				const max = new Date('2017-06-29T00:00:00');
+				const dateObj = new Date('2017-06-29T23:59:59.999');
+				assert.isFalse(isOutOfDateRange(dateObj, undefined, max));
+			},
+			'dates equal to minimum are inside the range'() {
+				assert.isFalse(isOutOfDateRange(fullDate, fullDate));
+			},
+			'dates equal to maximum are inside the range'() {
+				assert.isFalse(isOutOfDateRange(fullDate, undefined, fullDate));
+			},
+			'dates on the same day as the most narrow range are inside the range'() {
+				const min = new Date(
+					fullDate.getFullYear(),
+					fullDate.getMonth(),
+					fullDate.getDate(),
+					23,
+					59,
+					59,
+					999
+				);
+				const max = new Date(
+					fullDate.getFullYear(),
+					fullDate.getMonth(),
+					fullDate.getDate(),
+					0,
+					0,
+					0,
+					0
+				);
+				assert.isFalse(isOutOfDateRange(fullDate, min, max));
+			},
+			'when min is present dates before are out of the range'() {
+				const min = new Date(
+					fullDate.getFullYear(),
+					fullDate.getMonth(),
+					fullDate.getDate() + 1
+				);
+				assert.isTrue(isOutOfDateRange(fullDate, min));
+			},
+			'when max is present dates after are out of the range'() {
+				const max = new Date(
+					fullDate.getFullYear(),
+					fullDate.getMonth(),
+					fullDate.getDate() - 1
+				);
+				assert.isTrue(isOutOfDateRange(fullDate, undefined, max));
+			}
 		}
 	}
 });
