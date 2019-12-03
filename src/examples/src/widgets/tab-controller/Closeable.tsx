ecee0bfa1936d7231833af526c0d1bc4f0511b5a
Bradley Maier <bmaier@sitepen.com>
2019-12-03 08:37:36 -0800

    Create comparable tab controller examples in new structure (#903)
    * Create comparable tab controller examples in new structure
    
    * PR feedback
    
    * Update after rebase

--- src/examples/src/widgets/tab-controller/Closeable.tsx
+++ src/examples/src/widgets/tab-controller/Closeable.tsx
@@ -1,27 +1,34 @@
 import { tsx, create } from '@dojo/framework/core/vdom';
 import icache from '@dojo/framework/core/middleware/icache';
+import { includes } from '@dojo/framework/shim/array';
 
 import TabController from '@dojo/widgets/tab-controller';
 import Tab from '@dojo/widgets/tab';
 
 const factory = create({ icache });
 
-export default factory(function Disabled({ middleware: { icache } }) {
+export default factory(function Closeable({ middleware: { icache } }) {
 	const activeIndex = icache.getOrSet('active', 0);
+	const closedKeys = icache.getOrSet<string[]>('closedKeys', []);
 	return (
 		<TabController
 			activeIndex={activeIndex}
 			onRequestTabChange={(index) => {
 				icache.set('active', index);
 			}}
+			onRequestTabClose={(index, key) => {
+				icache.set('closedKeys', [...closedKeys, key]);
+			}}
 		>
-			<Tab key="tab-one" disabled={true} label="Tab One">
-				Hello Tab One
-			</Tab>
+			{!includes(closedKeys, 'tab-one') ? (
+				<Tab key="tab-one" label="Tab One" closeable={true}>
+					Hello Tab One
+				</Tab>
+			) : null}
 			<Tab key="tab-two" label="Tab Two">
 				Hello Tab Two
 			</Tab>
-			<Tab key="tab-three" disabled={true} label="Tab Three">
+			<Tab key="tab-three" label="Tab Three">
 				Hello Tab Three
 			</Tab>
 			<Tab key="tab-four" label="Tab Four">
