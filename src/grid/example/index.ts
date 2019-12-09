6556bdf4105ac8b8ff0ca92a6a21e63661d22998
Anthony Gubler <agubler@sitepen.com>
2019-12-09 14:46:25 +0000

    Add example

--- src/grid/example/index.ts
+++ src/grid/example/index.ts
@@ -2,7 +2,7 @@ import { WidgetBase } from '@dojo/framework/core/WidgetBase';
 import { v, w } from '@dojo/framework/core/vdom';
 import Grid from '../../grid/index';
 import { createFetcher } from '../../grid/utils';
-import mockData from './mockData';
+import mockData, { getMockData } from './mockData';
 
 export default class App extends WidgetBase {
 	private columnConfig = [
@@ -53,6 +53,13 @@ export default class App extends WidgetBase {
 			w(Grid, {
 				columnConfig: this.columnConfig,
 				fetcher: createFetcher(this.gridData),
+				height: 500
+			}),
+			v('h2', ['Paginated Grid Example']),
+			w(Grid, {
+				pagination: true,
+				columnConfig: this.columnConfig,
+				fetcher: createFetcher(getMockData(20)),
 				height: 500
 			})
 		]);
