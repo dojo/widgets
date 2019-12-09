421e82ad80b4485687a52ca60dfc42773f04d876
Anthony Gubler <agubler@sitepen.com>
2019-12-09 14:23:05 +0000

    Traditional grid pagination

---
+++ src/grid/styles/paginated-footer.m.css.d.ts
@@ -0,0 +1,7 @@
+export const rootFixed: string;
+export const containerFixed: string;
+export const detailsFixed: string;
+export const paginationListFixed: string;
+export const itemFixed: string;
+export const moreFixed: string;
+export const pageNavFixed: string;
