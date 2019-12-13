ebe97ed7abf958cb0a0d56674dc87263113e317c
James Milner <jamesmilner22@hotmail.com>
2019-12-13 14:17:48 +0000

    Move HelperText to functional widget (#936)
    * Move HelperText to functional widget/tsx and add theming
    
    * Fix the interface name
    
    * Update usage

--- src/helper-text/tests/unit/HelperText.spec.tsx
+++ src/helper-text/tests/unit/HelperText.spec.tsx
@@ -1,39 +1,29 @@
 const { registerSuite } = intern.getInterface('object');
 
-import { v, w } from '@dojo/framework/core/vdom';
 import assertationTemplate from '@dojo/framework/testing/assertionTemplate';
 import * as css from '../../../theme/default/helper-text.m.css';
 import HelperText from '../../index';
 import harness from '@dojo/framework/testing/harness';
+import { tsx } from '@dojo/framework/core/vdom';
 
 const baseTemplate = assertationTemplate(() => {
-	return v('div', {
-		key: 'root',
-		classes: [css.root, null, null]
-	});
+	return <div key="root" classes={[css.root, null, null]} />;
 });
 
 const textTemplate = baseTemplate.setChildren('@root', [
-	v(
-		'p',
-		{
-			key: 'text',
-			classes: css.text,
-			'aria-hidden': 'true',
-			title: 'test'
-		},
-		['test']
-	)
+	<p key="text" classes={css.text} aria-hidden="true" title="test">
+		test
+	</p>
 ]);
 
 registerSuite('HelperText', {
 	tests: {
 		'without text'() {
-			const h = harness(() => w(HelperText, {}));
+			const h = harness(() => <HelperText />);
 			h.expect(baseTemplate);
 		},
 		'with text'() {
-			const h = harness(() => w(HelperText, { text: 'test' }));
+			const h = harness(() => <HelperText text="test" />);
 			h.expect(textTemplate);
 		},
 		valid() {
@@ -42,7 +32,7 @@ registerSuite('HelperText', {
 				css.valid,
 				null
 			]);
-			const h = harness(() => w(HelperText, { text: 'test', valid: true }));
+			const h = harness(() => <HelperText text="test" valid={true} />);
 			h.expect(validTemplate);
 		},
 		invalid() {
@@ -51,7 +41,7 @@ registerSuite('HelperText', {
 				null,
 				css.invalid
 			]);
-			const h = harness(() => w(HelperText, { text: 'test', valid: false }));
+			const h = harness(() => <HelperText text="test" valid={false} />);
 			h.expect(invalidTemplate);
 		}
 	}
