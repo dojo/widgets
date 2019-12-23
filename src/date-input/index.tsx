be26a5a56434367f7d88666068b96274d2c8b91a
Justin Ellison <jellison@sitepen.com>
2019-12-23 10:07:27 -0500

    Widget best practices
    * Allow user-entered date
    * i18n date formatting
    * input validation

--- src/date-input/index.tsx
+++ src/date-input/index.tsx
@@ -1,16 +1,16 @@
 import { create, tsx } from '@dojo/framework/core/vdom';
 import { createICacheMiddleware } from '@dojo/framework/core/middleware/icache';
+import { i18n } from '@dojo/framework/core/middleware/i18n';
 import theme from '../middleware/theme';
 import Calendar from '../calendar';
 import TextInput from '../text-input';
 import Icon from '@dojo/widgets/icon';
 import Popup from '../popup';
 import * as css from '../theme/default/date-input.m.css';
-import HelperText from '../helper-text';
+
+import bundle from './nls/DateInput';
 
 export interface DateInputProperties {
-	/** Sets the helper text of the input */
-	helperText?: string;
 	/** The current value */
 	initialValue?: Date;
 	/** Set the latest date the calendar will display (it will show the whole month but not allow later selections) */
@@ -22,87 +22,103 @@ export interface DateInputProperties {
 }
 
 interface DateInputICache {
+	/** Current input value */
+	value: string;
+	/** The last valid Date of value */
 	date: Date;
+	/** Month of the popup calendar */
 	month: number;
+	/** Year of the popup calendar */
 	year: number;
+	/** Should validate the input value on the next cycle */
+	shouldValidate: boolean;
+	/** Message for current validation state */
+	validationMessages: string[];
 }
 
 const icache = createICacheMiddleware<DateInputICache>();
-const factory = create({ theme, icache }).properties<DateInputProperties>();
+const factory = create({ theme, icache, i18n }).properties<DateInputProperties>();
+const formatDate = (date: Date) => {
+	return Intl.DateTimeFormat().format(date);
+};
 
-export default factory(function({ properties, middleware: { theme, icache } }) {
-	const { max, min, onValue, initialValue = new Date(), helperText } = properties();
+export default factory(function({ properties, middleware: { theme, icache, i18n } }) {
+	const { max, min, onValue, initialValue = new Date() } = properties();
+	const { messages } = i18n.localize(bundle);
 	const classes = theme.classes(css);
-	const date = icache.getOrSet('date', initialValue);
-	const month = icache.getOrSet('month', initialValue.getMonth());
-	const year = icache.getOrSet('year', initialValue.getFullYear());
 
-	const formatDate = () => {
-		const year = date.getFullYear();
-		const month = date.getMonth() + 1;
-		const day = date
-			.getDate()
-			.toString()
-			.padStart(2, '0');
+	const value = icache.getOrSet('value', formatDate(initialValue));
+	const shouldValidate = icache.getOrSet('shouldValidate', true);
 
-		return `${year}-${month}-${day}`;
-	};
+	if (shouldValidate) {
+		let validationMessages: string[] = [];
+
+		if (min && max && min > max) {
+			validationMessages.push(messages.invalidProps);
+		}
+
+		if (isNaN(Date.parse(value))) {
+			validationMessages.push(messages.invalidDate);
+		}
+
+		if (validationMessages.length === 0) {
+			const newDate = new Date(value);
+			icache.set('date', newDate);
+			icache.set('month', newDate.getMonth());
+			icache.set('year', newDate.getFullYear());
+			if (onValue) {
+				onValue(newDate);
+			}
+		}
+
+		icache.set('validationMessages', validationMessages);
+		icache.set('shouldValidate', false);
+	}
 
 	return (
 		<div classes={classes.root}>
 			<Popup>
 				{{
 					trigger: (toggleOpen) => {
-						function openCalendar() {
-							// todo: focus calendar popup instead of text-input
-							toggleOpen();
-						}
-
 						return (
 							<div classes={classes.input}>
 								<TextInput
-									onClick={openCalendar}
-									onFocus={openCalendar}
 									trailing={() => (
-										<div onclick={openCalendar}>
+										<div onclick={toggleOpen}>
 											<Icon type="dateIcon" />
 										</div>
 									)}
 									type="text"
-									value={formatDate()}
+									value={value}
+									onBlur={() => icache.set('shouldValidate', true)}
+									onValue={(v) => icache.set('value', v || '')}
+									helperText={(icache.get('validationMessages') || []).join('; ')}
 								/>
 							</div>
 						);
 					},
 					content: (onClose) => {
-						function closePopup() {
-							onClose();
-						}
-
 						return (
 							<div classes={classes.popup}>
 								<Calendar
 									maxDate={max}
 									minDate={min}
-									month={month}
+									month={icache.get('month')}
 									onDateSelect={(date) => {
-										icache.set('date', date);
-										if (onValue) {
-											onValue(date);
-										}
-										closePopup();
+										icache.set('value', formatDate(date));
+										icache.set('shouldValidate', true);
+										onClose();
 									}}
 									onMonthChange={(month) => icache.set('month', month)}
 									onYearChange={(year) => icache.set('year', year)}
-									selectedDate={date}
-									year={year}
+									selectedDate={icache.get('date')}
+									year={icache.get('year')}
 								/>
 							</div>
 						);
 					}
 				}}
 			</Popup>
-			<HelperText key={helperText} text={helperText} />
 		</div>
 	);
 });
---
+++ src/date-input/nls/DateInput.ts
@@ -0,0 +1,6 @@
+const messages = {
+	invalidProps: 'Min date cannot be greater than max date',
+	invalidDate: 'Invalid date format'
+};
+
+export default { messages };
