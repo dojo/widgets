19b1470d5567d09301c99983f40ecc267f97f477
Justin Ellison <jellison@sitepen.com>
2019-12-23 13:51:34 -0500

    Material dialog theme
    Close icon will (presumably) be fixed once the Material Icon theme has been ported to @widgets

--- src/theme/material/dialog.m.css.d.ts
+++ src/theme/material/dialog.m.css.d.ts
@@ -1,1 +1,7 @@
 export const root: string;
+export const open: string;
+export const main: string;
+export const title: string;
+export const content: string;
+export const close: string;
+export const underlayVisible: string;
