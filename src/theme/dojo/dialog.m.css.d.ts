0ae2d0040705bacd3021301f9a615202bf275fc3
Justin Ellison <jellison@sitepen.com>
2019-12-26 08:25:55 -0500

    Adjusted css selector to avoid lint warnings

--- src/theme/dojo/dialog.m.css.d.ts
+++ src/theme/dojo/dialog.m.css.d.ts
@@ -4,3 +4,4 @@ export const underlayVisible: string;
 export const title: string;
 export const content: string;
 export const close: string;
+export const closeIcon: string;
--- src/theme/material/dialog.m.css.d.ts
+++ src/theme/material/dialog.m.css.d.ts
@@ -4,4 +4,5 @@ export const main: string;
 export const title: string;
 export const content: string;
 export const close: string;
+export const closeIcon: string;
 export const underlayVisible: string;
