41f80084c4e206e3cfb02649199dfe73d608c556
Sam Menza <smenza@sitepen.com>
2020-01-03 11:24:45 -0800

    Updated default style

--- src/theme/default/native-select.m.css.d.ts
+++ src/theme/default/native-select.m.css.d.ts
@@ -1,3 +1,6 @@
 export const root: string;
 export const select: string;
 export const disabled: string;
+export const required: string;
+export const inputWrapper: string;
+export const arrow: string;
