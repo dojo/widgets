9c8fd617d57cad3b983f5fbccf1489977bff1643
James <jamesmilner22@hotmail.com>
2020-01-13 17:50:04 +0000

    Make DisabledSlider disabled

--- src/examples/src/widgets/slider/DisabledSlider.tsx
+++ src/examples/src/widgets/slider/DisabledSlider.tsx
@@ -1,18 +1,8 @@
 import { create, tsx } from '@dojo/framework/core/vdom';
-import { icache } from '@dojo/framework/core/middleware/icache';
 import Slider from '@dojo/widgets/slider';
 
-const factory = create({ icache });
+const factory = create();
 
-export default factory(function DisabledSlider({ middleware: { icache } }) {
-	const value = icache.getOrSet<number>('value', 30);
-	return (
-		<Slider
-			min={0}
-			value={value}
-			max={100}
-			onValue={(value) => icache.set('value', value)}
-			label="Stuck at 30"
-		/>
-	);
+export default factory(function DisabledSlider() {
+	return <Slider min={0} value={50} max={100} disabled label="Disabled" />;
 });
