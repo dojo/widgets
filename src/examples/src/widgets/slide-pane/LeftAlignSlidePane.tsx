aa16373462d257065890b448c71e667c0ca0def4
James <jamesmilner22@hotmail.com>
2020-01-13 12:25:59 +0000

    Add new examples and remove old ones for SlidePane

---
+++ src/examples/src/widgets/slide-pane/LeftAlignSlidePane.tsx
@@ -0,0 +1,47 @@
+import { create, tsx } from '@dojo/framework/core/vdom';
+import SlidePane, { Align } from '@dojo/widgets/slide-pane';
+import icache from '@dojo/framework/core/middleware/icache';
+
+const factory = create({ icache });
+
+export default factory(function LeftAlignSlidePane({ middleware: { icache } }) {
+	return (
+		<div>
+			<SlidePane
+				title="Left Aligned SlidePane"
+				open={icache.getOrSet('open', true)}
+				underlay={false}
+				align={Align.left}
+				onRequestClose={() => {
+					console.log('close');
+					icache.set('open', false);
+				}}
+			>
+				{`Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				Quisque id purus ipsum. Aenean ac purus purus.
+				Nam sollicitudin varius augue, sed lacinia felis tempor in.
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				Quisque id purus ipsum. Aenean ac purus purus.
+				Nam sollicitudin varius augue, sed lacinia felis tempor in.
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				Quisque id purus ipsum. Aenean ac purus purus.
+				Nam sollicitudin varius augue, sed lacinia felis tempor in.
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				Quisque id purus ipsum. Aenean ac purus purus.
+				Nam sollicitudin varius augue, sed lacinia felis tempor in.
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				Quisque id purus ipsum. Aenean ac purus purus.
+				Nam sollicitudin varius augue, sed lacinia felis tempor in.
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				Quisque id purus ipsum. Aenean ac purus purus.
+				Nam sollicitudin varius augue, sed lacinia felis tempor in.
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				Quisque id purus ipsum. Aenean ac purus purus.
+				Nam sollicitudin varius augue, sed lacinia felis tempor in.
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				Quisque id purus ipsum. Aenean ac purus purus.
+				Nam sollicitudin varius augue, sed lacinia felis tempor in.`}
+			</SlidePane>
+		</div>
+	);
+});
