252bed6a5913585cd5b95c9b4921382f57c05640
Bradley Maier <bmaier@sitepen.com>
2020-01-17 09:08:25 -0800

    Basic material theme for toolbar (#1006)
    * Basic material theme for toolbar
    
    * Delete extra files
    
    * update toolbar theme
    
    * revert button type
    
    * Revert "revert button type"
    
    This reverts commit 258c3b7114d3be5b7a084f5a249a3f2d22fbe5d5.
    
    Co-authored-by: Tom Dye <tomdye@users.noreply.github.com>

--- src/examples/src/widgets/toolbar/Basic.tsx
+++ src/examples/src/widgets/toolbar/Basic.tsx
@@ -1,35 +1,35 @@
 import { create, tsx } from '@dojo/framework/core/vdom';
 import icache from '@dojo/framework/core/middleware/icache';
-import Button from '@dojo/widgets/button';
 import Toolbar from '@dojo/widgets/toolbar';
+import RaisedButton from '@dojo/widgets/raised-button';
 
 const factory = create({ icache });
 
 export default factory(function Basic({ middleware: { icache } }) {
 	return (
 		<div>
-			<Toolbar collapseWidth={0}>
-				<Button
+			<Toolbar heading="Heading" collapseWidth={0}>
+				<RaisedButton
 					onClick={() => {
 						icache.set('value', 'Clicked Option A');
 					}}
 				>
 					Option A{' '}
-				</Button>
-				<Button
+				</RaisedButton>
+				<RaisedButton
 					onClick={() => {
 						icache.set('value', 'Clicked Option B');
 					}}
 				>
 					Option B
-				</Button>
-				<Button
+				</RaisedButton>
+				<RaisedButton
 					onClick={() => {
 						icache.set('value', 'Clicked Option C');
 					}}
 				>
 					Option C
-				</Button>
+				</RaisedButton>
 			</Toolbar>
 			<div>{icache.get('value')}</div>
 		</div>
