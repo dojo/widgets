cbb717b5b14c48718950475768d899f244eaa4b2
Tom Dye <github@tomdye.co.uk>
2020-02-03 21:46:17 +0000

    theme accordion / title pane, import correct variables

--- src/theme/material/accordion-pane.m.css.d.ts
+++ src/theme/material/accordion-pane.m.css.d.ts
@@ -1,1 +1,5 @@
 export const root: string;
+export const firstTitlePane: string;
+export const lastTitlePane: string;
+export const openTitlePane: string;
+export const rootTitlePane: string;
--- src/theme/material/title-pane.m.css.d.ts
+++ src/theme/material/title-pane.m.css.d.ts
@@ -1,1 +1,5 @@
 export const root: string;
+export const titleButton: string;
+export const arrow: string;
+export const open: string;
+export const content: string;
