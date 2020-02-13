9423432822a8adbc43a77fad0325cfe5c5d48849
Nick Nisi <nick@nisi.org>
2020-02-12 23:24:01 -0600

    add material and dojo themes for avatar

--- src/avatar/index.tsx
+++ src/avatar/index.tsx
@@ -13,7 +13,7 @@ const factory = create({ theme }).properties<AvatarProperties>();
 
 export const Avatar = factory(function Avatar({ middleware: { theme }, properties, children }) {
 	const themeCss = theme.classes(css);
-	const { variant = 'circle', src, alt, size = 'medium' } = properties();
+	const { src, alt, variant = 'circle', size = 'medium' } = properties();
 	return (
 		<div key="root" classes={[themeCss.root, themeCss[size], themeCss[variant]]}>
 			{src ? <img alt={alt} classes={themeCss.image} src={src} /> : children()}
