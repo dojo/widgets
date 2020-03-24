eafd34f7d5e659a9062148c9daa857865e632e30
Eric Osmundson <eosmundson@sitepen.com>
2020-03-24 17:56:22 -0500

    Remove toolbar widget

--- src/theme/default/toolbar.m.css.d.ts
+++
@@ -1,6 +0,0 @@
-export const actions: string;
-export const collapsed: string;
-export const menuButton: string;
-export const root: string;
-export const title: string;
-export const toggleIcon: string;
--- src/theme/material/toolbar.m.css.d.ts
+++
@@ -1,3 +0,0 @@
-export const root: string;
-export const title: string;
-export const actions: string;
