68f35763834e6ec9113fa34262e652e98286527e
Bradley Maier <bmaier@sitepen.com>
2020-04-02 16:16:51 -0700

    Fix default for number input example

--- src/examples/src/widgets/number-input/Basic.tsx
+++ src/examples/src/widgets/number-input/Basic.tsx
@@ -5,15 +5,16 @@ import icache from '@dojo/framework/core/middleware/icache';
 const factory = create({ icache });
 
 const Example = factory(function Example({ middleware: { icache } }) {
+	const initialValue = 42;
 	return (
 		<virtual>
 			<NumberInput
-				initialValue={42}
+				initialValue={initialValue}
 				onValue={(value) => {
 					icache.set('value', value);
 				}}
 			/>
-			<div>The number input value is: {`${icache.get('value')}`}</div>
+			<div>The number input value is: {`${icache.getOrSet('value', initialValue)}`}</div>
 		</virtual>
 	);
 });
