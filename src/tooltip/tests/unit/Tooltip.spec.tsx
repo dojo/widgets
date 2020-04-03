2e8685534b35cca27ec5d7c9f79f392e75150bd9
Bradley Maier <bmaier@sitepen.com>
2020-04-03 10:27:20 -0700

    Update tooltip to use a child renderer for content (#1288)

--- src/tooltip/tests/unit/Tooltip.spec.tsx
+++ src/tooltip/tests/unit/Tooltip.spec.tsx
@@ -4,13 +4,14 @@ import { tsx } from '@dojo/framework/core/vdom';
 import harness from '@dojo/framework/testing/harness';
 
 import Tooltip, { Orientation } from '../../index';
+import Button from '../../../button/index';
 import * as css from '../../../theme/default/tooltip.m.css';
 import * as fixedCss from '../../styles/tooltip.m.css';
 
 registerSuite('Tooltip', {
 	tests: {
 		'should construct Tooltip'() {
-			const h = harness(() => <Tooltip content="" />);
+			const h = harness(() => <Tooltip>{{ content: '' }}</Tooltip>);
 			h.expect(() => (
 				<div classes={[undefined, css.right, fixedCss.rootFixed, fixedCss.rightFixed]}>
 					<div key="target" />
@@ -19,7 +20,7 @@ registerSuite('Tooltip', {
 		},
 
 		'should render content if open'() {
-			const h = harness(() => <Tooltip content="foobar" open />);
+			const h = harness(() => <Tooltip open>{{ content: 'foobar' }}</Tooltip>);
 
 			h.expect(() => (
 				<div classes={[undefined, css.right, fixedCss.rootFixed, fixedCss.rightFixed]}>
@@ -32,9 +33,28 @@ registerSuite('Tooltip', {
 			));
 		},
 
+		'should render trigger'() {
+			const h = harness(() => (
+				<Tooltip open>
+					{{ trigger: <Button>Give me a tooltip</Button>, content: 'foobar' }}
+				</Tooltip>
+			));
+
+			h.expect(() => (
+				<div classes={[undefined, css.right, fixedCss.rootFixed, fixedCss.rightFixed]}>
+					<div key="target">
+						<Button>Give me a tooltip</Button>
+						<div key="content" classes={[css.content, fixedCss.contentFixed]}>
+							foobar
+						</div>
+					</div>
+				</div>
+			));
+		},
+
 		'should render correct orientation'() {
 			const h = harness(() => (
-				<Tooltip orientation={Orientation.bottom} content={'foobar'} />
+				<Tooltip orientation={Orientation.bottom}>{{ content: 'foobar' }}</Tooltip>
 			));
 
 			h.expect(() => (
@@ -45,7 +65,11 @@ registerSuite('Tooltip', {
 		},
 
 		'should render aria properties'() {
-			const h = harness(() => <Tooltip content="bar" open aria={{ describedBy: 'foo' }} />);
+			const h = harness(() => (
+				<Tooltip open aria={{ describedBy: 'foo' }}>
+					{{ content: 'bar' }}
+				</Tooltip>
+			));
 
 			h.expect(() => (
 				<div classes={[undefined, css.right, fixedCss.rootFixed, fixedCss.rightFixed]}>
