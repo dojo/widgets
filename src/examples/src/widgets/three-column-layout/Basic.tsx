c5e401b96519b8e41b38e5bfad56122ecab34a2b
Bradley Maier <bmaier@sitepen.com>
2020-04-02 17:46:20 -0700

    Center text in and add borders to examples

--- src/examples/src/widgets/three-column-layout/Basic.tsx
+++ src/examples/src/widgets/three-column-layout/Basic.tsx
@@ -7,9 +7,17 @@ export default factory(function Basic() {
 	return (
 		<ThreeColumnLayout>
 			{{
-				leading: <div>This is the leading content</div>,
-				center: <div>This is the center content</div>,
-				trailing: <div>This is the trailing content</div>
+				leading: (
+					<div styles={{ textAlign: 'center', borderRight: '1px solid black' }}>
+						This is the leading content
+					</div>
+				),
+				center: <div styles={{ textAlign: 'center' }}>This is the center content</div>,
+				trailing: (
+					<div styles={{ textAlign: 'center', borderLeft: '1px solid black' }}>
+						This is the trailing content
+					</div>
+				)
 			}}
 		</ThreeColumnLayout>
 	);
