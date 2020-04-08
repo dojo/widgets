e56f3cab1f4e3f48fd7bd8f9ea14f38456b84a78
Bradley Maier <bmaier@sitepen.com>
2020-04-08 12:16:50 -0700

    Optionally control select (#1363)
    * Optionally control select
    
    * Update src/examples/src/widgets/select/Controlled.tsx
    
    Co-Authored-By: Eric <ericos@users.noreply.github.com>
    
    Co-authored-by: Eric <ericos@users.noreply.github.com>

---
+++ src/examples/src/widgets/select/Controlled.tsx
@@ -0,0 +1,33 @@
+import { create, tsx } from '@dojo/framework/core/vdom';
+import Select, { defaultTransform } from '@dojo/widgets/select';
+import icache from '@dojo/framework/core/middleware/icache';
+import { createMemoryResourceWithData } from '../list/memoryTemplate';
+
+const factory = create({ icache });
+const options = [
+	{ value: 'cat', label: 'Cat' },
+	{ value: 'dog', label: 'Dog' },
+	{ value: 'fish', label: 'Fish' }
+];
+
+const resource = createMemoryResourceWithData(options);
+
+export default factory(function Controlled({ middleware: { icache } }) {
+	return (
+		<virtual>
+			<Select
+				resource={resource}
+				transform={defaultTransform}
+				onValue={(value) => {
+					icache.set('value', value);
+				}}
+				value={icache.get('value')}
+			>
+				{{
+					label: 'Basic Select'
+				}}
+			</Select>
+			<pre>{icache.getOrSet('value', '')}</pre>
+		</virtual>
+	);
+});
