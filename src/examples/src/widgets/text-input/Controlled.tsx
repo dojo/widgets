adabdf928e8364e74a4fd725eef70f6ffe1ceeab
Tom Dye <tomdye@users.noreply.github.com>
2020-04-08 20:15:59 +0100

    Optionally Controlled textinput (#1357)
    * value for text input
    
    * number input

---
+++ src/examples/src/widgets/text-input/Controlled.tsx
@@ -0,0 +1,31 @@
+import { create, tsx } from '@dojo/framework/core/vdom';
+import icache from '@dojo/framework/core/middleware/icache';
+import TextInput from '@dojo/widgets/text-input';
+import Button from '@dojo/widgets/button';
+
+const factory = create({ icache });
+
+const Example = factory(function Controlled({ middleware: { icache } }) {
+	return (
+		<virtual>
+			<TextInput
+				value={icache.getOrSet('value', '')}
+				onValue={(value) => {
+					icache.set('value', value);
+				}}
+			>
+				{{ label: 'Controlled input with reset' }}
+			</TextInput>
+			<Button
+				onClick={() => {
+					icache.set('value', '');
+				}}
+			>
+				Reset input
+			</Button>
+			<div>The value text input is: "{icache.getOrSet('value', '')}"</div>
+		</virtual>
+	);
+});
+
+export default Example;
