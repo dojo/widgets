8cefa1823125b0b39a22b56dc403f225a5c0ed5e
Bradley Maier <bmaier@sitepen.com>
2020-04-17 08:42:22 -0700

    WIP

--- src/speed-dial/index.tsx
+++ src/speed-dial/index.tsx
@@ -1,8 +1,9 @@
 import { create, tsx } from '@dojo/framework/core/vdom';
 import theme from '../middleware/theme';
 import FloatingActionButton, { FabIcon } from '@dojo/widgets/floating-action-button';
-import { RenderResult } from '@dojo/framework/core/interfaces';
+import { DNode, RenderResult } from '@dojo/framework/core/interfaces';
 import * as css from '../theme/default/speed-dial.m.css';
+import Icon from '../icon';
 
 export interface SpeedDialIconProperties {
 	open?: boolean;
@@ -13,27 +14,50 @@ export interface SpeedDialIconChildren {
 	openIcon?: RenderResult;
 }
 
-const iconFactory = create({ theme }).properties<SpeedDialIconProperties>().children<SpeedDialIconChildren | undefined>();
+const iconFactory = create({ theme })
+	.properties<SpeedDialIconProperties>()
+	.children<SpeedDialIconChildren | undefined>();
 
-export const SpeedDialIcon = iconFactory(function SpeedDialIcon({ properties, children, middleware: { theme } }) {
+export const SpeedDialIcon = iconFactory(function SpeedDialIcon({
+	properties,
+	children,
+	middleware: { theme }
+}) {
+	const classes = theme.classes(css);
 	const [{ icon, openIcon } = { icon: undefined, openIcon: undefined }] = children();
 	const { open } = properties();
 
-	return open ? ( openIcon || <FabIcon type="plusIcon" classes={}
-		: ( icon || <FabIcon type="plusIcon"/> )
-	)
+	return open
+		? openIcon || (
+				<Icon
+					type="plusIcon"
+					classes={{
+						'@dojo/widgets/icon': {
+							icon: [classes.openIcon]
+						}
+					}}
+				/>
+		  )
+		: icon || <FabIcon type="plusIcon" />;
 });
 
-export interface SpeedDialProperties {
+export interface SpeedDialItem {
+	tooltip?: string;
+	alwaysShowTooltip?: string;
+	label: DNode;
+	action(): void;
+}
+export interface SpeedDialProperties {}
+
+export interface SpeedDialChildren {
+	icon: RenderResult;
 }
 
 const factory = create({ theme }).properties<SpeedDialProperties>();
 
-export const SpeedDial = factory(function SpeedDial({ properties, middleware: { theme }}) {
+export const SpeedDial = factory(function SpeedDial({ properties, middleware: { theme } }) {
 	const x = properties();
-	return (
-		<div/>
-	);
+	return <div />;
 });
 
 export default SpeedDial;
