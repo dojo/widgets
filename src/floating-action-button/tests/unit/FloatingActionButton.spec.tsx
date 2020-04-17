f101f0360d7ff77745aa91aeeb552139a02209ef
Bradley Maier <bmaier@sitepen.com>
2020-04-17 08:47:44 -0700

    Update tests for new framework

--- src/floating-action-button/tests/unit/FloatingActionButton.spec.tsx
+++ src/floating-action-button/tests/unit/FloatingActionButton.spec.tsx
@@ -1,12 +1,12 @@
 const { registerSuite } = intern.getInterface('object');
 
-import harness from '@dojo/framework/testing/harness';
+import harness from '@dojo/framework/testing/harness/harness';
 
 import FloatingActionButton from '../../index';
 import * as buttonCss from '../../../theme/default/button.m.css';
 import * as css from '../../../theme/default/floating-action-button.m.css';
 import { tsx } from '@dojo/framework/core/vdom';
-import assertionTemplate from '@dojo/framework/testing/assertionTemplate';
+import assertionTemplate from '@dojo/framework/testing/harness/assertionTemplate';
 import Button from '../../../button/index';
 import { compareTheme } from '../../../common/tests/support/test-helpers';
 
--- src/theme/dojo/floating-action-button.m.css.d.ts
+++ src/theme/dojo/floating-action-button.m.css.d.ts
@@ -1,2 +1,5 @@
 export const root: string;
+export const icon: string;
 export const extended: string;
+export const pressed: string;
+export const disabled: string;
