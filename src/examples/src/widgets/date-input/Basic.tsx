ec97b6d4e1accbcc5b5fb8695b5fb11cf9377d6e
rorticus <rmulligan@sitepen.com>
2020-04-21 09:38:57 -0400

    Fixing broken DateInput example (#1412)

--- src/examples/src/widgets/date-input/Basic.tsx
+++ src/examples/src/widgets/date-input/Basic.tsx
@@ -1,9 +1,10 @@
 import { create, tsx } from '@dojo/framework/core/vdom';
 import DateInput from '@dojo/widgets/date-input';
+import Example from '@dojo/widgets/examples/src/Example';
 
 const factory = create();
 
-export default factory(function Example() {
+export default factory(function Basic() {
 	return (
 		<Example>
 			<DateInput name="dateInput" />
