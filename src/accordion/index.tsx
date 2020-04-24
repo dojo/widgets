cab6bdb346052ba0d4b2798dda88153d92d423bb
Tom Dye <github@tomdye.co.uk>
2020-04-24 15:10:49 +0100

    simplify accordion

--- src/accordion/index.tsx
+++ src/accordion/index.tsx
@@ -1,48 +1,20 @@
 import * as css from '../theme/default/accordion.m.css';
 import * as titlePaneCss from '../theme/default/title-pane.m.css';
-import TitlePane, { TitlePaneProperties, TitlePaneChildren } from '../title-pane';
+import TitlePane from '../title-pane';
 import theme from '../middleware/theme';
-import { RenderResult } from '@dojo/framework/core/interfaces';
 import { create, tsx } from '@dojo/framework/core/vdom';
 import { createICacheMiddleware } from '@dojo/framework/core/middleware/icache';
+import { RenderResult } from '@dojo/framework/core/interfaces';
 
 export interface AccordionPaneProperties {
 	/* If true, only one TitlePane can be opened at a given time */
 	exclusive?: boolean;
+	/* The pane names */
+	panes: string[];
 }
-
-export interface AccordionPaneChildren {
-	(
-		onOpen: (key: string) => TitlePaneProperties['onOpen'],
-		onClose: (key: string) => TitlePaneProperties['onClose'],
-		open: (key: string) => TitlePaneProperties['open']
-	): RenderResult;
-}
-
 interface AccordionPaneICache {
-	openKeys: {
-		[key: string]: boolean;
-	};
+	openIndexes: Set<number>;
 }
-
-const paneFactory = create({ theme })
-	.properties<TitlePaneProperties>()
-	.children<TitlePaneChildren>();
-
-export const Pane = paneFactory(function Pane({ children, middleware: { theme }, properties }) {
-	return (
-		<TitlePane
-			{...properties()}
-			theme={theme.compose(
-				titlePaneCss,
-				css,
-				'pane'
-			)}
-		>
-			{children()[0]}
-		</TitlePane>
-	);
-});
 
 const icache = createICacheMiddleware<AccordionPaneICache>();
 
@@ -51,7 +23,7 @@ const factory = create({
 	theme
 })
 	.properties<AccordionPaneProperties>()
-	.children<AccordionPaneChildren>();
+	.children<RenderResult>();
 
 export const Accordion = factory(function LoadingIndicator({
 	middleware: { icache, theme },
@@ -59,34 +31,55 @@ export const Accordion = factory(function LoadingIndicator({
 	children
 }) {
 	const classes = theme.classes(css);
-	const [renderer] = children();
-	const { exclusive } = properties();
+	const { exclusive, panes } = properties();
 
-	const onOpen = (key: string) => {
-		return () => {
-			icache.set('openKeys', {
-				...(exclusive ? {} : icache.get('openKeys')),
-				[key]: true
-			});
-		};
+	const openIndexes = icache.getOrSet('openIndexes', new Set());
+	const contentPanes = children();
+
+	const onOpen = (index: number) => {
+		if (exclusive) {
+			icache.set('openIndexes', new Set([index]));
+		} else {
+			const openIndexes = icache.getOrSet('openIndexes', new Set());
+			openIndexes.add(index);
+			icache.set('openIndexes', openIndexes);
+		}
 	};
 
-	const onClose = (key: string) => {
-		return () => {
-			const openKeys = icache.get('openKeys') || {};
-			icache.set('openKeys', {
-				...openKeys,
-				[key]: false
-			});
-		};
+	const onClose = (index: number) => {
+		const openIndexes = icache.getOrSet('openIndexes', new Set());
+		openIndexes.delete(index);
+		icache.set('openIndexes', openIndexes);
 	};
 
-	const open = (key: string) => {
-		const openKeys: AccordionPaneICache['openKeys'] = icache.get('openKeys') || {};
-		return !!openKeys[key];
-	};
-
-	return <div classes={[theme.variant(), classes.root]}>{renderer(onOpen, onClose, open)}</div>;
+	return (
+		<div classes={[theme.variant(), classes.root]}>
+			{panes.map((paneName, index) => {
+				return (
+					<TitlePane
+						key={`pane-${index}`}
+						open={openIndexes.has(index)}
+						onOpen={() => {
+							onOpen(index);
+						}}
+						onClose={() => {
+							onClose(index);
+						}}
+						theme={theme.compose(
+							titlePaneCss,
+							css,
+							'pane'
+						)}
+					>
+						{{
+							title: paneName,
+							content: contentPanes[index]
+						}}
+					</TitlePane>
+				);
+			})}
+		</div>
+	);
 });
 
 export default Accordion;
