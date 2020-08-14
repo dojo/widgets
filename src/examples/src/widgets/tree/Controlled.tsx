f1d49ecaf067105b4d4516b397ee1177cb5a352f
Sam Menza <smenza@sitepen.com>
2020-08-14 12:30:18 -0400

    Fixed example

--- src/examples/src/widgets/tree/Controlled.tsx
+++ src/examples/src/widgets/tree/Controlled.tsx
@@ -16,34 +16,40 @@ const icache = createICacheMiddleware<ControlledCache>();
 const factory = create({ icache, resource });
 const nodes: TreeNodeOption[] = [
 	{
-		id: '851c1eea',
+		id: 'c9ae529a',
 		value: 'parent 1',
-		parent: 'root'
+		parent: 'root',
+		hasChildren: true
 	},
 	{
-		id: '8ef88e80',
+		id: 'cde74420',
 		value: 'parent 1-0',
-		parent: '851c1eea'
+		parent: 'c9ae529a',
+		hasChildren: true
 	},
 	{
-		id: 'a4ed7462',
-		value: 'child 1',
-		parent: '8ef88e80'
-	},
-	{
-		id: 'b055cda4',
-		value: 'child 2',
-		parent: '8ef88e80'
-	},
-	{
-		id: 'becc2f0e',
+		id: 'e2065522',
 		value: 'parent 1-1',
-		parent: '851c1eea'
+		parent: 'c9ae529a',
+		hasChildren: true
 	},
 	{
-		id: 'c35ca04e',
-		value: 'child 3',
-		parent: 'becc2f0e'
+		id: 'de48r11ea',
+		value: 'child-1',
+		parent: 'cde74420',
+		hasChildren: false
+	},
+	{
+		id: 'd8fada34',
+		value: 'child-2',
+		parent: 'cde74420',
+		hasChildren: false
+	},
+	{
+		id: 'e73a11b4',
+		value: 'child-3',
+		parent: 'e2065522',
+		hasChildren: false
 	}
 ];
 const template = createMemoryResourceTemplate<TreeNodeOption>();
@@ -65,7 +71,7 @@ export default factory(function Advanced({ id, middleware: { icache, resource } 
 				<li>
 					<button
 						onclick={() => {
-							icache.set('expanded', expanded ? undefined : ['851c1eea']);
+							icache.set('expanded', expanded ? undefined : ['c9ae529a']);
 						}}
 					>
 						Toggle Expand
@@ -74,7 +80,7 @@ export default factory(function Advanced({ id, middleware: { icache, resource } 
 				<li>
 					<button
 						onclick={() => {
-							icache.set('checked', checked ? undefined : ['851c1eea']);
+							icache.set('checked', checked ? undefined : ['c9ae529a']);
 						}}
 					>
 						Toggle Checked
