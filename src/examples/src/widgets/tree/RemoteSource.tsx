cc651c4f7f6728ad2022aba600609e2306f099eb
Sam Menza <smenza@sitepen.com>
2020-08-26 10:05:14 -0400

    Updated remote source

--- src/examples/src/widgets/tree/RemoteSource.tsx
+++ src/examples/src/widgets/tree/RemoteSource.tsx
@@ -11,54 +11,18 @@ const template = createResourceTemplate<TreeNodeOption>({
 	find: defaultFind,
 	read: async (request, { put, get }) => {
 		const { query } = request;
-		let data: TreeNodeOption[] = [];
-
-		if (query.parent !== 'root') {
-			const response = await fetch(`https://www.dnd5eapi.co/api/races/${query.parent}`, {
+		const response = await fetch(
+			`https://comfortable-opalescent-parrot.glitch.me/${query.parent}`,
+			{
 				headers: {
 					'Content-Type': 'application/json'
 				}
-			});
-			const parsedData = await response.json();
-			data = parsedData.traits.map((value: { url: string; name: string }) => {
-				return {
-					id: `${query.parent}-${value.name}`,
-					value: value.name,
-					hasChildren: false,
-					parent: query.parent
-				};
-			});
-		}
-
-		if (query.parent === 'root') {
-			const initialUrl = 'https://www.dnd5eapi.co/api/races';
-			const response = await fetch(initialUrl, {
-				headers: {
-					'Content-Type': 'application/json'
-				}
-			});
-
-			const parsedData = await response.json();
-			data = parsedData.results.map((value: { index: string; name: string; url: string }) => {
-				return {
-					id: value.index,
-					value: value.name,
-					hasChildren: true,
-					parent: 'root'
-				};
-			});
-		}
-
-		put(
-			{
-				data,
-				total: 30
-			},
-			{
-				...request,
-				size: 30
 			}
 		);
+
+		const { data, total } = await response.json();
+
+		put({ data, total }, request);
 	}
 });
 
@@ -71,13 +35,7 @@ export default factory(function Remote({ id, middleware: { resource } }) {
 			<Tree
 				checkable={true}
 				resource={resource({
-					template,
-					transform: {
-						id: 'id',
-						value: 'value',
-						parent: 'parent',
-						hasChildren: 'hasChildren'
-					}
+					template
 				})}
 			/>
 		</Example>
