e76bea7fb69c385604e54b1e9f2f332dd873a648
bitpshr <mail@bitpshr.net>
2020-10-14 08:23:42 -0400

    feedback: cleanup

--- src/examples/src/widgets/list/Draggable.tsx
+++ src/examples/src/widgets/list/Draggable.tsx
@@ -1,45 +1,33 @@
-import { create, tsx } from '@dojo/framework/core/vdom';
-import List, { ListOption } from '@dojo/widgets/list';
 import icache from '@dojo/framework/core/middleware/icache';
-import Example from '../../Example';
 import {
 	createMemoryResourceTemplate,
 	createResourceMiddleware
 } from '@dojo/framework/core/middleware/resources';
+import { create, tsx } from '@dojo/framework/core/vdom';
+import List, { ListOption } from '@dojo/widgets/list';
+
+import Example from '../../Example';
 
 const resource = createResourceMiddleware();
 const factory = create({ icache, resource });
 
-const animals = [
-	{ value: 'cat' },
-	{ value: 'dog' },
-	{ value: 'mouse' },
-	{ value: 'rat' },
-	{ value: 'cat' },
-	{ value: 'dog' },
-	{ value: 'mouse' },
-	{ value: 'rat' },
-	{ value: 'cat' },
-	{ value: 'dog' },
-	{ value: 'mouse' },
-	{ value: 'rat' }
-];
+const items = Array.from(Array(100).keys()).map((value) => ({ value: `${value}` }));
 const template = createMemoryResourceTemplate<ListOption>();
 
 export default factory(function Draggable({ id, middleware: { icache, resource } }) {
-	const animalData = icache.getOrSet('animalData', animals);
-	const data = resource({ template, initOptions: { id, data: animalData } });
+	const rawData = icache.getOrSet('rawData', items);
+	const data = resource({ template, initOptions: { id, data: rawData } });
 
 	return (
 		<Example>
 			<List
 				draggable
 				onMove={(from, to) => {
-					const sortable = [...animalData];
+					const sortable = [...rawData];
 					const item = sortable[from];
 					sortable.splice(from, 1);
 					sortable.splice(to, 0, item);
-					icache.set('animalData', sortable);
+					icache.set('rawData', sortable);
 				}}
 				resource={data}
 				onValue={(value: string) => {
