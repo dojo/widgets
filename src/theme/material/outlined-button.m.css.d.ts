1328078054caf3361f9737373cbbfec3492850ee
Rory Mulligan <rorticus@gmail.com>
2021-01-20 13:24:26 -0500

    Adding button kinds to dojo theme

--- src/theme/material/outlined-button.m.css.d.ts
+++ src/theme/material/outlined-button.m.css.d.ts
@@ -1,2 +1,4 @@
 export const root: string;
 export const pressed: string;
+export const cancel: string;
+export const secondary: string;
--- src/theme/material/raised-button.m.css.d.ts
+++ src/theme/material/raised-button.m.css.d.ts
@@ -1,3 +1,5 @@
 export const root: string;
 export const pressed: string;
 export const disabled: string;
+export const cancel: string;
+export const secondary: string;
