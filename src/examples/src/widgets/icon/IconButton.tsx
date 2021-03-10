7caafe2437775f9fed056fccf4b0906059ea3081
Anthony Ciccarello <aciccarello@sitepen.com>
2021-03-09 16:03:24 -0800

    fix icon button positioning
    closes dojo/widgets#1594

--- src/examples/src/widgets/icon/IconButton.tsx
+++ src/examples/src/widgets/icon/IconButton.tsx
@@ -1,6 +1,9 @@
 import { create, tsx } from '@dojo/framework/core/vdom';
 import Icon from '@dojo/widgets/icon';
 import Button from '@dojo/widgets/button';
+import RaisedButton from '@dojo/widgets/raised-button';
+import OutlinedButton from '@dojo/widgets/outlined-button';
+import ActionButton from '@dojo/widgets/action-button';
 import Example from '../../Example';
 
 const factory = create();
@@ -8,9 +11,20 @@ const factory = create();
 export default factory(function IconButton() {
 	return (
 		<Example>
-			<Button>
-				Send <Icon type="mailIcon" />
-			</Button>
+			<div style="display: flex; gap: 16px;">
+				<Button>
+					Send <Icon type="mailIcon" size="small" />
+				</Button>
+				<RaisedButton>
+					Search <Icon type="searchIcon" size="small" />
+				</RaisedButton>
+				<OutlinedButton>
+					Schedule <Icon type="dateIcon" size="small" />
+				</OutlinedButton>
+				<ActionButton>
+					Stop <Icon type="cancelIcon" size="small" />
+				</ActionButton>
+			</div>
 		</Example>
 	);
 });
