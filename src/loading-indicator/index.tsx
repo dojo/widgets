83185a93175339ee8b68eeced92c819e2235dc41
Sam Menza <smenza@sitepen.com>
2021-03-24 13:23:03 -0400

    Added material theme for circular loader, added size property

--- src/loading-indicator/index.tsx
+++ src/loading-indicator/index.tsx
@@ -6,7 +6,10 @@ import { create, tsx } from '@dojo/framework/core/vdom';
 export interface LoadingIndicatorProperties extends ThemedProperties {
 	/** If the element is actively loading. Defaults to true */
 	active?: boolean;
+	/* Sets loader type to linear or circular */
 	type?: 'linear' | 'circular';
+	/* Size of circular loader */
+	size?: 'small' | 'medium' | 'large';
 }
 
 const factory = create({ theme }).properties<LoadingIndicatorProperties>();
@@ -16,8 +19,9 @@ export const LoadingIndicator = factory(function LoadingIndicator({
 	middleware: { theme }
 }) {
 	const classes = theme.classes(css);
-	const { active = true, type = 'linear' } = properties();
+	const { active = true, type = 'linear', size = 'small' } = properties();
 
+	const sizeClass = size && classes[size as keyof typeof classes];
 	return (
 		<virtual>
 			{type === 'linear' && (
@@ -44,10 +48,64 @@ export const LoadingIndicator = factory(function LoadingIndicator({
 						theme.variant(),
 						classes.root,
 						!active && classes.inactive,
-						classes.circular
+						classes.circular,
+						sizeClass
 					]}
-					role="loader"
-				/>
+					role="progressbar"
+				>
+					<div classes={classes.circularContainer}>
+						<div classes={classes.spinnerLayer}>
+							<div classes={[classes.circleClipper, classes.circleLeft]}>
+								<svg
+									classes={classes.circleGraphic}
+									viewBox="0 0 24 24"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<circle
+										cx="12"
+										cy="12"
+										r="8.75"
+										stroke-dasharray="54.978"
+										stroke-dashoffset="27.489"
+										stroke-width="2.5"
+									/>
+								</svg>
+							</div>
+							<div classes={classes.gapPatch}>
+								<svg
+									classes={classes.circleGraphic}
+									viewBox="0 0 24 24"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<circle
+										cx="12"
+										cy="12"
+										r="8.75"
+										stroke-dasharray="54.978"
+										stroke-dashoffset="27.489"
+										stroke-width="2"
+									/>
+								</svg>
+							</div>
+							<div classes={[classes.circleClipper, classes.circleRight]}>
+								<svg
+									classes={classes.circleGraphic}
+									viewBox="0 0 24 24"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<circle
+										cx="12"
+										cy="12"
+										r="8.75"
+										stroke-dasharray="54.978"
+										stroke-dashoffset="27.489"
+										stroke-width="2.5"
+									/>
+								</svg>
+							</div>
+						</div>
+					</div>
+				</div>
 			)}
 		</virtual>
 	);
