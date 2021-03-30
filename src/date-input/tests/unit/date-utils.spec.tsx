96e6ac64584e6a21273194bbe7583ce830ac52bb
Anthony Ciccarello <aciccarello@sitepen.com>
2021-03-30 14:59:49 -0700

    use explicit date formats in tests

--- src/date-input/tests/unit/date-utils.spec.tsx
+++ src/date-input/tests/unit/date-utils.spec.tsx
@@ -1,29 +1,12 @@
-const { describe, it, beforeEach, afterEach } = intern.getInterface('bdd');
+const { describe, it } = intern.getInterface('bdd');
 const { assert } = intern.getPlugin('chai');
 
-import { createSandbox, SinonSandbox } from 'sinon';
+import * as sinon from 'sinon';
 
 import { parseDate } from '../../date-utils';
 
 describe('DateInput Date Utils', () => {
-	let sandbox: SinonSandbox;
-
-	beforeEach(() => {
-		sandbox = createSandbox();
-	});
-
-	afterEach(() => {
-		sandbox.restore();
-	});
-
 	describe('parseDate', () => {
-		function stubFormatForLocale(locale: string) {
-			const localeDateTimeFormat = Intl.DateTimeFormat(locale);
-			const stub = sandbox.stub(Intl, 'DateTimeFormat');
-			stub.returns({ format: (date: Date) => localeDateTimeFormat.format(date) });
-			return stub;
-		}
-
 		it('handles empty input', () => {
 			const value = undefined;
 			const actual = parseDate(value);
@@ -36,37 +19,41 @@ describe('DateInput Date Utils', () => {
 		});
 
 		it('can parse US format', () => {
-			const stub = stubFormatForLocale('en-US');
+			const stub = sinon.stub(Intl, 'DateTimeFormat');
+			stub.onFirstCall().returns({ format: () => '4/3/2018' }); // forces "canary" date to match "en-us"
 			const actual = parseDate('6/1/2018');
 			assert.equal(actual && actual.getTime(), new Date(2018, 5, 1).getTime());
 			stub.restore();
 		});
 
 		it('can parse non-US format', () => {
-			const stub = stubFormatForLocale('fr-fr');
-			stub.onCall(0).returns({ format: () => '3/4/2018' }); // forces "canary" date down non-US path
+			const stub = sinon.stub(Intl, 'DateTimeFormat');
+			stub.onFirstCall().returns({ format: () => '03/04/2018' }); // forces "canary" date to match "fr-fr"
 
-			const actual = parseDate('1/6/2018');
+			const actual = parseDate('01/06/2018');
 			assert.equal(actual && actual.getTime(), new Date(2018, 5, 1).getTime());
 			stub.restore();
 		});
 
 		it('can parse period separated date format', () => {
-			const stub = stubFormatForLocale('de-de');
+			const stub = sinon.stub(Intl, 'DateTimeFormat');
+			stub.onFirstCall().returns({ format: () => '3.4.2018' }); // forces "canary" date to match "de-de"
 			const actual = parseDate('1.6.2018');
 			assert.equal(actual && actual.getTime(), new Date(2018, 5, 1).getTime());
 			stub.restore();
 		});
 
 		it('can parse dash separated date format', () => {
-			const stub = stubFormatForLocale('nl-nl');
+			const stub = sinon.stub(Intl, 'DateTimeFormat');
+			stub.onFirstCall().returns({ format: () => '3-4-2018' }); // forces "canary" date to match "nl-nl"
 			const actual = parseDate('1-6-2018');
 			assert.equal(actual && actual.getTime(), new Date(2018, 5, 1).getTime());
 			stub.restore();
 		});
 
 		it('can parse YYYY/MM/DD format', () => {
-			const stub = stubFormatForLocale('zh-cn');
+			const stub = sinon.stub(Intl, 'DateTimeFormat');
+			stub.onFirstCall().returns({ format: () => '2018/4/3' }); // forces "canary" date to match "zh-cn"
 			const actual = parseDate('2018/6/1');
 			assert.equal(actual && actual.getTime(), new Date(2018, 5, 1).getTime());
 			stub.restore();
