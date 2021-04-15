37918c40c5e816469aa5fb93b11d0a5ea990c3a9
bitpshr <mail@bitpshr.net>
2021-04-15 12:11:48 -0400

    feedback: use external css

--- src/examples/src/widgets/list/CustomTheme.tsx
+++ src/examples/src/widgets/list/CustomTheme.tsx
@@ -3,32 +3,34 @@ import List from '@dojo/widgets/list';
 import icache from '@dojo/framework/core/middleware/icache';
 import Example from '../../Example';
 import { listOptionTemplate } from '../../template';
+import * as css from './CustomTheme.m.css';
 
 const factory = create({ icache });
 
-const exampleStyles = `
-    .exampleList {
-        --mdc-list-item-selected-background: #4fc3f7;
-        --mdc-list-item-selected-color: #eeeeee;
-        --mdc-list-item-active-background: #0093c4;
-        --mdc-list-item-active-color: #ffffff;
-        --mdc-list-item-hover-background: #4fc3f7;
-        --mdc-list-item-hover-color: #eeeeee;
-
-        --list-item-selected-background: #43a047;
-        --list-item-selected-color: #eeeeee;
-        --list-item-active-background: #00701a;
-        --list-item-active-color: #ffffff;
-        --list-item-hover-background: #43a047;
-        --list-item-hover-color: #eeeeee;
-    }
-`;
+/**
+ * This code uses the custom CSS shown below:
+ *
+ * .exampleList {
+ *     --mdc-list-item-selected-background: #4fc3f7;
+ *     --mdc-list-item-selected-color: #eeeeee;
+ *     --mdc-list-item-active-background: #0093c4;
+ *     --mdc-list-item-active-color: #ffffff;
+ *     --mdc-list-item-hover-background: #4fc3f7;
+ *     --mdc-list-item-hover-color: #eeeeee;
+ *
+ *     --list-item-selected-background: #43a047;
+ *     --list-item-selected-color: #eeeeee;
+ *     --list-item-active-background: #00701a;
+ *     --list-item-active-color: #ffffff;
+ *     --list-item-hover-background: #43a047;
+ *     --list-item-hover-color: #eeeeee;
+ * }
+ */
 
 export default factory(function CustomTheme({ middleware: { icache } }) {
 	return (
 		<Example>
-			<style innerHTML={exampleStyles} />
-			<div classes="exampleList">
+			<div classes={css.exampleList}>
 				<List
 					variant="inherit"
 					resource={{ template: listOptionTemplate }}
