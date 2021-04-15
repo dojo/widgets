23b69c3b55ced9070ca0212cda6df2db03e2fea1
Paul Bouchon <mail@bitpshr.net>
2021-04-15 12:32:03 -0400

    feat: easier list item theming (#1744)
    * feat: easier list item theming
    
    * feedback: add hover state classes and an example
    
    * feedback: use external css

--- src/theme/material/list-item.m.css.d.ts
+++ src/theme/material/list-item.m.css.d.ts
@@ -1,6 +1,7 @@
 export const root: string;
 export const height: string;
 export const selected: string;
+export const disabled: string;
 export const active: string;
 export const collapsed: string;
 export const movedUp: string;
