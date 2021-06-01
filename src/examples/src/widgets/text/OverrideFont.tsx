1bc3326df6c056412d80c6cb6925eb947cb9be4a
Anthony Gubler <agubler@sitepen.com>
2021-06-01 12:36:15 +0100

    compile errors

--- src/examples/src/widgets/text/OverrideFont.tsx
+++ src/examples/src/widgets/text/OverrideFont.tsx
@@ -10,7 +10,7 @@ export default factory(function OverrideFont() {
 		<Example>
 			<virtual>
 				<div style="--mdc-theme-font-family: 'Comic Sans MS', 'Comic Sans', cursive;">
-					<Text variant="inherit" size="xxl">
+					<Text variant="inherit" size="x-large">
 						Override the variant font
 					</Text>
 				</div>
