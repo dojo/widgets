4d95ef2ebd31e32e5a9acc8ab7c453225c24e43c
Daniel Lautzenheiser <lautzd@gmail.com>
2021-06-04 11:49:22 -0400

    Update other themes

--- src/text-input/index.tsx
+++ src/text-input/index.tsx
@@ -303,74 +303,76 @@ export const TextInput = factory(function TextInput({
 				role="presentation"
 			>
 				{_renderLabel()}
-				{leading && <span classes={themeCss.leading}>{leading}</span>}
-				{kind === 'default' && <span classes={themeCss.ripple} />}
-				<input
-					{...formatAriaProperties(aria)}
-					aria-invalid={valid === false ? 'true' : undefined}
-					autocomplete={formatAutocomplete(autocomplete)}
-					classes={themeCss.input}
-					disabled={disabled}
-					id={widgetId}
-					focus={focus.shouldFocus}
-					key={'input'}
-					max={max}
-					maxlength={maxLength ? `${maxLength}` : null}
-					min={min}
-					minlength={minLength ? `${minLength}` : null}
-					name={name}
-					pattern={pattern}
-					placeholder={placeholder}
-					readOnly={readOnly}
-					aria-readonly={readOnly ? 'true' : undefined}
-					required={required}
-					step={step}
-					type={type}
-					value={value}
-					onblur={() => {
-						onBlur && onBlur();
-					}}
-					onfocus={() => {
-						onFocus && onFocus();
-					}}
-					oninput={(event: Event) => {
-						event.stopPropagation();
-						const value = (event.target as HTMLInputElement).value;
-						icache.set('value', value);
-						onValue && onValue(value);
-					}}
-					onkeydown={(event: KeyboardEvent) => {
-						event.stopPropagation();
-						onKeyDown && onKeyDown(event.which, () => event.preventDefault());
-					}}
-					onkeyup={(event: KeyboardEvent) => {
-						event.stopPropagation();
-						onKeyUp && onKeyUp(event.which, () => event.preventDefault());
-					}}
-					onclick={() => {
-						onClick && onClick();
-					}}
-					onpointerenter={() => {
-						onOver && onOver();
-					}}
-					onpointerleave={() => {
-						onOut && onOut();
-					}}
-					onanimationstart={(event: AnimationEvent) => {
-						if (event.animationName === themeCss.onAutofillShown) {
-							icache.set('autofilled', true);
-						}
-					}}
-				/>
-				{kind === 'default' && (
-					<span
-						classes={[
-							themeCss.lineRipple,
-							inputFocused ? themeCss.defaultKindLineRippleFocused : null
-						]}
+				<div key="inputWrapper" classes={themeCss.inputWrapper} role="presentation">
+					{leading && <span classes={themeCss.leading}>{leading}</span>}
+					{kind === 'default' && <span classes={themeCss.ripple} />}
+					<input
+						{...formatAriaProperties(aria)}
+						aria-invalid={valid === false ? 'true' : undefined}
+						autocomplete={formatAutocomplete(autocomplete)}
+						classes={themeCss.input}
+						disabled={disabled}
+						id={widgetId}
+						focus={focus.shouldFocus}
+						key={'input'}
+						max={max}
+						maxlength={maxLength ? `${maxLength}` : null}
+						min={min}
+						minlength={minLength ? `${minLength}` : null}
+						name={name}
+						pattern={pattern}
+						placeholder={placeholder}
+						readOnly={readOnly}
+						aria-readonly={readOnly ? 'true' : undefined}
+						required={required}
+						step={step}
+						type={type}
+						value={value}
+						onblur={() => {
+							onBlur && onBlur();
+						}}
+						onfocus={() => {
+							onFocus && onFocus();
+						}}
+						oninput={(event: Event) => {
+							event.stopPropagation();
+							const value = (event.target as HTMLInputElement).value;
+							icache.set('value', value);
+							onValue && onValue(value);
+						}}
+						onkeydown={(event: KeyboardEvent) => {
+							event.stopPropagation();
+							onKeyDown && onKeyDown(event.which, () => event.preventDefault());
+						}}
+						onkeyup={(event: KeyboardEvent) => {
+							event.stopPropagation();
+							onKeyUp && onKeyUp(event.which, () => event.preventDefault());
+						}}
+						onclick={() => {
+							onClick && onClick();
+						}}
+						onpointerenter={() => {
+							onOver && onOver();
+						}}
+						onpointerleave={() => {
+							onOut && onOut();
+						}}
+						onanimationstart={(event: AnimationEvent) => {
+							if (event.animationName === themeCss.onAutofillShown) {
+								icache.set('autofilled', true);
+							}
+						}}
 					/>
-				)}
-				{trailing && <span classes={themeCss.trailing}>{trailing}</span>}
+					{kind === 'default' && (
+						<span
+							classes={[
+								themeCss.lineRipple,
+								inputFocused ? themeCss.defaultKindLineRippleFocused : null
+							]}
+						/>
+					)}
+					{trailing && <span classes={themeCss.trailing}>{trailing}</span>}
+				</div>
 			</div>
 			<HelperText
 				text={computedHelperText}
