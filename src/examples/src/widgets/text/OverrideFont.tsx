c797e92202e409042e00dc7124471f22bfd06174
Anthony Gubler <agubler@sitepen.com>
2021-06-10 13:22:59 +0100

    allow button vars to be configured

--- src/examples/src/widgets/text/OverrideFont.tsx
+++ src/examples/src/widgets/text/OverrideFont.tsx
@@ -8,13 +8,11 @@ const factory = create({ icache });
 export default factory(function OverrideFont() {
 	return (
 		<Example>
-			<virtual>
-				<div style="--mdc-theme-font-family: 'Comic Sans MS', 'Comic Sans', cursive;">
-					<Text variant="inherit" size="x-large">
-						Override the variant font
-					</Text>
-				</div>
-			</virtual>
+			<div style="--mdc-theme-font-family: 'Comic Sans MS', 'Comic Sans', cursive;">
+				<Text variant="inherit" size="x-large">
+					Override the variant font
+				</Text>
+			</div>
 		</Example>
 	);
 });
